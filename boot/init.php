e7b7288be13bd375f3a6f5a501a3ffcd308054fc
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-07 23:33:38 +0200

    added composer autoloading file and removed Phalcon loader

--- boot/init.php
+++ boot/init.php
@@ -5,24 +5,77 @@
 //! @details
 //! @author Filippo F. Fadda
 
+$start = microtime(true);
 
 try {
-  $config = new Phalcon\Config\Adapter\Ini(__DIR__.'/../pitpress/config.ini');
+  $config = new Phalcon\Config\Adapter\Ini(__DIR__.'/../config.ini');
 
-  // System bootstrap.
-  include __DIR__."/../boot/loader.php";
-  include __DIR__."/../boot/services.php";
-  include __DIR__."/../boot/routes.php";
+  // Initializes the Composer autoloading system. (Note: We don't use the Phalcon loader.)
+  require __DIR__."/../vendor/autoload.php";
+
+  // Start the services.
+  require __DIR__."/../boot/services.php";
+
+  // Retrieves the Router component.
+  $router = $di['router'];
+
+  // Handle the current route.
+  $router->handle();
+
+  // Retrieves the Dispatcher component.
+  $dispatcher = $di['dispatcher'];
+
+  // Passes the processed router parameters to the dispatcher.
+  $dispatcher->setNamespaceName($router->getNamespaceName());
+  $dispatcher->setControllerName($router->getControllerName());
+  $dispatcher->setActionName($router->getActionName());
+  $dispatcher->setParams($router->getParams());
+
+  // Dispatches the request.
+  $dispatcher->dispatch();
+
+  // Retrieves the View component.
+  $view = $di['view'];
+
+  // Starts the related view.
+  $view->start();
+
+  // Renders the related views.
+  $view->render(
+    $dispatcher->getControllerName(),
+    $dispatcher->getActionName(),
+    $dispatcher->getParams()
+  );
+
+  // Finishes the related view.
+  $view->finish();
+
+  // Retrieves the Response component.
+  $response = $di['response'];
+
+  // Passes the output of the view to the response.
+  $response->setContent($view->getContent());
+
+  // Sends the request headers.
+  $response->sendHeaders();
+
+  // Prints the response.
+  echo $response->getContent();
+
+
 
   // Creates the application object.
-  $application = new \Phalcon\Mvc\Application();
+  /*$application = new Phalcon\Mvc\Application();
 
   // Sets the dependency injector component.
   $application->setDI($di);
 
   // Handles the request.
-  echo $application->handle()->getContent();
+  echo $application->handle()->getContent(); */
 }
-catch (Phalcon\Exception $e) {
+catch (Exception $e) {
   echo $e->getMessage();
 }
+
+$stop = microtime(true);
+$time = round($stop - $start, 3);
