961c523ec23b448ca2db6e245fb2d0eae5477130
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-08 23:45:04 +0200

    just added

---
+++ src/PitPress/Model/Link.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file Link.php
+//! @brief This file contains the Link class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Model;
+
+
+class Link extends Item {
+
+}
