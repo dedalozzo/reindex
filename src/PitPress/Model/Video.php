1e229ad89749155438ee21d9fce8248b6d0cf815
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-08 23:45:15 +0200

    just added

---
+++ src/PitPress/Model/Video.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file Video.php
+//! @brief This file contains the Video class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Model;
+
+
+class Video extends Item {
+
+}
