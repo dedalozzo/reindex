6e5ab672983b7ca9dfa31a704721b87ab6b289f5
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-10 01:17:09 +0200

    overrided magic methods for property name, because Article doesn't have a name but a title.

--- src/PitPress/Model/Article.php
+++ src/PitPress/Model/Article.php
@@ -11,4 +11,27 @@ namespace PitPress\Model;
 
 class Article extends Item {
 
+  // Overrides the Item implementation, because an article doesn't have a name but a title.
+  public function getName() {
+    throw new \BadMethodCallException();
+  }
+
+
+  // Overrides the Item implementation, because an article doesn't have a name but a title.
+  public function issetName() {
+    throw new \BadMethodCallException();
+  }
+
+
+  // Overrides the Item implementation, because an article doesn't have a name but a title.
+  public function setName($value) {
+    throw new \BadMethodCallException();
+  }
+
+
+  // Overrides the Item implementation, because an article doesn't have a name but a title.
+  public function unsetName() {
+    throw new \BadMethodCallException();
+  }
+
 }
