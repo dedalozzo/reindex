205ae51f73da8e6e944c3bee4ec9dac067c92d88
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-12 15:39:57 +0200

    added Title property

--- src/PitPress/Model/Article.php
+++ src/PitPress/Model/Article.php
@@ -9,7 +9,16 @@
 namespace PitPress\Model;
 
 
+//! @brief
+//! @nosubgrouping
 class Article extends Item {
+
+  //! @name Item's Attributes
+  //! @brief Those are standard item's attributes.
+  //@{
+  const TITLE = "title"; //!< Document's title.
+  //@}
+
 
   // Overrides the Item implementation, because an article doesn't have a name but a title.
   public function getName() {
@@ -32,6 +41,25 @@ class Article extends Item {
   // Overrides the Item implementation, because an article doesn't have a name but a title.
   public function unsetName() {
     throw new \BadMethodCallException();
+  }
+
+
+  public function getTitle() {
+    return $this->meta[self::TITLE];
+  }
+
+  public function issetTitle() {
+    return isset($this->meta[self::TITLE]);
+  }
+
+
+  public function setTitle($value) {
+    $this->meta[self::TITLE] = $value;
+  }
+
+  public function unsetTitle() {
+    if ($this->isMetadataPresent(self::TITLE))
+      unset($this->meta[self::TITLE]);
   }
 
 }
