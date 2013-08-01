116b20dacda4e2a871fab7c922d8246bf7c1a8e7
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-01 16:58:46 +0200

    just added

---
+++ src/PitPress/Model/Accessory/Subscription.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file Subscription.php
+//! @brief This file contains the Subscription class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Model\Accessory;
+
+
+//! @brief This class is used to keep trace of the user subscriptions.
+//! @nosubgrouping
+class Subscription extends Star {}
