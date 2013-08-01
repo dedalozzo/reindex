18f4ac665a007f25842b89d7dd5974845f83a9a0
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-01 17:04:34 +0200

    we use this class to ignore tags

---
+++ src/PitPress/Model/Accessory/Ignore.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file Ignore.php
+//! @brief This file contains the Ignore class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Model\Accessory;
+
+
+//! @brief This class is used to keep trace of the user ignored tags.
+//! @nosubgrouping
+class Ignore extends Star {}
