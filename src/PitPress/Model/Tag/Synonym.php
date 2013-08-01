6d468d5acd2b82a85a078226ac48392675cd0ef6
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-01 17:09:39 +0200

    just added

---
+++ src/PitPress/Model/Tag/Synonym.php
@@ -0,0 +1,15 @@
+<?php
+
+//! @file Synonym.php
+//! @brief This file contains the Synonym class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Model\Tag;
+
+
+//! This class represents a synonym of a tag.
+class Synonym {
+
+} 
