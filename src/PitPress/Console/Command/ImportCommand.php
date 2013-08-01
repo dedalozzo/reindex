04087da9cf9bffba79c4ba338aa0891e3cef9c5d
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-01 17:05:56 +0200

    a lot of changes, implemented many new things, added markdown conversion, subscriptions import, etc.

--- src/PitPress/Console/Command/ImportCommand.php
+++ src/PitPress/Console/Command/ImportCommand.php
@@ -18,16 +18,18 @@ use PitPress\Model\Blog\Article;
 use PitPress\Model\Blog\Book;
 use PitPress\Model\Tag\Tag;
 use PitPress\Model\User\User;
+use PitPress\Model\Aggregate;
+use PitPress\Model\Accessory\Star;
 use PitPress\Model\Accessory\Classification;
+use PitPress\Model\Accessory\Subscription;
+
+use ElephantOnCouch\Generator\UUID;
 
 
 //! @brief Imports into CouchDB the data from Programmazione.it v6.4 MySQL database.
 //! @nosubgrouping
+//! @todo: Extract excerpt from body and store it.
 //! @todo: Download and save images as article attachments.
-//! @todo: Use the correlation code to threat related articles as pages of a tutorial.
-//! @todo: Create a ViewCount document using the hitNum.
-//! @todo: Import the ipAddress from User.
-//! @todo: Import the subscriptions.
 class ImportCommand extends AbstractCommand {
 
   const ARTICLE_DRAFT = 0;
@@ -47,6 +49,7 @@ class ImportCommand extends AbstractCommand {
 
   private $mysql;
   private $couch;
+  private $redis;
 
   private $input;
   private $output;
@@ -57,7 +60,7 @@ class ImportCommand extends AbstractCommand {
     $this->output->writeln("Importing users...");
 
     //$sql = "SELECT idMember, name AS firstName, surname AS lastName, nickName AS displayName, email, password, sex, birthDate AS birthday, ipAddress, confirmHash AS confirmationHash, confirmed AS authenticated, regDate AS creationDate, lastUpdate, avatarData, avatarType, realNamePcy FROM Member";
-    $sql = "SELECT idMember, name AS firstName, surname AS lastName, nickName AS displayName, email, password, sex, birthDate AS birthday, ipAddress, confirmHash AS confirmationHash, confirmed AS authenticated, regDate AS creationDate, lastUpdate, realNamePcy FROM Member";
+    $sql = "SELECT idMember, name AS firstName, surname AS lastName, nickName AS displayName, email, password, sex, UNIX_TIMESTAMP(birthDate) AS birthday, ipAddress, confirmHash AS confirmationHash, confirmed AS authenticated, UNIX_TIMESTAMP(regDate) AS creationDate, lastUpdate, realNamePcy FROM Member";
     $sql .= $this->limit;
 
     $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
@@ -75,6 +78,7 @@ class ImportCommand extends AbstractCommand {
       $user->email = utf8_encode($item->email);
       $user->password = utf8_encode($item->password);
       $user->birthday = utf8_encode($item->birthday);
+      $user->ipAddress = utf8_encode($item->ipAddress);
 
       $this->couch->saveDoc($user);
 
@@ -92,7 +96,7 @@ class ImportCommand extends AbstractCommand {
     $this->output->writeln("Importing articles...");
 
     // select idItem, title from Item where stereotype = 2 and  date < DATE('2005-12-07 12:12') order by date;
-    $sql = "SELECT idItem, title, body, date, hitNum, replyNum, stereotype, locked, contributorName, correlationCode, idMember FROM Item WHERE (stereotype = ".self::ARTICLE.") ORDER BY date ASC";
+    $sql = "SELECT id, idItem, title, body, UNIX_TIMESTAMP(date) AS unixTime, hitNum, replyNum, stereotype, locked, contributorName, correlationCode, idMember FROM Item WHERE (stereotype = ".self::ARTICLE.") ORDER BY date ASC";
     //$sql = "SELECT idItem, title, body, date, hitNum, replyNum, stereotype, locked, contributorName, correlationCode, idMember FROM Item WHERE (stereotype = ".self::ARTICLE.") AND idItem = 30806";
 
     $sql .= $this->limit;
@@ -106,11 +110,11 @@ class ImportCommand extends AbstractCommand {
     while ($item = mysqli_fetch_object($result)) {
       $article = new Article();
 
+      $article->id = $item->id;
+      $article->publishingDate = (int)$item->unixTime;
       $article->title = utf8_encode($item->title);
       $article->creator = utf8_encode($item->contributorName);
-      //$article->correlationCode = utf8_encode($item->contributorName);
 
-      // UTF encodes the body.
       $body = $item->body;
 
 
@@ -271,12 +275,129 @@ class ImportCommand extends AbstractCommand {
         $body = preg_replace('/___SNIPPET___/', PHP_EOL.trim($snippet).PHP_EOL, $body, 1);
       }
 
-      //echo $body;
+
+      // Converts from BBCode to Markdown!
+
+      // Replaces bold.
+      $body = preg_replace_callback('%\[b\]([\W\D\w\s]*?)\[/b\]%i',
+
+        function ($matches) use ($item) {
+          return "**".$matches[1]."**";
+        },
+
+        $body
+      );
+
+      // Replaces italic.
+      $body = preg_replace_callback('%\[i\]([\W\D\w\s]*?)\[/i\]%i',
+
+        function ($matches) use ($item) {
+          return "*".$matches[1]."*";
+        },
+
+        $body
+      );
+
+
+      // Replaces underline. Unfortunately Markdown doesn't support underline, so we just revert to normal text.
+      $body = preg_replace_callback('%\[u\]([\W\D\w\s]*?)\[/u\]%i',
+
+        function ($matches) use ($item) {
+          return $matches[1];
+        },
+
+        $body
+      );
+
+
+      // Replaces strikethrough.
+      $body = preg_replace_callback('%\[s\]([\W\D\w\s]*?)\[/s\]%i',
+
+        function ($matches) use ($item) {
+          return "~~".$matches[1]."~~";
+        },
+
+        $body
+      );
+
+
+      // Replaces lists.
+      $body = preg_replace_callback('%\[list(?P<type>=1)?\](?P<items>[\W\D\w\s]*?)\[/list\]%i',
+
+        function ($matches) use ($item) {
+          $buffer = "";
+
+          if (preg_match_all('/(?:(?![[*\]]).)*/i', $matches['items'], $listItems)) {
+
+            if (isset($matches['type']) && $matches['type'] == '=1') { // ordered list
+              foreach ($listItems as $itemMatch)
+                $buffer .= '- '.trim($itemMatch[1]).PHP_EOL;
+            }
+            else { // unordered list
+              $counter = count($listItems);
+              for ($i = 0; $i < $counter; $i++)
+                $buffer .= (string)($i + 1).'. '.trim($listItems[$i][1]).PHP_EOL;
+            }
+
+          }
+
+          return $buffer;
+        },
+
+        $body
+      );
+
+
+      // Replaces urls.
+      $body = preg_replace_callback('%\[\s*url\s*=\s*("(?:[^"]*")|\A[^\']*\Z|(?:[^\'">\s]+))\s*(?:[^]\s]*)\]([\W\D\w\s]*?)\[/url\]%i',
+
+        function ($matches) use ($item) {
+          if (isset($matches[1]) && isset($matches[2]))
+            return "[".$matches[2]."](".$matches[1].")";
+          else
+            throw new \RuntimeException(sprintf("Article with idItem = %d have malformed BBCode urls", $item->idItem));
+        },
+
+        $body
+      );
+
+
+      // Replaces images.
+      $body = preg_replace_callback('%\[\s*img\s*\]\s*("(?:[^"]*")|\A[^\']*\Z|(?:[^\'">\s]+))\s*(?:[^]\s]*)\[/img\]%i',
+
+        function ($matches) use ($item) {
+          if (isset($matches[1]))
+            return "![]"."(".$matches[1].")";
+          else
+            throw new \RuntimeException(sprintf("Article with idItem = %d have malformed BBCode images", $item->idItem));
+        },
+
+        $body
+      );
+
+
+      // Replaces code snippets.
+      $body = preg_replace_callback('%\[\s*code\s*(?P<language>=?\w*)\](?P<snippet>[\W\D\w\s]*?)\[/code\]%i',
+
+        function ($matches) use ($item) {
+          if (isset($matches['snippet']))
+            return "```".$matches['language'].PHP_EOL.$matches['snippet']."```".PHP_EOL;
+          else
+            throw new \RuntimeException(sprintf("Article with idItem = %d have malformed BBCode snippet", $item->idItem));
+        },
+
+        $body
+      );
+
 
       // This is the converted body.
       $article->body = utf8_encode($body);
 
+      // We finally save the article.
       $this->couch->saveDoc($article);
+
+      // We update the article views.
+      $this->redis->hSet($article->id, 'hits', $item->hitNum);
 
       $progress->advance();
     }
@@ -291,7 +412,7 @@ class ImportCommand extends AbstractCommand {
   private function importBooks() {
     $this->output->writeln("Importing books...");
 
-    $sql = "SELECT idItem, title, body, date, hitNum, replyNum, stereotype, locked, contributorName, idMember FROM Item WHERE (stereotype = ".self::BOOK.") ORDER BY date DESC";
+    $sql = "SELECT id, idItem, title, body, UNIX_TIMESTAMP(date) AS unixTime, hitNum, replyNum, stereotype, locked, contributorName, idMember FROM Item WHERE (stereotype = ".self::BOOK.") ORDER BY date DESC";
     $sql .= $this->limit;
 
     $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
@@ -303,7 +424,10 @@ class ImportCommand extends AbstractCommand {
     while ($item = mysqli_fetch_object($result)) {
       $book = new Book();
 
+      $book->id = $item->id;
+      $book->publishingDate = (int)$item->unixTime;
       $book->title = utf8_encode($item->title);
+      $book->creator = utf8_encode($item->contributorName);
 
       $body = $item->body;
 
@@ -331,9 +455,10 @@ class ImportCommand extends AbstractCommand {
       if (preg_match('/\\[vendorLink\\](.*?)\\[\/vendorLink\\]/s', $body, $matches) && !empty($matches[1]))
         $book->link = utf8_encode($matches[1]);
 
-      $book->creator = utf8_encode($item->contributorName);
-
       $this->couch->saveDoc($book);
+
+      // We update the book views.
+      $this->redis->hSet($book->id, 'hits', $item->hitNum);
 
       $progress->advance();
     }
@@ -348,7 +473,7 @@ class ImportCommand extends AbstractCommand {
   private function importTags() {
     $this->output->writeln("Importing tags...");
 
-    $sql = "SELECT idCategory, name, lastUpdate, passed FROM Category";
+    $sql = "SELECT id, idCategory, name, UNIX_TIMESTAMP(lastUpdate) AS unixTime, passed FROM Category";
     $sql .= $this->limit;
 
     $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
@@ -360,7 +485,9 @@ class ImportCommand extends AbstractCommand {
     while ($item = mysqli_fetch_object($result)) {
       $tag = new Tag();
 
-      $tag->name = utf8_encode(strtolower(strstr($item->name, " ", "-")));
+      $tag->id = $item->id;
+      $tag->publishingDate = (int)$item->unixTime;
+      $tag->name = utf8_encode(strtolower(str_replace(" ", "-", $item->name)));
 
       $this->couch->saveDoc($tag);
 
@@ -377,7 +504,7 @@ class ImportCommand extends AbstractCommand {
   private function importClassifications() {
     $this->output->writeln("Importing classifications...");
 
-    $sql = "SELECT I.id AS itemId, C.id AS tagId FROM Item I, Category C, ItemsXCategory X WHERE I.idItem = X.idItem AND C.idCategory = X.idCategory";
+    $sql = "SELECT I.id AS itemId, C.id AS tagId, I.stereotype AS stereotype, UNIX_TIMESTAMP(I.date) AS unixTime FROM Item I, Category C, ItemsXCategory X WHERE I.idItem = X.idItem AND C.idCategory = X.idCategory AND (I.stereotype = 2 OR I.stereotype = 11)";
     $sql .= $this->limit;
 
     $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
@@ -387,7 +514,124 @@ class ImportCommand extends AbstractCommand {
     $progress->start($this->output, $rows);
 
     while ($item = mysqli_fetch_object($result)) {
-      $doc = new Classification($item->itemId, $item->tagId);
+
+      if ($item->stereotype == 2)
+        $postType = 'article';
+      else
+        $postType = 'book';
+
+      $doc = Classification::create($item->itemId, $postType, 'blog', $item->tagId, (int)$item->unixTime);
+
+      $this->couch->saveDoc($doc);
+
+      $progress->advance();
+    }
+
+    mysqli_free_result($result);
+
+    $progress->finish();
+  }
+
+
+  //! @brief Imports favourites.
+  private function importFavourites() {
+    $this->output->writeln("Importing favourites...");
+
+    $sql = "SELECT I.id AS itemId, M.idMember AS userId, UNIX_TIMESTAMP(F.date) as timestamp FROM Item I, Member M, Favourite F WHERE I.idItem = F.idItem AND M.idMember = F.idMember";
+    $sql .= $this->limit;
+
+    $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
+
+    $rows = mysqli_num_rows($result);
+    $progress = $this->getApplication()->getHelperSet()->get('progress');
+    $progress->start($this->output, $rows);
+
+    while ($item = mysqli_fetch_object($result)) {
+      $timestamp = (int)$item->timestamp;
+
+      if ($timestamp > 0)
+        $doc = Star::create($item->itemId, $item->userId, $timestamp);
+      else
+        $doc = Star::create($item->itemId, $item->userId);
+
+      $this->couch->saveDoc($doc);
+
+      $progress->advance();
+    }
+
+    mysqli_free_result($result);
+
+    $progress->finish();
+  }
+
+
+  //! @brief Imports aggregates.
+  private function importAggregates() {
+    $this->output->writeln("Importing aggregates...");
+
+    $sql = "SELECT correlationCode, title, UNIX_TIMESTAMP(date) AS unixTime, contributorName, idMember FROM Item WHERE (stereotype = ".self::ARTICLE.") GROUP BY correlationCode HAVING COUNT(correlationCode) > 1 ORDER BY date ASC";
+    $sql .= $this->limit;
+
+    $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
+
+    $rows = mysqli_num_rows($result);
+    $progress = $this->getApplication()->getHelperSet()->get('progress');
+    $progress->start($this->output, $rows);
+
+    while ($item = mysqli_fetch_object($result)) {
+      $aggregate = new Aggregate();
+
+      $aggregate->id = UUID::generate(UUID::UUID_RANDOM, UUID::FMT_STRING);
+      $aggregate->publishingDate = (int)$item->unixTime;
+      $aggregate->title = utf8_encode(rtrim($item->title, '()/123456789 \t\n\r\0\x0B'));
+      $aggregate->creator = utf8_encode($item->contributorName);
+
+
+      $sql = "SELECT id, UNIX_TIMESTAMP(date) AS unixTime, hitNum FROM Item WHERE correlationCode = '".$item->correlationCode."' ORDER BY date ASC";
+
+      $related = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
+
+      $i = 0;
+      while ($article = mysqli_fetch_object($related)) {
+        $aggregate->addPost($article->id, $i);
+
+        // We update the total aggregate views.
+        $this->redis->hIncrBy($aggregate->id, 'hits', $article->hitNum);
+
+        $i++;
+      }
+
+      $this->couch->saveDoc($aggregate);
+
+      $progress->advance();
+    }
+
+    mysqli_free_result($result);
+
+    $progress->finish();
+  }
+
+
+  //! @brief Imports subscriptions.
+  private function importSubscriptions() {
+    $this->output->writeln("Importing subscriptions...");
+
+    $sql = "SELECT I.id AS itemId, M.idMember AS userId, UNIX_TIMESTAMP(T.creationTime) as timestamp FROM Item I, Member M, Thread T WHERE I.idItem = T.idItem AND M.idMember = T.idMember";
+    $sql .= $this->limit;
+
+    $result = mysqli_query($this->mysql, $sql) or die(mysqli_error($this->mysql));
+
+    $rows = mysqli_num_rows($result);
+    $progress = $this->getApplication()->getHelperSet()->get('progress');
+    $progress->start($this->output, $rows);
+
+    while ($item = mysqli_fetch_object($result)) {
+      $timestamp = (int)$item->timestamp;
+
+      if ($timestamp > 0)
+        $doc = Subscription::create($item->itemId, $item->userId, $timestamp);
+      else
+        $doc = Subscription::create($item->itemId, $item->userId);
 
       $this->couch->saveDoc($doc);
 
@@ -407,6 +651,9 @@ class ImportCommand extends AbstractCommand {
     $this->importBooks();
     $this->importTags();
     $this->importClassifications();
+    $this->importFavourites();
+    $this->importAggregates();
+    $this->importSubscriptions();
   }
 
 
@@ -417,12 +664,12 @@ class ImportCommand extends AbstractCommand {
     $this->addArgument("entities",
         InputArgument::IS_ARRAY | InputArgument::REQUIRED,
         "The entities you want import. Use 'all' if you want import all the entities, 'users' if you want just import the
-        users or separate multiple entities with a space. The available entities are: users, articles, books, tags");
+        users or separate multiple entities with a space. The available entities are: users, articles, books, tags.");
     $this->addOption("limit",
         NULL,
         InputOption::VALUE_OPTIONAL,
         "Limit the imported articles. This option is applied only when 'articles' argument is provided, alone or as a
-        part of the array, else is ignored");
+        part of the array, else is ignored.");
   }
 
 
@@ -430,6 +677,7 @@ class ImportCommand extends AbstractCommand {
   protected function execute(InputInterface $input, OutputInterface $output) {
     $this->mysql = $this->_di['mysql'];
     $this->couch = $this->_di['couchdb'];
+    $this->redis = $this->_di['redis'];
 
     $this->input = $input;
     $this->output = $output;
@@ -468,11 +716,25 @@ class ImportCommand extends AbstractCommand {
           case 'classifications':
             $this->importClassifications();
             break;
+
+          case 'favourites':
+            $this->importFavourites();
+            break;
+
+          case 'aggregates':
+            $this->importAggregates();
+            break;
+
+          case 'subscriptions':
+            $this->importSubscriptions();
+            break;
         }
 
     }
     else
       $this->importAll();
+
+    $this->couch->ensureFullCommit();
   }
 
 }
