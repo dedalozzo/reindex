d15210b2362c847c2447083ca8db6393c7caf5a0
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-01 16:58:36 +0200

    removed constructor and added the static method Create()

--- src/PitPress/Model/Accessory/Star.php
+++ src/PitPress/Model/Accessory/Star.php
@@ -13,19 +13,20 @@ use ElephantOnCouch\Doc\Doc;
 
 
 //! @brief This class is used to keep trace of the user favourites.
-//! @details Every user can add a document to his favourites. So we need to know the favourites list, and we can do it
-//! using a view that emit as key the user id and as value the item id. We also need a way to show if a particular item
-//! has been added by the logged user. Another view will emit as key an array composed by the item id plus the user id.
-//! This is a classic many-to-many relationship.
-//! Each time a document is deleted, we must delete all the favourites related; when instead a user is deleted, we must
-//! delete all the emitted favourites.
 //! @nosubgrouping
 class Star extends Doc {
 
-  public function __construct($itemId, $userId) {
-    $this->meta["itemId"] = $itemId;
-    $this->meta["userId"] = $userId;
-    $this->meta["timestamp"] = time();
+  //! @brief Creates an instance of Star class.
+  public static function create($itemId, $userId, $timestamp = NULL) {
+    $instance = new self();
+
+    $instance->meta["itemId"] = $itemId;
+    $instance->meta["userId"] = $userId;
+
+    if (is_null($timestamp))
+      $instance->meta["timestamp"] = time();
+
+    return $instance;
   }
 
 }
