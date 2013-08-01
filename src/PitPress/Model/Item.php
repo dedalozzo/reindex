0dc8a67cc3f9f821f22a99fecb47071dd0dd55d8
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-01 17:11:38 +0200

    added redis and counting hits methods

--- src/PitPress/Model/Item.php
+++ src/PitPress/Model/Item.php
@@ -1,6 +1,7 @@
 <?php
 
-//! @file Item.php@brief This file contains the Item class.
+//! @file Item.php
+//! @brief This file contains the Item class.
 //! @details
 //! @author Filippo F. Fadda
 
@@ -18,18 +19,42 @@ use Phalcon\DI;
 abstract class Item extends Doc {
   protected $di; // Stores the default Dependency Injector.
   protected $couch; // Stores the ElephantOnCouch client instance.
+  protected $redis; // Stores the Redis client instance.
 
 
   //! @brief Constructor.
   public function __construct() {
     $this->di = DI::getDefault();
     $this->couch = $this->di['couchdb'];
+    $this->redis = $this->di['redis'];
   }
 
 
-  //! @brief Returns the creation date.
-  public function getCreationDate() {
-    return $this->meta['creationDate'];
+  //! @name Hits Counting Methods
+  // @{
+
+  //! @brief Returns the times the item has been viewed.
+  public function getHitsCount() {
+    if (isset($this->rev))
+      return $this->redis->hGet($this->id, 'hits');
+    else
+      return 0;
+  }
+
+
+  //! @brief Increments the times the item has been viewed.
+  public function incHits() {
+    // We can increment the views of a document that has been already saved.
+    if (isset($this->rev))
+      $this->redis->hIncrBy($this->id, 'hits', 1);
+  }
+
+  //@}
+
+
+  //! @brief Returns the creation timestamp.
+  public function getTimestamp() {
+    return $this->meta['timestamp'];
   }
 
 
