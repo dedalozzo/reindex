29b8a9e0b993cfd682cd4ea550b95b07f0d2e517
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-08 22:28:07 +0200

    added a custom render to provide syntax highlighting

---
+++ src/PitPress/Render/SyntaxHighlighter.php
@@ -0,0 +1,27 @@
+<?php
+
+//! @file SyntaxHighlighter.php
+//! @brief This file contains the SyntaxHighlighter class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+//! @brief The Sundown renders namespace.
+namespace PitPress\Render;
+
+
+use Sundown\Render\HTML;
+use Pygmentize\Pygmentize;
+
+
+//! @brief This handler override the blockCode() method of Sundown HTML render, to highlight the source code using
+//! Pygmentize class.
+class SyntaxHighlighter extends HTML {
+
+  public function blockCode($code, $language) {
+    $highlightedSource = Pygmentize::highlight($code, $language);
+
+    return $highlightedSource;
+  }
+
+} 
