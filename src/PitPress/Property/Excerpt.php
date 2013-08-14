0f934611552df33c8baaad02d4565751103bbc92
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-15 00:09:41 +0200

    removed truncate() and purge(), moved to Text helper

--- src/PitPress/Property/Excerpt.php
+++ src/PitPress/Property/Excerpt.php
@@ -12,44 +12,6 @@ namespace PitPress\Property;
 //! @cond HIDDEN_SYMBOLS
 trait Excerpt {
 
-  //! @brief Cuts a string to a given number of characters without breaking words.
-  //! @param[in] string $text The input string.
-  //! @param[in] integer $length The number of characters at which the string will be wrapped, ex. 200 characters.
-  //! @param[in] string $etc The characters you want append to the end of text.
-  //! @param[in] boolean $breakWords If <i>true</i> breaks the words to return the exact number of chars.
-  //! @param[in] boolean $middle Truncates the text but remove middle instead the end of the string.
-  function truncate($text, $length = 200, $etc = '...', $charset='UTF-8', $breakWords = FALSE, $middle = FALSE) {
-    if ($length == 0)
-      return '';
-
-    if (mb_strlen($text) > $length) {
-      $length -= min($length, mb_strlen($etc));
-
-      if (!$breakWords && !$middle)
-        $text = preg_replace('/\s+?(\S+)?$/u', '', mb_substr($text, 0, $length+1, $charset));
-
-      if(!$middle)
-        return mb_substr($text, 0, $length, $charset) . $etc;
-      else
-        return mb_substr($text, 0, $length/2, $charset) . $etc . mb_substr($text, -$length/2, (mb_strlen($text)-$length/2), $charset);
-    }
-    else
-      return $text;
-  }
-
-
-  //! @brief Removes the content of pre tags, than strip all tags.
-  //! @param[in] string $text The input string.
-  function purge($text) {
-    // Removes the content of <pre></pre>.
-    $text = preg_replace('/<(pre)(?:(?!<\/\1).)*?<\/\1>/s', '', $text);
-
-    // Removes all the HTML tags.
-    $text = strip_tags($text);
-
-    return $text;
-  }
-
 
   public function getExcerpt() {
     return $this->meta['excerpt'];
