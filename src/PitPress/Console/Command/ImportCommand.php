3259f81365b4c53c4f2fd0aaadedd8961424f6fd
Filippo Fadda <filippo.fadda@gmail.com>
2013-08-25 00:07:14 +0200

    removed $input and $output members

--- src/PitPress/Console/Command/ImportCommand.php
+++ src/PitPress/Console/Command/ImportCommand.php
@@ -57,9 +57,6 @@ class ImportCommand extends AbstractCommand {
   private $redis;
   private $markdown;
 
-  private $input;
-  private $output;
-
 
   //! @brief Imports users.
   private function importUsers() {
@@ -487,9 +484,6 @@ class ImportCommand extends AbstractCommand {
     $this->couch = $this->_di['couchdb'];
     $this->redis = $this->_di['redis'];
     $this->markdown = $this->_di['markdown'];
-
-    $this->input = $input;
-    $this->output = $output;
 
     $entities = $input->getArgument('entities');
     $limit = (int)$input->getOption('limit');
