61f07e8c0c577a411066d10475cfc72e13670cc8
Filippo Fadda <filippo.fadda@gmail.com>
2013-10-28 16:50:39 +0100

    fixed the reduce() method and added $couch2 connection

--- src/PitPress/Hook/UpdateScoreHook.php
+++ src/PitPress/Hook/UpdateScoreHook.php
@@ -9,6 +9,7 @@
 //! @brief This is the hooks namespace
 namespace PitPress\Hook;
 
+use Phalcon\CLI\Console\Exception;
 use Phalcon\DI\InjectionAwareInterface;
 
 use ElephantOnCouch\Hook\ChunkHook;
@@ -23,12 +24,14 @@ use PitPress\Model\Accessory\Score;
 class UpdateScoreHook implements ChunkHook, InjectionAwareInterface {
 
   private $di;
+  private $config;
   private $logger;
   private $couch;
+  private $couch2;
 
   private $votes = [];
   private $counter = 0;
-  private $limit = 25;
+  private $limit = 10000;
 
   private $firstChunkRead = FALSE;
   private $lastChunkRead = FALSE;
@@ -36,14 +39,22 @@ class UpdateScoreHook implements ChunkHook, InjectionAwareInterface {
 
   private function reduce() {
     $reductions = [];
-    foreach ($this->votes as $vote)
-      $reductions[$vote->postId] += $vote->value;
+    foreach ($this->votes as $vote) {
+
+      if (isset($reductions[$vote->postId]))
+        $reductions[$vote->postId] += $vote->getValue();
+      else
+        $reductions[$vote->postId] = $vote->getValue();
+
+      $vote->markAsRecorded();
+    }
 
     return $reductions;
   }
 
 
   public function process($chunk) {
+    $chunk = trim($chunk, ",\r\n");
 
     if (preg_match('/\A[{].+[}]\z/i', $chunk)) {
       $array = ArrayHelper::fromJson($chunk, TRUE)['doc'];
@@ -72,24 +83,26 @@ class UpdateScoreHook implements ChunkHook, InjectionAwareInterface {
       // Scores.
       $opts = new ViewQueryOpts();
       $opts->includeMissingKeys()->doNotReduce()->includeDocs();
-      $scores = $this->couch->queryView("scores", "perPost", $keys, $opts)['rows'];
+      $scores = $this->couch2->queryView("scores", "perPost", $keys, $opts)['rows'];
 
       $counter = count($keys);
       for ($i = 0; $i < $counter; $i++) {
 
         if (isset($scores[$i]['id'])) {
           $score = new Score();
-          $array = ArrayHelper::fromJson($scores[$i], TRUE)['doc'];
-          $score->assignArray($array);
+          $score->assignArray($scores[$i]['doc']);
           $score->addPoints($partialScores[$i]);
           $docs[] = $score;
         }
         else {
-          $post = $this->couch->getDoc(Couch::STD_DOC_PATH, $keys[$i]);
+          $post = $this->couch2->getDoc(Couch::STD_DOC_PATH, $keys[$i]);
 
           if (is_object($post)) {
             $score = Score::create($post->section, $post->type, $post->id, $post->publishingDate, $partialScores[$i]);
             $docs[] = $score;
+          }
+          else {
+            throw new Exception("Non esiste un post idenficato dall'id");
           }
         }
       }
@@ -97,7 +110,8 @@ class UpdateScoreHook implements ChunkHook, InjectionAwareInterface {
       $this->counter = 0;
 
       $docs = array_merge($docs, $this->votes);
-      $this->couch->performBulkOperations($docs, FALSE, TRUE, FALSE);
+      $this->couch2->performBulkOperations($docs, FALSE, TRUE);
+      unset($this->$votes);
     }
 
   }
@@ -106,8 +120,12 @@ class UpdateScoreHook implements ChunkHook, InjectionAwareInterface {
   //! @brief Sets the Dependency Injector.
   public function setDi($di) {
     $this->di = $di;
+    $this->config = $this->di['config'];
     $this->logger = $this->di['logger'];
-    $this->couch = $this->di['couch'];
+    $this->couch = $this->di['couchdb'];
+
+    $this->couch2 = new Couch($this->config->couchdb->ipAddress.":".$this->config->couchdb->port, $this->config->couchdb->user, $this->config->couchdb->password, FALSE);
+    $this->couch2->selectDb($this->config->couchdb->database);
   }
 
 
