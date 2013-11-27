c8c9a6ee42df51641c7150f34b1ca9b0e4429f1d
Filippo Fadda <filippo.fadda@gmail.com>
2013-11-27 02:39:44 +0100

    just added

---
+++ src/PitPress/Model/Activity/AbstractActivity.php
@@ -0,0 +1,16 @@
+<?php
+
+//! @file AbstractActivity.php
+//! @brief This file contains the AbstractActivity class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+//! @brief This is the activities namespace.
+namespace Activity;
+
+
+//! @brief todo
+abstract class AbstractActivity {
+
+}
