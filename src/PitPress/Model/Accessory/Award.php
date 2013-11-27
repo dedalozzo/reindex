4a57bfa922a3c804ff686a15e15d6b7d8c5c9e15
Filippo Fadda <filippo.fadda@gmail.com>
2013-11-27 02:39:54 +0100

    just added

---
+++ src/PitPress/Model/Accessory/Award.php
@@ -0,0 +1,31 @@
+<?php
+
+//! @file Award.php
+//! @brief This file contains the Award class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Model\Accessory;
+
+
+use ElephantOnCouch\Doc\Doc;
+
+
+//! @brief This class is used to keep trace of user awards.
+//! @details An award can be assigned once or multiple times, depends of the badge type.
+//! @nosubgrouping
+class Award extends Doc {
+
+  //! @brief Creates an instance of Award class.
+  public static function create($userId, $badge) {
+    $instance = new self();
+
+    $instance->meta["userId"] = $userId;
+    $instance->meta["badgeClass"] = $badge;
+    $instance->meta["timestamp"] = time();
+
+    return $instance;
+  }
+
+} 
