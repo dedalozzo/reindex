9fc4888c884a4478f3e93fe50e02b072e3de14dc
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-13 15:46:01 +0100

    removed sign in route and action

--- src/PitPress/Route/IndexGroup.php
+++ src/PitPress/Route/IndexGroup.php
@@ -27,7 +27,6 @@ class IndexGroup extends Group {
       ]);
 
     $this->addGet('/', ['action' => 'popular']);
-    $this->addGet('/accedi/', ['action' => 'signIn']);
 
     // All the following routes start with /aggiornamenti.
     $this->setPrefix('/aggiornamenti');
