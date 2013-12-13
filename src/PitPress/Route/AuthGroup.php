66dfe23a2745449fc6fac8be6afefa8136997e2b
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-13 15:45:18 +0100

    added auth controller and group

---
+++ src/PitPress/Route/AuthGroup.php
@@ -0,0 +1,41 @@
+<?php
+
+//! @file AuthGroup.php
+//! @brief Group of Updates routes.
+//! @details
+//! @author Filippo F. Fadda
+
+
+//! @brief PitPress routes namespace.
+namespace PitPress\Route;
+
+
+use Phalcon\Mvc\Router\Group;
+
+
+//! @brief Group of authentication routes.
+//! @nosubgrouping
+class AuthGroup extends Group {
+
+
+  public function initialize() {
+    // Sets the default controller for the following routes.
+    $this->setPaths(
+      [
+        'namespace' => 'PitPress\Controller',
+        'controller' => 'auth'
+      ]);
+
+    // All the following routes start with /login.
+    $this->setPrefix('/login');
+
+    //$this->addGet('/login/{provider}', ['action' => 'login']);
+    //$this->addGet('/{provider}', ['action' => 'authenticate']);
+    
+    $this->addGet('/facebook/{params}', ['action' => 'facebook']);
+    $this->addGet('/google/{params}', ['action' => 'google']);
+    $this->addGet('/linkedin/{params}', ['action' => 'linkedin']);
+    $this->addGet('/github/{params}', ['action' => 'github']);
+  }
+
+}
