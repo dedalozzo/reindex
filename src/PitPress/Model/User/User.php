49b2a868fa250e2785ce82e7073c655da186ab1c
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-15 15:04:48 +0100

    added many authentication methods

--- src/PitPress/Model/User/User.php
+++ src/PitPress/Model/User/User.php
@@ -59,6 +59,117 @@ class User extends Storable implements Extension\ICount {
   //! @name Authentication Methods
   // @{
 
+  public static function login($email, $password) {
+    $error = null;
+
+    $login = strtolower($email);
+    $pass = md5($password);
+
+    $sql = "SELECT idMember, ipAddress, regDate, confirmed FROM Member WHERE nickName = '$login' AND password = '$pass'";
+
+    /*if ($result = mysql_query($sql, $connection))
+      if (mysql_num_rows($result)) {
+        $row = mysql_fetch_object($result);
+        if ($row->confirmed) {
+          $sql = "UPDATE Member SET ipAddress = '".$_SERVER['REMOTE_ADDR']."' WHERE idMember = ".$row->idMember;
+          if (mysql_query($sql, $connection)) {
+            $str = $row->idMember.$_SERVER['REMOTE_ADDR'].$row->regDate;
+            $md5 = md5(crypt($str, 'jzojhghgfd'));
+
+            // to avoid Internet Explorer 6.x implementation issues
+            header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
+            header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
+
+            setcookie("id", $row->idMember, mktime(0, 0, 0, 12, 12, 2030), "/", constant('DOMAIN'));
+            setcookie("md5", $md5, mktime(0, 0, 0, 12, 12, 2030), "/", constant('DOMAIN'));
+          }
+          else
+            $error = "C'� stato un errore nella procedura di login, riprova tra un minuto, grazie.";
+        }
+        else
+          $error = "L'utente risulta iscritto, ma l'iscrizione non � ancora stata confermata. Segui le istruzioni ricevute nella e-mail di attivazione che ti � stata inviata. Se ancora non l'hai ricevuta, richiedi una nuova <a href=\"index.php?entity=eactivationemail\" target=\"_self\">e-mail di attivazione</a>.";
+      }
+      else
+        $error = "Non vi � nessun utente registrato con la login inserita o la password � errata.";
+    else $error = mysql_error();
+    */
+
+    if (isset($error)) { // reset cookies
+      // to avoid Internet Explorer 6.x implementation issues
+      header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
+      header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
+
+      setcookie("id", "", time(), "/", constant('DOMAIN'));
+      setcookie("md5", "", time(), "/", constant('DOMAIN'));
+    }
+
+    //mysql_free_result($result);
+    return $error;
+  }
+
+
+  public function logout() {
+    // to avoid Internet Explorer 6.x implementation issues
+    header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
+    header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
+
+    // set a null cookie and redirect to the home page
+    setcookie("id", "", 0, "/", constant('DOMAIN'));
+    setcookie("md5", "", 0, "/", constant('DOMAIN'));
+    setcookie("test", "", 0, "/", constant('DOMAIN'));
+
+    header("Location: index.php");
+    exit;
+  }
+
+
+  public function activate($confirmationHash) {
+    //$sql = "SELECT idMember, UNIX_TIMESTAMP(lastUpdate), confirmed, email FROM Member WHERE confirmHash = '".mysql_real_escape_string($confirmHash)."'";
+    /*$result = mysql_query($sql, $connection) or die(mysql_error());
+
+    if ($row = mysql_fetch_row($result)) {
+      if ($row[2])
+        go_to("index.php?entity=elogin"); // member already activated
+      elseif (((strtotime("now") - $row[1]) / 3600) > 24) // the hash is expired
+        go_to("index.php?entity=eitem&idItem=29108"); // hash code is expired
+      else {
+        $sql = "UPDATE Member SET confirmed = 1, lastUpdate = NOW() WHERE idMember = ".$row[0];
+        mysql_query($sql, $connection) or die(mysql_error());
+
+        $error = subscribeNewsletter($row[3]);
+        if (isset($error))
+          go_to("index.php?entity=eitem&idItem=29109"); // unable to send newsletter subscription email
+        else
+          go_to("index.php?entity=elogin"); // member activated
+      }
+    }
+    else
+      go_to("index.php?entity=eitem&idItem=29110"); // invalid hash code
+    */
+  }
+
+
+  public function facebookConnect() {
+
+  }
+
+
+  public function googleConnect() {
+
+  }
+
+
+  public function linkedinConnect() {
+
+  }
+
+
+  public function githubConnect() {
+
+  }
+
+
+
   //! @brief Authenticate the user.
   public function authenticate() {
     $this->meta['authenticated'] = "true";
