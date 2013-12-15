55bf236299b0fe828e1ea3ad401aa8b111cb218c
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-16 00:43:53 +0100

    added the crypt service

---
+++ services/crypt.php
@@ -0,0 +1,13 @@
+<?php
+
+//! @file crypt.php
+//! @brief Creates a crypt component instance.
+//! @details
+//! @author Filippo F. Fadda
+
+
+$di->set('crypt', function() use ($config) {
+  $crypt = new Phalcon\Crypt();
+  $crypt->setKey($config->application->key);
+  return $crypt;
+});
