7ffb12e4406dd395e57a94711b901554522d8dda
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-27 15:26:15 +0100

    just added all the authentication exceptions

---
+++ src/PitPress/Exception/UserNotConfirmedException.php
@@ -0,0 +1,13 @@
+<?php
+
+//! @file UserNotConfirmedException.php
+//! @brief This file contains the UserNotConfirmedException class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Exception;
+
+
+//! @brief Exception thrown when the user hasn't been confirmed yet.
+class UserNotConfirmedException extends \RuntimeException {}
