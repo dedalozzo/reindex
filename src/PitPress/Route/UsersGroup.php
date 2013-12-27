3ad77d39107ed0f6e3e49c946921e1f2710e9ed6
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-27 15:22:04 +0100

    removed sign in/up actions and routes

--- src/PitPress/Route/UsersGroup.php
+++ src/PitPress/Route/UsersGroup.php
@@ -28,10 +28,6 @@ class UsersGroup extends Group {
     $this->setHostName('utenti.programmazione.me');
 
     $this->addGet('/', ['action' => 'reputation']);
-    $this->addGet('/accedi/', ['action' => 'signin']);
-    $this->addGet('/registrati/', ['action' => 'signUp']);
-    $this->addGet('/resetta-password/', ['action' => 'resetPassword']);
-    $this->addGet('/invia-email-attivazione/', ['action' => 'sendActivationEmail']);
     $this->addGet('/{id}', ['action' => 'show']);
     $this->addGet('/reputazione/{period}', ['action' => 'reputation']);
     $this->addGet('/nuovi/', ['action' => 'newest']);
