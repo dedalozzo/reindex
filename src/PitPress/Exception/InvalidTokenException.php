93e8790a0f4e360e1fb2c74373b06535743c1f4e
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-27 15:26:15 +0100

    just added all the authentication exceptions

---
+++ src/PitPress/Exception/InvalidTokenException.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file InvalidTokenException.php
+//! @brief This file contains the InvalidTokenException class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+//! @brief This is the namespace of exceptions.
+namespace PitPress\Exception;
+
+
+//! @brief Exception thrown when an invalid token is provided.
+class InvalidTokenException extends \RuntimeException {}
