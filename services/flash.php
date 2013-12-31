11ab966fa5b78e0ff4a650aae13ff4c33e7a1ac1
Filippo Fadda <filippo.fadda@gmail.com>
2013-12-31 23:29:04 +0100

    added the flash service

---
+++ services/flash.php
@@ -0,0 +1,19 @@
+<?php
+
+//! @file flash.php
+//! @brief Creates the flash component.
+//! @details
+//! @author Filippo F. Fadda
+
+
+use Phalcon\Flash\Session as Flash;
+
+
+// Returns the flash instance.
+$di->setShared('flash', function() {
+  return new Flash([
+    'error' => 'alert alert-error',
+    'success' => 'alert alert-success',
+    'notice' => 'alert alert-info',
+  ]);
+});
