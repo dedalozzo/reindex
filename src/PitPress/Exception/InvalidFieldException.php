fdd6d85c16ac709bd403dd32f2fbcf39fa289879
Filippo Fadda <filippo.fadda@gmail.com>
2014-01-19 17:27:40 +0100

    this new exception is raised when the validation process fails

---
+++ src/PitPress/Exception/InvalidFieldException.php
@@ -0,0 +1,13 @@
+<?php
+
+//! @file InvalidFieldException.php
+//! @brief This file contains the InvalidFieldException class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Exception;
+
+
+//! @brief Exception thrown when an incomplete field or an invalid value for the field is provided.
+class InvalidFieldException extends \RuntimeException {}
