ff149e9643b954999c2422f3d3e9bbbe2eacf159
Filippo Fadda <filippo.fadda@gmail.com>
2014-02-24 15:55:03 +0100

    implemented and defined some methods

--- src/PitPress/Model/Badge/Badge.php
+++ src/PitPress/Model/Badge/Badge.php
@@ -10,6 +10,47 @@
 namespace PitPress\Model\Badge;
 
 
-abstract class Badge {
+use PitPress\Model\Activity\Activity;
+
+use ElephantOnCouch\Doc\Doc;
+
+
+//! @brief This is the ancestor of all badges, it's abstract and can't be instantiated.
+//! @details Badge implements the observer pattern.
+abstract class Badge extends Doc {
+
+  protected $subject;
+
+
+  //! @brief Creates an instance of the badge.
+  //! @details This function is used internally
+  public static function create(Activity $subject) {
+    $obj = new static();
+    $obj->subject = $subject;
+    return $obj;
+  }
+
+
+  //! @brief Returns the complete class name, including his namespace.
+  public static function getClass() {
+    return __CLASS__;
+  }
+
+
+  //! @brief Every time an activity is performed by a user, this method is called for all the interested badges. The
+  //! badge in fact acts like an observer while the activity is the subject. This is a variant of the observer pattern.
+  //! subject can update the badge state in relation to his changes.
+  public static function update(Activity $subject) {
+
+  }
+
+
+  //! @brief Awards the current badge.
+  public abstract function award();
+
+
+  //! @brief Withdrawn the badge previously awarded.
+  //! @details Only some badges might be retired.
+  public abstract function withdrawn();
 
 } 
