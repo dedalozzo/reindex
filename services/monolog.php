6b7c0aa23235d53af721246b55b536cf0f01ae66
Filippo Fadda <filippo.fadda@gmail.com>
2014-03-17 23:03:27 +0100

    replaced the standard Phalcon logger with Monolog and added the Monolog dependency

---
+++ services/monolog.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file monolog.php
+//! @brief Just a trick to return the logger.
+//! @details
+//! @author Filippo F. Fadda
+
+
+// Returns a Monolog instance.
+$di->setShared('monolog',
+  function() use ($monolog) {
+    return $monolog;
+  }
+);
