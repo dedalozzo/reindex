ad61e6a49a9e56c201beb6ee40fe11d709751683
Filippo Fadda <filippo.fadda@gmail.com>
2014-04-14 20:44:17 +0200

    now the host name is dynamic

--- src/PitPress/Route/AuthGroup.php
+++ src/PitPress/Route/AuthGroup.php
@@ -11,6 +11,7 @@ namespace PitPress\Route;
 
 
 use Phalcon\Mvc\Router\Group;
+use Phalcon\DI;
 
 
 //! @brief Group of authentication routes.
@@ -26,7 +27,7 @@ class AuthGroup extends Group {
         'controller' => 'auth'
       ]);
 
-    $this->setHostName('programmazione.me');
+    $this->setHostName(DI::getDefault()['config']['application']['domainName']);
 
     $this->addGet('/registrati/', ['action' => 'signUp']);
     $this->addGet('/resetta-password/', ['action' => 'resetPassword']);
