f1425d27c27842159f454184f6122395a22ab55e
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-08 15:33:36 +0200

    the SyntaxHighlighter class is now deprecated

--- src/PitPress/Render/SyntaxHighlighter.php
+++ src/PitPress/Render/SyntaxHighlighter.php
@@ -16,6 +16,7 @@ use Pygmentize\Pygmentize;
 
 //! @brief This handler override the blockCode() method of Sundown HTML render, to highlight the source code using
 //! Pygmentize class.
+//! @deprecated The SyntaxHighlighter is deprecated due to change Sundown with Hoedown.
 class SyntaxHighlighter extends HTML {
 
   public function blockCode($code, $language) {
