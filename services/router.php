793308f8e51c4c36b146f7fc904c59faf845555f
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-08 15:33:08 +0200

    added the user's profile routes and his relative group of routes

--- services/router.php
+++ services/router.php
@@ -27,13 +27,14 @@ $di->setShared('router',
     );
 
     $router->mount(new Route\IndexGroup());
-    $router->mount(new Route\AuthGroup());
     $router->mount(new Route\BlogGroup());
     $router->mount(new Route\QuestionsGroup());
     $router->mount(new Route\LinksGroup());
     $router->mount(new Route\TagsGroup());
     $router->mount(new Route\BadgesGroup());
     $router->mount(new Route\UsersGroup());
+    $router->mount(new Route\AuthGroup());
+    $router->mount(new Route\ProfileGroup());
 
     return $router;
   }
