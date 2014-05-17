9b0382de382642db8e784f552fea213724ecd41e
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-17 17:37:05 +0200

    overrided the save() method to set the supertype

--- src/PitPress/Model/Forum/Comment.php
+++ src/PitPress/Model/Forum/Comment.php
@@ -1,7 +1,7 @@
 <?php
 
-//! @file Replayphp
-//! @brief This file contains the Replay class.
+//! @file Comment.php
+//! @brief This file contains the Comment class.
 //! @details
 //! @author Filippo F. Fadda
 
@@ -9,10 +9,24 @@
 namespace PitPress\Model\Forum;
 
 
-use PitPress\Model\Replay as AbstractComment;
+use PitPress\Model\Reply;
 
 
-class Comment extends AbstractComment {
+//! @brief A comment is only used on questions and answers. A comment is a way to ask more information to the question's
+//! author, to be able to provide the right answer to the question. All the comments are listed below the question (or
+//! the answer) before any answers is showed.
+//! @nosubgrouping
+class Comment extends Reply {
+
+  public function save() {
+    // We don't use the supertype `reply`, because a comment is different from a reply.
+    $this->meta['supertype'] = 'comment';
+
+    // We need to bypass the father's method and call instead the grandpa one. This is done to avoid override the
+    // supertype we just assigned.
+    parent::save(TRUE);
+  }
+
 }
 
 
