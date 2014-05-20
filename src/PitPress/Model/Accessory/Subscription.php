265fd831f4ccbb43c62afe3b5beb0f67684256da
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-20 02:49:10 +0200

    no longer inherits from Star but Doc

--- src/PitPress/Model/Accessory/Subscription.php
+++ src/PitPress/Model/Accessory/Subscription.php
@@ -9,6 +9,24 @@
 namespace PitPress\Model\Accessory;
 
 
+use ElephantOnCouch\Doc\Doc;
+
+
 //! @brief This class is used to keep trace of the user subscriptions.
 //! @nosubgrouping
-class Subscription extends Star {}
+class Subscription extends Doc {
+
+  //! @brief Creates an instance of Star class.
+  public static function create($userId, $itemId, $timestamp = NULL) {
+    $instance = new self();
+
+    $instance->meta["userId"] = $userId;
+    $instance->meta["itemId"] = $itemId;
+
+    if (is_null($timestamp))
+      $instance->meta["timestamp"] = time();
+
+    return $instance;
+  }
+
+}
