0a1092bbe53f934e9ceb91284415d7353e5bf71b
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-20 02:54:23 +0200

    implemented favoritesAction and did many changes to the timelineAction()

--- src/PitPress/Controller/ProfileController.php
+++ src/PitPress/Controller/ProfileController.php
@@ -21,10 +21,11 @@ class ProfileController extends ListController {
 
   // Stores the typology sub-menu definition.
   protected static $typologySubMenu = ['guide', 'libri', 'articoli', 'domande', 'links', 'tutti'];
+  protected static $typologyCorrespondence = ['guide' => 'tutorial', 'libri' => 'book', 'articoli' => 'article', 'domande' => 'question', 'links' => 'link'];
 
 
   //! @brief Displays the newest user's updates.
-  public function timelineAction($id) {
+  public function timelineAction($id, $type = NULL) {
     // If no user id is provided, shows all the users.
     if (empty($id))
       return $this->dispatcher->forward(
@@ -51,18 +52,26 @@ class ProfileController extends ListController {
 
     $this->view->setVar('title', $doc->displayName);
 
+    if (empty($type))
+      $type = 'tutti';
 
-    $opts = new ViewQueryOpts();
-    $opts->doNotReduce()->setLimit(30)->reverseOrderOfResults()->setStartKey([$doc->id, new \stdClass()])->setEndKey([$doc->id]);
-    $rows = $this->couch->queryView("posts", "newestByUser", NULL, $opts)['rows'];
+    if ($type == 'tutti') {
+      $opts = new ViewQueryOpts();
+      $opts->doNotReduce()->setLimit(30)->reverseOrderOfResults()->setStartKey([$doc->id, new \stdClass()])->setEndKey([$doc->id]);
+      $rows = $this->couch->queryView("posts", "newestByUser", NULL, $opts)['rows'];
+    }
+    else {
+      $typology = self::$typologyCorrespondence[$type];
+
+      $opts = new ViewQueryOpts();
+      $opts->doNotReduce()->setLimit(30)->reverseOrderOfResults()->setStartKey([$doc->id, $typology, new \stdClass()])->setEndKey([$doc->id, $typology]);
+      $rows = $this->couch->queryView('posts', 'newestByUserPerType', NULL, $opts)['rows'];
+    }
 
     $this->view->setVar('entries', $this->getEntries(array_column($rows, 'id')));
 
     $stat = new Stat();
     $this->view->setVar('entriesCount', $stat->getUpdatesCount());
-
-    if (empty($type))
-      $type = 'tutti';
 
     $this->view->setVar('subsectionMenu', self::$typologySubMenu);
     $this->view->setVar('subsectionIndex', array_flip(self::$typologySubMenu)[$type]);
@@ -82,8 +91,57 @@ class ProfileController extends ListController {
   }
 
 
-  public function favoritesAction($id) {
+  public function favoritesAction($id, $type = NULL) {
+    // If no user id is provided, shows all the users.
+    if (empty($id))
+      return $this->dispatcher->forward(
+        [
+          'controller' => 'users',
+          'action' => 'reputation'
+        ]);
 
+    $opts = new ViewQueryOpts();
+    $opts->setKey($id)->setLimit(1);
+    $rows = $this->couch->queryView("users", "allNames", NULL, $opts)['rows'];
+
+    // If the user doesn't exist, forward to 404.
+    if (empty($rows))
+      return $this->dispatcher->forward(
+        [
+          'controller' => 'error',
+          'action' => 'show404'
+        ]);
+
+    $doc = $this->couchdb->getDoc(Couch::STD_DOC_PATH, $id);
+    $this->view->setVar('doc', $doc);
+
+    $this->view->setVar('title', $doc->displayName);
+
+    if (empty($type))
+      $type = 'tutti';
+
+    if ($type == 'tutti') {
+      $opts = new ViewQueryOpts();
+      $opts->doNotReduce()->setLimit(30)->reverseOrderOfResults()->setStartKey([$doc->id, new \stdClass()])->setEndKey([$doc->id]);
+      $rows = $this->couch->queryView("favorites", "lastAdded", NULL, $opts)['rows'];
+    }
+    else {
+      $typology = self::$typologyCorrespondence[$type];
+
+      $opts = new ViewQueryOpts();
+      $opts->doNotReduce()->setLimit(30)->reverseOrderOfResults()->setStartKey([$doc->id, $typology, new \stdClass()])->setEndKey([$doc->id, $typology]);
+      $rows = $this->couch->queryView('favorites', 'lastAddedPerType', NULL, $opts)['rows'];
+    }
+
+    $this->view->setVar('entries', $this->getEntries(array_column($rows, 'value')));
+
+    $stat = new Stat();
+    $this->view->setVar('entriesCount', $stat->getUpdatesCount());
+
+    $this->view->setVar('subsectionMenu', self::$typologySubMenu);
+    $this->view->setVar('subsectionIndex', array_flip(self::$typologySubMenu)[$type]);
+
+    $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
   }
 
 
