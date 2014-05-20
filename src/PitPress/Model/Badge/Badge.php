54c0cd8aebd7843ffea6ff97d6d9249b421852fa
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-20 19:15:21 +0200

    added getMetal() method, made inherit from Storable, implemented save()

--- src/PitPress/Model/Badge/Badge.php
+++ src/PitPress/Model/Badge/Badge.php
@@ -11,13 +11,12 @@ namespace PitPress\Model\Badge;
 
 
 use PitPress\Model\Activity\Activity;
-
-use ElephantOnCouch\Doc\Doc;
+use PitPress\Model\Storable;
 
 
 //! @brief This is the ancestor of all badges, it's abstract and can't be instantiated.
 //! @details Badge implements the observer pattern.
-abstract class Badge extends Doc {
+abstract class Badge extends Storable {
 
   protected $subject;
 
@@ -37,6 +36,11 @@ abstract class Badge extends Doc {
   }
 
 
+  //! @brief The badge is made by the returned metal.
+  //! @return string
+  abstract public function getMetal();
+
+
   //! @brief Every time an activity is performed by a user, this method is called for all the interested badges. The
   //! badge in fact acts like an observer while the activity is the subject. This is a variant of the observer pattern.
   //! subject can update the badge state in relation to his changes.
@@ -52,5 +56,14 @@ abstract class Badge extends Doc {
   //! @brief Withdrawn the badge previously awarded.
   //! @details Only some badges might be retired.
   public abstract function withdrawn();
+
+
+
+  public function save() {
+    $this->meta['supertype'] = 'badge';
+    $this->meta['section'] = $this->getMetal();
+
+    parent::save();
+  }
 
 } 
