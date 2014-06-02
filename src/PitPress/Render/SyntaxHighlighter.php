50fa559194594e9fe316effbc0b6d8ad37421a0f
Filippo Fadda <filippo.fadda@gmail.com>
2014-06-02 22:48:50 +0200

    updated doc style

--- src/PitPress/Render/SyntaxHighlighter.php
+++ src/PitPress/Render/SyntaxHighlighter.php
@@ -1,12 +1,16 @@
 <?php
 
-//! @file SyntaxHighlighter.php
-//! @brief This file contains the SyntaxHighlighter class.
-//! @details
-//! @author Filippo F. Fadda
+/**
+ * @file SyntaxHighlighter.php
+ * @brief This file contains the SyntaxHighlighter class.
+ * @details
+ * @author Filippo F. Fadda
+ */
 
 
-//! @brief The Sundown renders namespace.
+/**
+ * @brief The Sundown renders namespace.
+ */
 namespace PitPress\Render;
 
 
@@ -14,9 +18,11 @@ use Sundown\Render\HTML;
 use Pygmentize\Pygmentize;
 
 
-//! @brief This handler override the blockCode() method of Sundown HTML render, to highlight the source code using
-//! Pygmentize class.
-//! @deprecated The SyntaxHighlighter is deprecated due to change Sundown with Hoedown.
+/**
+ * @brief This handler override the blockCode() method of Sundown HTML render, to highlight the source code using
+ * Pygmentize class.
+ * @deprecated The SyntaxHighlighter is deprecated due to change Sundown with Hoedown.
+ */
 class SyntaxHighlighter extends HTML {
 
   public function blockCode($code, $language) {
