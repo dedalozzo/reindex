7d26f48282bb5b47807d9c781c3d3899ff9d8731
Filippo Fadda <filippo.fadda@gmail.com>
2014-06-02 22:11:23 +0200

    updated documentation

--- src/PitPress/Console/Console.php
+++ src/PitPress/Console/Console.php
@@ -1,12 +1,16 @@
 <?php
 
-//! @file Console.php
-//! @brief This file contains the Console class.
-//! @details
-//! @author Filippo F. Fadda
+/**
+ * @file Console.php
+ * @brief This file contains the Console class.
+ * @details
+ * @author Filippo F. Fadda
+ */
 
 
-//! @brief This namespace contains the Console class.
+/**
+ * @brief This namespace contains the Console class.
+ */
 namespace PitPress\Console;
 
 
@@ -15,22 +19,28 @@ use Symfony\Component\Console\Application;
 use Phalcon\DiInterface;
 
 
-//! @brief This class extends the Application class of Symfony framework, with methods aim to set the Phalcon Dependency
-//! Injector.
+/**
+ * @brief This class extends the Application class of Symfony framework, with methods aim to set the Phalcon Dependency
+ * Injector.
+ */
 class Console extends Application {
 
   protected $_di;
 
 
-  //! @brief Sets the Phalcon Dependency Injector.
-  //! @param[in] DiInterface $di The Phalcon Dependency Injector object.
+  /**
+   * @brief Sets the Phalcon Dependency Injector.
+   * @param[in] DiInterface $di The Phalcon Dependency Injector object.
+   */
   public function setDi(DiInterface $di) {
     $this->_di = $di;
   }
 
 
-  //! @brief Gets the Phalcon Dependency Injector.
-  //! @return DiInterface
+  /**
+   * @brief Gets the Phalcon Dependency Injector.
+   * @return DiInterface
+   */
   public function getDi() {
     return $this->_di;
   }
