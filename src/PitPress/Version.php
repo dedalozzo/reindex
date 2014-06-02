d8566aa6b31d6880e62531a0a7f1faa7fbcc80f4
Filippo Fadda <filippo.fadda@gmail.com>
2014-06-02 23:59:50 +0200

    updated the doc style

--- src/PitPress/Version.php
+++ src/PitPress/Version.php
@@ -1,17 +1,22 @@
 <?php
 
-//! @file Version.php
-//! @brief This file contains the Version class.
-//! @details
-//! @author Filippo F. Fadda
+/**
+ * @file Version.php
+ * @brief This file contains the Version class.
+ * @details
+ * @author Filippo F. Fadda
+ */
 
 
+//! PitPress root namespace.
 namespace PitPress;
 
 
-//! @brief This helper class is aimed to provide the software version number.
-//! @details The version number is composed by a group of three numbers. The first one is the major release number, the
-//! second one is the minor release number and the third one is used to identify the maintenance version.
+/**
+ * @brief This helper class is aimed to provide the software version number.
+ * @details The version number is composed by a group of three numbers. The first one is the major release number, the
+ * second one is the minor release number and the third one is used to identify the maintenance version.
+ */
 class Version {
 
   const MAJOR = '0'; //!< Major release number.
@@ -19,8 +24,10 @@ class Version {
   const MAINTENANCE = '5'; //!< Maintenance release number (bug fixes only).
 
 
-  //! @brief Returns the version number as string.
-  //! @return string The version number.
+  /**
+   * @brief Returns the version number as string.
+   * @return string The version number.
+   */
   public static function getNumber() {
     return self::MAJOR.".".self::MINOR.".".self::MAINTENANCE;
   }
