aa8fc949f0f80ed04fa2b0acb78bb685f7b92b47
Filippo Fadda <filippo.fadda@gmail.com>
2014-06-11 02:40:25 +0200

    added capitalize(), sanitize(), bbcodeToMarkdown, htmlToBBCode, convertCharset and made purge() method utf8 compliant

--- src/PitPress/Helper/Text.php
+++ src/PitPress/Helper/Text.php
@@ -12,20 +12,43 @@
 namespace PitPress\Helper;
 
 
+use Converter;
+
+
 /**
- * @brief This helper class contains routines to process texts.
+ * @brief This helper class contains routines to process text.
  * @nosubgrouping
  */
 class Text {
+
+
+  /**
+   * @brief Converts a string from a charset to another one.
+   * @details The default conversion is from `LATIN1` to `UTF-8`.
+   * @param[in] string $text The input string.
+   * @param[in] bool $stripslashes (optional) If `true` strip all the slashes before converting the text.
+   * @param[in] string $fromCharset (optional) The origin charset.
+   * @param[in] string $toCharset (optional) The target charset.
+   * @return string
+   */
+  public static function convertCharset($text, $stripslashes = FALSE, $fromCharset = 'LATIN1', $toCharset = 'UTF-8') {
+    if ($stripslashes)
+      return iconv($fromCharset, $toCharset, stripslashes($text));
+    else
+      return iconv($fromCharset, $toCharset, $text);
+  }
+
 
   /**
    * @brief Cuts a string to a given number of characters without breaking words.
    * @param[in] string $text The input string.
    * @param[in] integer $length The number of characters at which the string will be wrapped, ex. 200 characters.
    * @param[in] string $etc The characters you want append to the end of text.
-   * @param[in] boolean $breakWords If `true` breaks the words to return the exact number of chars.
-   * @param[in] boolean $middle Truncates the text but remove middle instead the end of the string.
+   * @param[in] string $charset (optional) The charset used.
+   * @param[in] bool $breakWords (optional) If `true` breaks the words to return the exact number of chars.
+   * @param[in] bool $middle (optional) Truncates the text but remove middle instead the end of the string.
    * @return string
+   * @warning This function works with UTF-8 strings.
    */
   public static function truncate($text, $length = 200, $etc = ' ...', $charset='UTF-8', $breakWords = FALSE, $middle = FALSE) {
     if ($length == 0)
@@ -48,18 +71,86 @@ class Text {
 
 
   /**
+   * @brief Capitalizes the given string.
+   * @param[in] string $text The input string.
+   * @param[in] string $charset (optional) The charset used.
+   * @return string
+   * @warning This function works with UTF-8 strings.
+   */
+  public static function capitalize($text, $charset = 'UTF-8') {
+    return mb_strtoupper(mb_substr($text, 0, 1, $charset), $charset) . mb_strtolower(mb_substr($text, 1, mb_strlen($text), $charset), $charset);
+  }
+
+
+  /**
    * @brief Removes the content of pre tags, than strip all tags.
    * @param[in] string $text The input string.
    * @return string
+   * @warning This function works with UTF-8 strings.
    */
   public static function purge($text) {
     // Removes the content of <pre></pre>.
-    $text = preg_replace('/<(pre)(?:(?!<\/\1).)*?<\/\1>/s', '', $text);
+    $text = preg_replace('/<(pre)(?:(?!<\/\1).)*?<\/\1>/su', '', $text);
 
     // Removes all the HTML tags.
     $text = strip_tags($text);
 
     return $text;
+  }
+
+
+  /**
+   * @brief Removes unwanted MS Word smart characters from a string.
+   * @param[in] string $text The text to be sanitized.
+   * @return string The sanitized text.
+   * @warning This function doesn't work with UTF-8 strings.
+   */
+  public static function sanitize($text) {
+    $from = [
+      "\xe2\x80\x98", // Left single quote.
+      "\xe2\x80\x99", // Right single quote.
+      "\xe2\x80\x9c", // Left double quote.
+      "\xe2\x80\x9d", // Right double quote.
+      "\xe2\x80\x94", // Em dash.
+      "\xe2\x80\xa6" // Elipses.
+    ];
+
+    $to = [
+      "'",
+      "'",
+      '"',
+      '"',
+      '&mdash;',
+      '...'
+    ];
+
+    return htmlspecialchars(str_replace($from, $to, $text));
+  }
+
+
+  /**
+   * @brief Converts the given text from HTML to BBCode.
+   * @param[in] string $text The text to be sanitized.
+   * @param[in] string $id (optional) In case of error, use the given ID in the exception message.
+   * @return string The sanitized text.
+   * @warning This function works with UTF-8 strings.
+   */
+  public static function htmlToBBCode($text, $id = "") {
+    $converter = new Converter\HTMLConverter($text, $id);
+    return $converter->toBBCode();
+  }
+
+
+  /**
+   * @brief Converts the given text from BBCode to Markdown.
+   * @param[in] string $text The text to be sanitized.
+   * @param[in] string $id (optional) In case of error, use the given ID in the exception message.
+   * @return string The sanitized text.
+   * @warning This function works with UTF-8 strings.
+   */
+  public static function bbcodeToMarkdown($text, $id = "") {
+    $converter = new Converter\BBCodeConverter($text, $id);
+    return $converter->toMarkdown();
   }
 
 } 
