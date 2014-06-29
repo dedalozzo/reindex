6187e4210d962d58cb5b7c218052a133ad2da589
Filippo Fadda <filippo.fadda@gmail.com>
2014-06-29 18:38:52 +0200

    Beacon new badge

---
+++ src/PitPress/Model/Badge/Link/Beacon.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file Beacon.php
+ * @brief This file contains the Beacon class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace PitPress\Model\Badge\Link;
+
+
+use PitPress\Model\Badge\Gold;
+
+
+/**
+ * @brief Reported 500 links.
+ * @details Awarded once.
+ */
+class Beacon extends Gold {
+
+
+  /**
+   * @copydoc Badge::getName()
+   */
+  public function getName() {
+    return "Faro";
+  }
+
+
+  /**
+   * @copydoc Badge::getBrief()
+   */
+  public function getBrief() {
+    return <<<'DESC'
+Hai segnalato almeno 500 links. Assegnato una sola volta.
+DESC;
+  }
+
+
+  /**
+   * @copydoc Badge::award()
+   */
+  public function award() {
+
+  }
+
+
+  /**
+   * @copydoc Badge::withdrawn()
+   */
+  public function withdrawn() {
+
+  }
+
+}
