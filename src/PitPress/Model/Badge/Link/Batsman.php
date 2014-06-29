ffafa4a8dc8daa8a8b24edac86d0c935e6763b65
Filippo Fadda <filippo.fadda@gmail.com>
2014-06-29 18:38:37 +0200

    Batsman new badge

---
+++ src/PitPress/Model/Badge/Link/Batsman.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file Batsman.php
+ * @brief This file contains the Batsman class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace PitPress\Model\Badge\Link;
+
+
+use PitPress\Model\Badge\Bronze;
+
+
+/**
+ * @brief Reported your first link.
+ * @details Awarded once.
+ */
+class Batsman extends Bronze {
+
+
+  /**
+   * @copydoc Badge::getName()
+   */
+  public function getName() {
+    return "Segnalatore";
+  }
+
+
+  /**
+   * @copydoc Badge::getBrief()
+   */
+  public function getBrief() {
+    return <<<'DESC'
+Hai segnalato il tuo primo link. Assegnato una sola volta.
+DESC;
+  }
+
+
+  /**
+   * @copydoc Badge::award()
+   */
+  public function award() {
+
+  }
+
+
+  /**
+   * @copydoc Badge::withdrawn()
+   */
+  public function withdrawn() {
+
+  }
+
+}
