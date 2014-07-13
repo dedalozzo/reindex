ede25a94ede5fd318b4421b4ad4c34a4e6aa038c
Filippo Fadda <filippo.fadda@gmail.com>
2014-07-13 19:08:22 +0200

    improved the show404Action()

--- src/PitPress/Controller/ErrorController.php
+++ src/PitPress/Controller/ErrorController.php
@@ -11,11 +11,18 @@
 namespace PitPress\Controller;
 
 
+use Phalcon\Mvc\View;
+
+
 class ErrorController extends BaseController {
 
 
   public function show404Action() {
-    $this->response->setHeader(404, 'Not Found');
+    $this->response->setHeader('HTTP/1.0 404', 'Not Found');
+    $this->view->setVar('title', 'Pagina non trovata');
+    $this->view->setVar('method', strtolower($_SERVER['REQUEST_METHOD']));
+    $this->view->setVar('url', "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+    $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
     $this->view->pick('404/404');
   }
 
