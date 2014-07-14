e626d512f40ddc2a6a3973ba9ec983d055262c5c
Filippo Fadda <filippo.fadda@gmail.com>
2014-07-14 17:04:18 +0200

    removed the code to handle exceptions because it doesn't work at all

--- services/dispatcher.php
+++ services/dispatcher.php
@@ -14,27 +14,7 @@ use Phalcon\Mvc\Dispatcher;
 // Returns the dispatcher instance.
 $di->setShared('dispatcher',
   function() use ($di) {
-    $eventsManager = $di->getShared('eventsManager');
-
-    $eventsManager->attach(
-      "dispatch:beforeException",
-      function($event, $dispatcher, $exception) {
-        $code = $exception->getCode();
-
-        if ($code == Dispatcher::EXCEPTION_HANDLER_NOT_FOUND or $code == Dispatcher::EXCEPTION_ACTION_NOT_FOUND) {
-          $dispatcher->forward(
-            [
-              'controller' => 'error',
-              'action'     => 'show404'
-            ]
-          );
-        }
-      }
-    );
-
     $dispatcher = new Dispatcher();
-    $dispatcher->setEventsManager($eventsManager);
-
     return $dispatcher;
   }
 );
