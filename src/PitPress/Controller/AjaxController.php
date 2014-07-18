41ebed5e5eec53a95bf94f3c19c7975b350564f0
Filippo Fadda <filippo.fadda@gmail.com>
2014-07-18 17:38:28 +0200

    implemented the likeAction() method

--- src/PitPress/Controller/AjaxController.php
+++ src/PitPress/Controller/AjaxController.php
@@ -9,6 +9,9 @@
 namespace PitPress\Controller;
 
 
+use ElephantOnCouch\Couch;
+
+
 /**
  * @brief Controller for the AJAX requests.
  * @nosubgrouping
@@ -33,29 +36,50 @@ class AjaxController extends BaseController {
   }
 
 
-  public function afterExecuteRoute() {
-    parent::afterExecuteRoute();
+  /**
+   * @brief Cross-site HTTP requests are HTTP requests for resources from a different domain than the domain of the
+   * resource making the request. This occurs very commonly on the web today, since pages load a number of resources in
+   * a cross-site manner, including CSS stylesheets, images and scripts, and other resources.\n
+   * Cross-site HTTP requests initiated from within scripts have been subject to well-known restrictions, for
+   * well-understood security reasons.  For example HTTP Requests made using the XMLHttpRequest object were subject to
+   * the same-origin policy.  In particular, this meant that a web application using XMLHttpRequest could only make
+   * HTTP requests to the domain it was loaded from, and not to other domains.  Developers expressed the desire to
+   * safely evolve capabilities such as XMLHttpRequest to make cross-site requests, for better, safer mash-ups within
+   * web applications.\n
+   * To make possible cross-site AJAX calls, for example from blog.programmazione.it to ajax.programmazione.it, we
+   * must set `Access-Control-Allow-Origin` header.
+   */
+  protected function validateOrigin() {
 
-    /*
-     * Cross-site HTTP requests are HTTP requests for resources from a different domain than the domain of the resource
-     * making the request.  For instance, a resource loaded from Domain A (http://domaina.example) such as an HTML web
-     * page, makes a request for a resource on Domain B (http://domainb.foo), such as an image, using the img element
-     * (http://domainb.foo/image.jpg).  This occurs very commonly on the web today — pages load a number of resources in
-     * a cross-site manner, including CSS stylesheets, images and scripts, and other resources.\n
-     * Cross-site HTTP requests initiated from within scripts have been subject to well-known restrictions, for
-     * well-understood security reasons.  For example HTTP Requests made using the XMLHttpRequest object were subject to
-     * the same-origin policy.  In particular, this meant that a web application using XMLHttpRequest could only make
-     * HTTP requests to the domain it was loaded from, and not to other domains.  Developers expressed the desire to
-     * safely evolve capabilities such as XMLHttpRequest to make cross-site requests, for better, safer mash-ups within
-     * web applications.\n
-     * To make possible cross-site AJAX calls, for example from blog.programmazione.it to ajax.programmazione.it, we
-     * must set `Access-Control-Allow-Origin` header.
-    */
     if (isset($_SERVER['HTTP_ORIGIN'])) {
       $origin = $_SERVER['HTTP_ORIGIN'];
 
-      if ($this->getDomainName($origin) == $this->domainName)
+      if ($this->getDomainName($origin) == $this->domainName) {
         $this->response->setHeader('Access-Control-Allow-Origin', $origin);
+        $this->response->setHeader('Access-Control-Allow-Credentials', 'true');
+      }
+      else
+        throw new \DomainException("Stai tentando di effettuare una richiesta da un dominio sconosciuto.");
+    }
+
+  }
+
+
+  public function initialize() {
+    parent::initialize();
+    $this->validateOrigin();
+  }
+
+
+  public function likeAction() {
+    if ($this->request->hasPost('id')) {
+      $doc = $this->couchdb->getDoc(Couch::STD_DOC_PATH, $this->request->getPost('id'));
+      echo json_encode($doc->like($this->user));
+
+      $this->view->disable();
+    }
+    else {
+      throw new \DomainException("Stai tentando di effettuare una richiesta da un dominio sconosciuto.");
     }
   }
 
