511b3851d9c5ca221ec0b327ea88e6c3e62ca4e6
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-01 18:49:46 +0200

    refactored all the routes

--- src/PitPress/Route/IndexGroup.php
+++ src/PitPress/Route/IndexGroup.php
@@ -2,7 +2,7 @@
 
 /**
  * @file IndexGroup.php
- * @brief Group of Updates routes.
+ * @brief This file contains the IndexGroup class.
  * @details
  * @author Filippo F. Fadda
  */
@@ -17,11 +17,10 @@ use Phalcon\DI;
 
 
 /**
- * @brief Group of index routes.
+ * @brief Group of Index routes.
  * @nosubgrouping
  */
 class IndexGroup extends Group {
-
 
   public function initialize() {
     // Sets the default controller for the following routes.
@@ -41,25 +40,18 @@ class IndexGroup extends Group {
     $this->addGet('/([0-9]{4})/([0-9]{2})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2, 'day' => 3]);
     //$this->addGet('/([0-9]{4})/(?:([0-9]{2})/(?:([0-9]{2})/){0,1}){0,1}', ['action' => 'perDate', 'year' => 1, 'month' => 2, 'day' => 3]);
 
-    // All the following routes start with /something.
-    $this->setPrefix('/([a-z]{0,20})');
-    $this->addGet('/', ['action' => 'newest', 'filter' => 1]);
-    $this->addGet('/nuov[ie]/', ['action' => 'newest', 'filter' => 1]);
-    $this->addGet('/popolari/', ['action' => 'popular', 'filter' => 1]);
-    $this->addGet('/popolari/{period}/', ['action' => 'popular', 'filter' => 1]);
-    $this->addGet('/attiv[ie]/', ['action' => 'active', 'filter' => 1]);
-    $this->addGet('/interessanti/', ['action' => 'interesting', 'filter' => 1]);
+    // All the following routes start with /tutti.
+    $this->setPrefix('/tutti');
+    $this->addGet('/', ['action' => 'newest']);
+    $this->addGet('/nuovi/', ['action' => 'newest']);
+    $this->addGet('/popolari/', ['action' => 'popular']);
+    $this->addGet('/popolari/{filter}/', ['action' => 'popular']);
+    $this->addGet('/attivi/', ['action' => 'active']);
+    $this->addGet('/interessanti/', ['action' => 'interesting']);
 
-    // perDateByType
-    $this->addGet('/([0-9]{4})/', ['action' => 'perDateByType', 'filter' => 1, 'year' => 2]);
-    $this->addGet('/([0-9]{4})/([0-9]{2})/', ['action' => 'perDateByType', 'filter' => 1, 'year' => 2, 'month' => 3]);
-    $this->addGet('/([0-9]{4})/([0-9]{2})/([0-9]{2})/', ['action' => 'perDateByType', 'filter' => 1, 'year' => 2, 'month' => 3, 'day' => 4]);
-
-    // All the following routes start with /domande.
-    $this->setPrefix('/domande');
-    $this->addGet('/importanti/', ['action' => 'important']);
-    $this->addGet('/aperte/', ['action' => 'open']);
-    $this->addGet('/aperte/{filter}/', ['action' => 'open']);
+    $this->addGet('/([0-9]{4})/', ['action' => 'perDate', 'year' => 1]);
+    $this->addGet('/([0-9]{4})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2]);
+    $this->addGet('/([0-9]{4})/([0-9]{2})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2, 'day' => 3]);
 
     //$this->addGet('/rss', ['action' => 'rss']);
   }
