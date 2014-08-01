e2133dff19e6511db845b7be6d103f948e8c5a19
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-01 18:59:58 +0200

    added a group of link routes

---
+++ src/PitPress/Route/LinkGroup.php
@@ -0,0 +1,48 @@
+<?php
+
+//! @file LinkGroup.php
+//! @brief This file contains the LinkGroup class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Route;
+
+
+use Phalcon\Mvc\Router\Group;
+use Phalcon\DI;
+
+
+/**
+ * @brief Group of Link routes.
+ * @nosubgrouping
+ */
+class LinkGroup extends Group {
+
+  public function initialize() {
+    // Sets the default controller for the following routes.
+    $this->setPaths(
+      [
+        'namespace' => 'PitPress\Controller',
+        'controller' => 'link'
+      ]);
+
+    $this->setHostName(DI::getDefault()['config']['application']['domainName']);
+
+    // All the following routes start with /links.
+    $this->setPrefix('/links');
+    $this->addGet('/', ['action' => 'newest']);
+    $this->addGet('/nuovi/', ['action' => 'newest']);
+    $this->addGet('/popolari/', ['action' => 'popular']);
+    $this->addGet('/popolari/{filter}/', ['action' => 'popular']);
+    $this->addGet('/attivi/', ['action' => 'active']);
+    $this->addGet('/interessanti/', ['action' => 'interesting']);
+
+    $this->addGet('/([0-9]{4})/', ['action' => 'perDate', 'year' => 1]);
+    $this->addGet('/([0-9]{4})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2]);
+    $this->addGet('/([0-9]{4})/([0-9]{2})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2, 'day' => 3]);
+
+    //$this->addGet('/rss', ['action' => 'rss']);
+  }
+
+} 
