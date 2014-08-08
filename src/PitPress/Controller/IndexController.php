f19b436541bfa690a3ecf98f54d21fbaf7ac05d6
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-08 22:23:09 +0200

    added the pagination and the favoriteAction()

--- src/PitPress/Controller/IndexController.php
+++ src/PitPress/Controller/IndexController.php
@@ -60,7 +60,7 @@ class IndexController extends ListController {
   /*
    * @brief Retrieves information for a bunch of posts.
    */
-  protected function getInfo($viewName, $type, $count = 20) {
+  protected function getInfo($viewName, $type, $count = 10) {
     $opts = new ViewQueryOpts();
     $opts->doNotReduce()->setLimit($count)->reverseOrderOfResults()->setStartKey([$type, Couch::WildCard()])->setEndKey([$type]);
     $rows = $this->couch->queryView('posts', $viewName, NULL, $opts);
@@ -88,7 +88,7 @@ class IndexController extends ListController {
 
     $entries = [];
     $postCount = count($posts);
-    for ($i = 0; $i < $postCount - 1; $i++) {
+    for ($i = 0; $i < $postCount; $i++) {
       $entry = new \stdClass();
       $entry->id = $posts[$i]['id'];
 
@@ -109,10 +109,10 @@ class IndexController extends ListController {
   /**
    * @brief Gets a list of tags recently updated.
    */
-  protected function recentTags() {
+  protected function recentTags($count = 20) {
     // todo Change this part, getting the classification of the last week, grouped by tagId.
     $opts = new ViewQueryOpts();
-    $opts->doNotReduce()->setLimit(60);
+    $opts->doNotReduce()->setLimit($count);
     // todo Change newest...
     $classifications = $this->couch->queryView("classifications", "newest", NULL, $opts);
     $keys = array_column($classifications->asArray(), 'value');
@@ -125,7 +125,7 @@ class IndexController extends ListController {
     $postsPerTag = $this->couch->queryView("classifications", "perTag", $keys, $opts);
 
     $recentTags = [];
-    for ($i = 0; $i < 60; $i++)
+    for ($i = 0; $i < $count; $i++)
       $recentTags[] = [$tags[$i]['value'], $postsPerTag[$i]['value']];
 
     $this->view->setVar('recentTags', $recentTags);
@@ -159,7 +159,7 @@ class IndexController extends ListController {
     // Those are the notebook pages, printed using the `updates.volt` widget.
     $this->view->setVar('questions', $this->getInfo('perDateByType', 'question'));
     $this->view->setVar('articles', $this->getInfo('perDateByType', 'article'));
-    $this->view->setVar('books', $this->getInfo('perDateByType', 'link'));
+    $this->view->setVar('books', $this->getInfo('perDateByType', 'book'));
   }
 
 
@@ -207,7 +207,12 @@ class IndexController extends ListController {
     }
 
     $entries = $this->getEntries(array_column($rows->asArray(), 'id'));
-    $this->view->setVar('nextPage', $this->buildPaginationUrl($entries));
+
+    if (count($entries) > self::RESULTS_PER_PAGE) {
+      $last = array_pop($entries);
+      $this->view->setVar('nextPage', $this->buildPaginationUrl($last->publishingDate, $last->id));
+    }
+
     $this->view->setVar('entries', $entries);
     $this->view->setVar('entriesCount', $this->countPosts());
 
@@ -245,7 +250,12 @@ class IndexController extends ListController {
     }
 
     $entries = $this->getEntries(array_column($rows->asArray(), 'id'));
-    $this->view->setVar('nextPage', $this->buildPaginationUrl($entries));
+
+    if (count($entries) > self::RESULTS_PER_PAGE) {
+      $last = array_pop($entries);
+      $this->view->setVar('nextPage', $this->buildPaginationUrl($last->publishingDate, $last->id));
+    }
+
     $this->view->setVar('entries', $entries);
     $this->view->setVar('entriesCount', Helper\Text::formatNumber($count));
     $this->view->setVar('title', sprintf('%s per data', ucfirst($this->getLabel())));
@@ -314,5 +324,43 @@ class IndexController extends ListController {
     $this->view->setVar('entriesCount', 0);
     $this->view->setVar('title', sprintf('%s interessanti', ucfirst($this->getLabel())));
   }
+
+
+  public function favoriteAction() {
+    $opts = new ViewQueryOpts();
+    $opts->doNotReduce()->reverseOrderOfResults()->setLimit(self::RESULTS_PER_PAGE+1);
+
+    // Paginates results.
+    $startKey = isset($_GET['startkey']) ? (int)$_GET['startkey'] : Couch::WildCard();
+    if (isset($_GET['startkey_docid'])) $opts->setStartDocId($_GET['startkey_docid']);
+
+    if ($this->isSameClass()) {
+      $opts->doNotReduce()->setLimit(self::RESULTS_PER_PAGE+1)->reverseOrderOfResults()->setStartKey([$this->user->id, $startKey])->setEndKey([$this->user->id]);
+      $rows = $this->couch->queryView("favorites", "perDateAdded", NULL, $opts);
+
+      $opts->reduce()->setStartKey([$this->user->id, Couch::WildCard()])->unsetOpt('startkey_docid');
+      $count = $this->couch->queryView("favorites", "perDateAdded", NULL, $opts)->getReducedValue();
+    }
+    else {
+      $opts->doNotReduce()->setLimit(self::RESULTS_PER_PAGE+1)->reverseOrderOfResults()->setStartKey([$this->user->id, $startKey])->setEndKey([$this->user->id]);
+      $rows = $this->couch->queryView("favorites", "perDateAddedByType", NULL, $opts);
+
+      $opts->reduce()->setStartKey([$this->user->id, Couch::WildCard()])->unsetOpt('startkey_docid');
+      $count = $this->couch->queryView("favorites", "perDateAddedByType", NULL, $opts)->getReducedValue();
+    }
+
+    $stars = $rows->asArray();
+    // If the query returned more entries than the ones must display on the page, a link to the next page must be provided.
+    if ($rows->count() > self::RESULTS_PER_PAGE) {
+      $last = array_pop($stars);
+      $this->view->setVar('nextPage', $this->buildPaginationUrl($last['key'][1], $last['id']));
+    }
+
+    $this->view->setVar('entries', $this->getEntries(array_column($stars, 'value')));
+    $this->view->setVar('entriesCount', Helper\Text::formatNumber($count));
+    $this->view->setVar('entriesLabel', 'preferiti');
+    $this->view->setVar('title', sprintf('%s preferiti', ucfirst($this->getLabel())));
+  }
+
 
 }
