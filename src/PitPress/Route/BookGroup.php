37d89a861aa07561e1b73d54aaba5492991c5ab5
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-10 16:10:44 +0200

    fixed a typo in the preferiti route

--- src/PitPress/Route/BookGroup.php
+++ src/PitPress/Route/BookGroup.php
@@ -37,7 +37,7 @@ class BookGroup extends Group {
     $this->addGet('/popolari/{filter}/', ['action' => 'popular']);
     $this->addGet('/attivi/', ['action' => 'active']);
     $this->addGet('/interessanti/', ['action' => 'interesting']);
-    $this->addGet('/preferiti/', ['action' => 'favorites']);
+    $this->addGet('/preferiti/', ['action' => 'favorite']);
 
     $this->addGet('/([0-9]{4})/', ['action' => 'perDate', 'year' => 1]);
     $this->addGet('/([0-9]{4})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2]);
