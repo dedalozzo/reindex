5ef326f41005a178d1e9b5d5fcda46617c9d1816
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-12 01:59:10 +0200

    made the groups inherit from IndexGroup and implemented 3 methods

--- src/PitPress/Route/LinkGroup.php
+++ src/PitPress/Route/LinkGroup.php
@@ -9,7 +9,6 @@
 namespace PitPress\Route;
 
 
-use Phalcon\Mvc\Router\Group;
 use Phalcon\DI;
 
 
@@ -17,33 +16,21 @@ use Phalcon\DI;
  * @brief Group of Link routes.
  * @nosubgrouping
  */
-class LinkGroup extends Group {
+class LinkGroup extends IndexGroup {
 
-  public function initialize() {
-    // Sets the default controller for the following routes.
-    $this->setPaths(
-      [
-        'namespace' => 'PitPress\Controller',
-        'controller' => 'link'
-      ]);
 
-    $this->setHostName(DI::getDefault()['config']['application']['domainName']);
+  protected function getController() {
+    return 'book';
+  }
 
-    // All the following routes start with /links.
-    $this->setPrefix('/links');
-    $this->addGet('/', ['action' => 'newest']);
-    $this->addGet('/nuovi/', ['action' => 'newest']);
-    $this->addGet('/popolari/', ['action' => 'popular']);
-    $this->addGet('/popolari/{filter}/', ['action' => 'popular']);
-    $this->addGet('/attivi/', ['action' => 'active']);
-    $this->addGet('/interessanti/', ['action' => 'interesting']);
-    $this->addGet('/preferiti/', ['action' => 'favorite']);
 
-    $this->addGet('/([0-9]{4})/', ['action' => 'perDate', 'year' => 1]);
-    $this->addGet('/([0-9]{4})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2]);
-    $this->addGet('/([0-9]{4})/([0-9]{2})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2, 'day' => 3]);
+  protected function getDefaultAction() {
+    return 'newest';
+  }
 
-    //$this->addGet('/rss', ['action' => 'rss']);
+
+  public function getPrefix() {
+    return '/links';
   }
 
 } 
--- src/PitPress/Route/QuestionGroup.php
+++ src/PitPress/Route/QuestionGroup.php
@@ -9,7 +9,6 @@
 namespace PitPress\Route;
 
 
-use Phalcon\Mvc\Router\Group;
 use Phalcon\DI;
 
 
@@ -17,19 +16,13 @@ use Phalcon\DI;
  * @brief Group of Question routes.
  * @nosubgrouping
  */
-class QuestionGroup extends Group {
+class QuestionGroup extends IndexGroup {
 
-  public function initialize() {
-    // Sets the default controller for the following routes.
-    $this->setPaths(
-      [
-        'namespace' => 'PitPress\Controller',
-        'controller' => 'question'
-      ]);
 
+  protected function addRoutes() {
+    $this->setPaths(['namespace' => 'PitPress\Controller', 'controller' => 'question']);
     $this->setHostName(DI::getDefault()['config']['application']['domainName']);
 
-    // All the following routes start with /domande.
     $this->setPrefix('/domande');
     $this->addGet('/', ['action' => 'newest']);
     $this->addGet('/nuove/', ['action' => 'newest']);
@@ -41,10 +34,24 @@ class QuestionGroup extends Group {
     $this->addGet('/aperte/', ['action' => 'open']);
     $this->addGet('/aperte/{filter}/', ['action' => 'open']);
     $this->addGet('/preferite/', ['action' => 'favorite']);
+    $this->addGet('/preferite/{filter}/', ['action' => 'favorite']);
 
     $this->addGet('/([0-9]{4})/', ['action' => 'perDate', 'year' => 1]);
     $this->addGet('/([0-9]{4})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2]);
     $this->addGet('/([0-9]{4})/([0-9]{2})/([0-9]{2})/', ['action' => 'perDate', 'year' => 1, 'month' => 2, 'day' => 3]);
+
+    $this->setPrefix('/tags/{tag}/domande');
+    $this->addGet('', ['action' => 'newest']);
+    $this->addGet('/nuove/', ['action' => 'newest']);
+    $this->addGet('/popolari/', ['action' => 'popular']);
+    $this->addGet('/popolari/{filter}/', ['action' => 'popular']);
+    $this->addGet('/attive/', ['action' => 'active']);
+    $this->addGet('/interessanti/', ['action' => 'interesting']);
+    $this->addGet('/importanti/', ['action' => 'important']);
+    $this->addGet('/aperte/', ['action' => 'open']);
+    $this->addGet('/aperte/{filter}/', ['action' => 'open']);
+    $this->addGet('/preferite/', ['action' => 'favorite']);
+    $this->addGet('/preferite/{filter}/', ['action' => 'favorite']);
 
     //$this->addGet('/rss', ['action' => 'rss']);
   }
