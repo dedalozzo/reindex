6ff646aeae97e46a16287d1b6a38eca9322d12a3
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-20 20:35:58 +0200

    implemented the methods declared in IVersion interface

--- src/PitPress/Extension/TVersion.php
+++ src/PitPress/Extension/TVersion.php
@@ -11,10 +11,53 @@
 namespace PitPress\Extension;
 
 
+use ElephantOnCouch\Generator\UUID;
+
+
 /**
  * @brief Implements IVersion interface.
  */
 trait TVersion {
+
+
+  public static function createVersion($id = NULL) {
+    $instance = new self();
+
+    if (is_null($id))
+      $instance->meta['_id'] = UUID::generate(UUID::UUID_RANDOM, UUID::FMT_STRING).IVersion::SEPARATOR.(string)microtime();
+    else
+      $instance->meta['_id'] = (string)$id.IVersion::SEPARATOR.(string)microtime();
+
+    return $instance;
+  }
+
+
+  public function getUnversionId() {
+    return strtok($this->meta['_id'], IVersion::SEPARATOR);
+  }
+
+
+  public function getVersion() {
+    return substr($this->meta['_id'], stripos($this->meta['_id'], IVersion::SEPARATOR) + strlen(IVersion::SEPARATOR));
+  }
+
+
+  public function issetVersion() {
+    if (stripos($this->meta['_id'], IVersion::SEPARATOR) === FALSE)
+      return FALSE;
+    else
+      return TRUE;
+  }
+
+
+  public function setVersion($value) {
+    $this->meta['_id'] = $this->getUnversionId().IVersion::SEPARATOR.(string)$value;
+  }
+
+
+  public function unsetVersion() {
+    $this->meta['_id'] = $this->getUnversionId();
+  }
 
 
   public function getChanges() {
