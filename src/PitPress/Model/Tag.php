15560d32ccf5fe3fa2f459e668b92eb88b86a924
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-20 20:37:07 +0200

    implements IVersion interface, also moved to Model dir

---
+++ src/PitPress/Model/Tag.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @file Tag.php
+ * @brief This file contains the Tag class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace PitPress\Model;
+
+
+use PitPress\Extension;
+
+
+/**
+ * @brief A label used to categorize posts.
+ * @details Every post must be tagged with a maximun of five tags.
+ * @nosubgrouping
+ */
+class Tag extends Item implements Extension\ICount, Extension\IStar, Extension\IVersion {
+  use Extension\TCount, Extension\TStar, Extension\TVersion;
+
+  /** @name Ignoring Methods */
+  //!@{
+
+  /**
+   * @brief Adds the tag to the ignore list of the current user.
+   */
+  public function ignore(User $user) {
+  }
+
+
+  /**
+   * @brief Removes the tag from the ignore list of the current user.
+   */
+  public function unignore(User $user) {
+  }
+
+  //!@}
+
+
+  /**
+   * @brief Gets the item state.
+   */
+  public function getState() {
+    return $this->meta['state'];
+  }
+
+
+  public function getFollowersCount() {
+
+  }
+
+
+  //! @cond HIDDEN_SYMBOLS
+
+  public function getName() {
+    return $this->meta['name'];
+  }
+
+
+  public function issetName() {
+    return isset($this->meta['name']);
+  }
+
+
+  public function setName($value) {
+    $this->meta['name'] = $value;
+  }
+
+
+  public function unsetName() {
+    if ($this->isMetadataPresent('name'))
+      unset($this->meta['name']);
+  }
+
+  //! @endcond
+
+}
