1ab33f7e77343b6173c2a89c625b1b75a8db5788
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-22 01:01:30 +0200

    added the const SEPARATOR and the method getUnversionId()

--- src/PitPress/Model/Storable.php
+++ src/PitPress/Model/Storable.php
@@ -18,9 +18,14 @@ use Phalcon\DI;
 
 /**
  * @brief This class is used to represent a storable object.
+ * @details A storable object is not versionable, but for implementation reasons provides the ability to get the ID
+ * pruned of its version number. This happens because all the extensions work with an unversion ID.
  * @nosubgrouping
  */
 abstract class Storable extends Doc {
+
+  const SEPARATOR = '::'; //!< Used to separate the ID from the version number.
+
   protected $di; // Stores the default Dependency Injector.
   protected $couch; // Stores the ElephantOnCouch client instance.
   protected $redis; // Stores the Redis client instance.
@@ -37,10 +42,19 @@ abstract class Storable extends Doc {
 
 
   /**
+   * @brief Prunes the ID of its version number, if any.
+   * @return string
+   */
+  public function getUnversionId() {
+    return strtok($this->meta['_id'], self::SEPARATOR);
+  }
+
+
+  /**
    * @brief Returns information about the last update.
    */
   public function getLastUpdateInfo() {
-    return "";
+    // todo
   }
 
   /**
