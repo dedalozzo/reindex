20fe031aacf1eae761f70d6cc7f581ec17c099ed
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-22 18:41:39 +0200

    updated version number

--- src/PitPress/Version.php
+++ src/PitPress/Version.php
@@ -21,7 +21,7 @@ class Version {
 
   const MAJOR = '0'; //!< Major release number.
   const MINOR = '7'; //!< Minor release number.
-  const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
+  const MAINTENANCE = '1'; //!< Maintenance release number (bug fixes only).
 
 
   /**
