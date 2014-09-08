8a3477f89d19256ed01a2bd954cd3e0fde7eb719
Filippo Fadda <filippo.fadda@gmail.com>
2014-09-08 15:40:14 +0200

    added the property description

---
+++ src/PitPress/Property/TDescription.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file TDescription.php
+ * @brief This file contains the TDescription trait.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace PitPress\Property;
+
+
+//! @cond HIDDEN_SYMBOLS
+trait TDescription {
+
+
+  public function getDescription() {
+    return $this->meta['description'];
+  }
+
+
+  public function issetDescription() {
+    return isset($this->meta['description']);
+  }
+
+
+  public function setDescription($value) {
+    $this->meta["description"] = $value;
+  }
+
+
+  public function unsetDescription() {
+    if ($this->isMetadataPresent('description'))
+      unset($this->meta['description']);
+  }
+
+}
+//! @endcond
