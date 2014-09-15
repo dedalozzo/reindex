5d50402e44d67b660857bb2b66d5daaebffd720d
Filippo Fadda <filippo.fadda@gmail.com>
2014-09-15 19:05:00 +0200

    added createdAt and modifiedAt properties

--- src/PitPress/Model/Storable.php
+++ src/PitPress/Model/Storable.php
@@ -38,18 +38,62 @@ abstract class Storable extends Doc {
 
 
   /**
-   * @brief Returns information about the last update.
-   */
-  public function getLastUpdateInfo() {
-    // todo
-  }
-
-  /**
    * @brief Saves the item to the database.
    */
   public function save() {
-    $this->meta['lastUpdate'] = time();
+    $this->modifiedAt = time();
+
+    // Creation timestamp has not been provided.
+    if (is_null($this->createdAt))
+      $this->createdAt = $this->modifiedAt;
+
     $this->couch->saveDoc($this);
   }
+
+
+  //! @cond HIDDEN_SYMBOLS
+
+  public function getCreatedAt() {
+    return $this->meta['createdAt'];
+  }
+
+
+  public function issetCreatedAt() {
+    return isset($this->meta['createdAt']);
+  }
+
+
+  public function setCreatedAt($value) {
+    $this->meta['createdAt'] = $value;
+  }
+
+
+  public function unsetCreatedAt() {
+    if ($this->isMetadataPresent('createdAt'))
+      unset($this->meta['createdAt']);
+  }
+
+
+  public function getModifiedAt() {
+    return $this->meta['modifiedAt'];
+  }
+
+
+  public function issetModifiedAt() {
+    return isset($this->meta['modifiedAt']);
+  }
+
+
+  public function setModifiedAt($value) {
+    $this->meta['modifiedAt'] = $value;
+  }
+
+
+  public function unsetModifiedAt() {
+    if ($this->isMetadataPresent('modifiedAt'))
+      unset($this->meta['modifiedAt']);
+  }
+
+  //! @endcond
 
 }
