0c8b62aaf32104d8ab24fb81b76343643e277b44
Filippo Fadda <filippo.fadda@gmail.com>
2014-09-15 18:54:12 +0200

    replaced ipAddress with host

--- services/couchdb.php
+++ services/couchdb.php
@@ -15,7 +15,7 @@ use ElephantOnCouch\Adapter;
 // Creates an instance of ElephantOnCouch client and return it.
 $di->setShared('couchdb',
   function() use ($config) {
-    $couch = new Couch(new Adapter\NativeAdapter($config->couchdb->ipAddress.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
+    $couch = new Couch(new Adapter\NativeAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
     //$couch = new Couch(new Adapter\CurlAdapter($config->couchdb->ipAddress.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
 
     $couch->selectDb($config->couchdb->database);
