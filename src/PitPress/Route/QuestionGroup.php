b9fbb4caf2d985882e97d289de737cbf63442e96
Filippo Fadda <filippo.fadda@gmail.com>
2014-10-02 00:35:44 +0200

    completely reviews the QuestionGroup routes

--- src/PitPress/Route/QuestionGroup.php
+++ src/PitPress/Route/QuestionGroup.php
@@ -35,17 +35,16 @@ class QuestionGroup extends IndexGroup {
 
 
   protected function addRoutes($postfix = "") {
-    $this->addGet('/', ['action' => $this->getDefaultAction()]);
-    $this->addGet('/nuove/', ['action' => 'newest']);
-    $this->addGet('/popolari/', ['action' => 'popular']);
-    $this->addGet('/popolari/{filter}/', ['action' => 'popular']);
-    $this->addGet('/attive/', ['action' => 'active']);
-    $this->addGet('/interessanti/', ['action' => 'interesting']);
-    $this->addGet('/importanti/', ['action' => 'important']);
-    $this->addGet('/aperte/', ['action' => 'open']);
-    $this->addGet('/aperte/{filter}/', ['action' => 'open']);
-    $this->addGet('/preferite/', ['action' => 'favorite']);
-    $this->addGet('/preferite/{filter}/', ['action' => 'favorite']);
+    $this->addGet('/', ['action' => $this->getDefaultAction().$postfix]);
+    $this->addGet('/nuove/', ['action' => 'newest'.$postfix]);
+    $this->addGet('/popolari/', ['action' => 'popular'.$postfix]);
+    $this->addGet('/popolari/{filter}/', ['action' => 'popular'.$postfix]);
+    $this->addGet('/attive/', ['action' => 'active'.$postfix]);
+    $this->addGet('/importanti/', ['action' => 'important'.$postfix]);
+    $this->addGet('/aperte/', ['action' => 'open'.$postfix]);
+    $this->addGet('/aperte/{filter}/', ['action' => 'open'.$postfix]);
+    $this->addGet('/preferite/', ['action' => 'favorite'.$postfix]);
+    $this->addGet('/preferite/{filter}/', ['action' => 'favorite'.$postfix]);
 
     $this->addGet('/{year:[0-9]{4}}/', ['action' => 'perDate'.$postfix]);
     $this->addGet('/{year:[0-9]{4}}/{month:[0-9]{2}}/', ['action' => 'perDate'.$postfix]);
