f56456923aa4ed6716f2affce099531484440261
Filippo Fadda <filippo.fadda@gmail.com>
2014-10-08 03:48:07 +0200

    fixed some issue on the Password validator

--- src/PitPress/Validator/Password.php
+++ src/PitPress/Validator/Password.php
@@ -19,8 +19,8 @@ use Phalcon\Validation\Message;
  * @nosubgrouping
  */
 class Password extends Validator implements Validation\ValidatorInterface {
-  const PASSWORD_MIN_LENGTH = 8;
-  const PASSWORD_MAX_LENGTH = 30;
+  const MIN_LENGTH = 8;
+  const MAX_LENGTH = 30;
 
 
   /**
@@ -34,10 +34,10 @@ class Password extends Validator implements Validation\ValidatorInterface {
 
     if (empty($value))
       $message = "La password è obbligatoria.";
-    elseif (strlen($value) < self::PASSWORD_MIN_LENGTH)
-      $message = sprintf("La password deve contenere almeno %d caratteri.", self::PASSWORD_MIN_LENGTH);
-    elseif (strlen($value) > self::PASSWORD_MAX_LENGTH)
-      $message = sprintf("La password può contenere al massimo 20 caratteri.", self::PASSWORD_MAX_LENGTH);
+    elseif (strlen($value) < self::MIN_LENGTH)
+      $message = sprintf("La password deve contenere almeno %d caratteri.", self::MIN_LENGTH);
+    elseif (strlen($value) > self::MAX_LENGTH)
+      $message = sprintf("La password può contenere al massimo %d caratteri.", self::MAX_LENGTH);
     elseif (!preg_match("#[0-9]+#", $value))
       $message = "La password deve contenere almeno un numero.";
     elseif (!preg_match("#[a-zA-Z]+#", $value))
