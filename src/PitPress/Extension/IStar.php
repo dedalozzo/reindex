329642f437543d86effecfce3cfdde64aab67f4d
Filippo Fadda <filippo.fadda@gmail.com>
2014-11-23 23:28:51 +0100

    removed $user parameter from all the functions used to have it

--- src/PitPress/Extension/IStar.php
+++ src/PitPress/Extension/IStar.php
@@ -34,18 +34,16 @@ interface IStar {
 
   /**
    * @brief Returns `true` if the current user starred this post.
-   * @param[in] User $user The current user logged in.
    * @param[out] string $starId The star document identifier related to the current post.
    * @return bool
    */
-  public function isStarred(User $user, &$starId = NULL);
+  public function isStarred(&$starId = NULL);
 
 
   /**
    * @brief Adds or removes the item to the favourites list of the current user.
-   * @param[in] User $user The current user logged in.
    */
-  public function star(User $user);
+  public function star();
 
 
   /**
