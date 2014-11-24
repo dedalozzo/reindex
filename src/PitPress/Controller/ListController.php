3e7cddee9381a99b7120a4915a474d115f177e74
Filippo Fadda <filippo.fadda@gmail.com>
2014-11-24 16:47:20 +0100

    removed from the Index.inizialize() method the list.min.js asset

--- src/PitPress/Controller/ListController.php
+++ src/PitPress/Controller/ListController.php
@@ -129,8 +129,6 @@ abstract class ListController extends BaseController {
 
   public function initialize() {
     parent::initialize();
-
-    $this->assets->addJs("/pit-bootstrap/dist/js/list.min.js", FALSE);
   }
 
 
