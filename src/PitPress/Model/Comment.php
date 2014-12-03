efca64a1e148ec505ea58cae454bb440065d01f2
Filippo Fadda <filippo.fadda@gmail.com>
2014-12-04 00:26:19 +0100

    Comment now extends Storable

--- src/PitPress/Model/Comment.php
+++ src/PitPress/Model/Comment.php
@@ -17,16 +17,7 @@ namespace PitPress\Model;
  * the answer) before any answers is showed.
  * @nosubgrouping
  */
-class Comment extends Reply {
-
-  public function save() {
-    // We don't use the supertype `reply`, because a comment is different from a reply.
-    $this->meta['supertype'] = 'comment';
-
-    // We need to bypass the father's method and call instead the grandpa one. This is done to avoid override the
-    // supertype we just assigned.
-    parent::save(TRUE);
-  }
+class Comment extends Storable {
 
 }
 
