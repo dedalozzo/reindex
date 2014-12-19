0a12bd072a823482c71e0683c63f92f0b5574d70
Filippo Fadda <filippo.fadda@gmail.com>
2014-12-19 01:46:25 +0100

    version has been updated to 0.7.4

--- src/PitPress/Version.php
+++ src/PitPress/Version.php
@@ -21,7 +21,7 @@ class Version {
 
   const MAJOR = '0'; //!< Major release number.
   const MINOR = '7'; //!< Minor release number.
-  const MAINTENANCE = '3'; //!< Maintenance release number (bug fixes only).
+  const MAINTENANCE = '4'; //!< Maintenance release number (bug fixes only).
 
 
   /**
