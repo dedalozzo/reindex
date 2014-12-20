85f0b664277807fb687d02faf8e3ad97cf27fc58
Filippo Fadda <filippo.fadda@gmail.com>
2014-12-20 23:41:19 +0100

    added the host parameter to the mysqli_connect()

--- services/mysql.php
+++ services/mysql.php
@@ -11,8 +11,8 @@
 // Creates an instance of MySQL client and return it.
 $di->setShared('mysql',
   function() use ($config) {
-    $mysql = mysqli_connect('localhost', $config->mysql->user, $config->mysql->password) or die(mysqli_error($mysql));
-    mysqli_select_db($mysql, $config->mysql->database) or die(mysql_error());
+    $mysql = mysqli_connect($config->mysql->host, $config->mysql->user, $config->mysql->password) or die(mysqli_error($mysql));
+    mysqli_select_db($mysql, $config->mysql->database) or die(mysql_error($mysql));
 
     return $mysql;
   }
