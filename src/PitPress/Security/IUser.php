42aa2ebb90f298e790bc499333ea25844e6bf61d
Filippo Fadda <filippo.fadda@gmail.com>
2014-12-20 23:49:42 +0100

    added IUser, a new interface with common methods to handle users

---
+++ src/PitPress/Security/IUser.php
@@ -0,0 +1,69 @@
+<?php
+
+//! @file IUser.php
+//! @brief This file contains the IUser class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Security;
+
+
+/**
+ * @brief This interface defines common methods between every class who represent an user.
+ * @nosubgrouping
+ */
+interface IUser {
+
+
+  /**
+   * @brief Returns the user id if any, otherwise `null`.
+   * @return string|bool
+   */
+  function getId();
+
+
+  /**
+   * @brief Returns `true` if the user has been confirmed.
+   * @return bool
+   */
+  function isConfirmed();
+
+
+  /**
+   * @brief Returns `true` in case the user is a guest.
+   * @return bool
+   */
+  function isGuest();
+
+
+  /**
+   * @brief Returns `true` in case the user is a moderator.
+   * @return bool
+   */
+  function isModerator();
+
+
+  /**
+   * @brief Returns `true` in case the user is an administrator.
+   * @return bool
+   */
+  function isAdmin();
+
+
+  /**
+   * @brief Returns `true` in case the user is an editor.
+   * @details An user can obtain this privilege earning reputation.
+   * @return bool
+   */
+  function isEditor();
+
+
+  /**
+   * @brief Returns `true` in case the user is a reviewer.
+   * @details An user can obtain this privilege earning reputation.
+   * @return bool
+   */
+  function isReviewer();
+
+}
