4d001f68ffee759fc634c719762d59babea0a7f8
Filippo Fadda <filippo.fadda@gmail.com>
2014-12-24 20:36:53 +0100

    added System class, a special user to perform special tasks

---
+++ src/PitPress/Security/System.php
@@ -0,0 +1,81 @@
+<?php
+
+//! @file System.php
+//! @brief This file contains the System class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Security;
+
+
+/**
+ * @brief A special user used to perform special task.
+ * @nosubgrouping
+ */
+class System implements IUser {
+
+
+  /**
+   * @brief This implementation returns always `null`.
+   * @return null
+   */
+  public function getId() {
+    return NULL;
+  }
+
+
+  /**
+   * @brief This implementation returns always `true`.
+   * @return bool
+   */
+  public function isConfirmed() {
+    return TRUE;
+  }
+
+
+  /**
+   * @brief This implementation returns always `false`.
+   * @return bool
+   */
+  public function isGuest() {
+    return FALSE;
+  }
+
+
+  /**
+   * @brief This implementation returns always `true`.
+   * @return bool
+   */
+  public function isModerator() {
+    return TRUE;
+  }
+
+
+  /**
+   * @brief This implementation returns always `true`.
+   * @return bool
+   */
+  public function isAdmin() {
+    return TRUE;
+  }
+
+
+  /**
+   * @brief This implementation returns always `true`.
+   * @return bool
+   */
+  public function isEditor() {
+    return TRUE;
+  }
+
+
+  /**
+   * @brief This implementation returns always `true`.
+   * @return bool
+   */
+  public function isReviewer() {
+    return TRUE;
+  }
+
+}
