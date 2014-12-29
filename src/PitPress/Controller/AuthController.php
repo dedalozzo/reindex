4f974637c8d416f4bfebb9ae26cf6760098cd971
Filippo Fadda <filippo.fadda@gmail.com>
2014-12-29 03:05:50 +0100

    compressed in a line a forward to 404

--- src/PitPress/Controller/AuthController.php
+++ src/PitPress/Controller/AuthController.php
@@ -205,12 +205,7 @@ class AuthController extends BaseController {
    * @brief Displays the logon form.
    */
   public function logonAction() {
-    if ($this->user->isMember())
-      return $this->dispatcher->forward(
-        [
-          'controller' => 'error',
-          'action' => 'show404'
-        ]);
+    if ($this->user->isMember()) return $this->dispatcher->forward(['controller' => 'error', 'action' => 'show404']);
 
     if ($this->request->getPost('signup'))
       $this->signUp();
