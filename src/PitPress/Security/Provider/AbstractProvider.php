ad2452db64133aaab08cc98cda7527e8be526181
Filippo Fadda <filippo.fadda@gmail.com>
2015-01-05 00:20:09 +0100

    added an abstract class, the anchestor of all providers class

---
+++ src/PitPress/Security/Provider/AbstractProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file AbstractProvider.php
+//! @brief This file contains the AbstractProvider class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Security\Provider;
+
+
+abstract class AbstractProvider {
+
+}
