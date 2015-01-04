75d64721646a5b9f8d61c7bfd1be6d76eb8f6d82
Filippo Fadda <filippo.fadda@gmail.com>
2015-01-05 00:19:31 +0100

    added the Facebook provider

---
+++ src/PitPress/Security/Provider/FacebookProvider.php
@@ -0,0 +1,113 @@
+<?php
+
+//! @file FacebookProvider.php
+//! @brief This file contains the FacebookProvider class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+namespace PitPress\Security\Provider;
+
+
+class FacebookProvider extends AbstractProvider implements IProvider {
+
+
+  /**
+   * @brief Returns the provider's name.
+   * @return string
+   */
+  function getName() {
+    //! @todo: Implement getName() method.
+  }
+
+
+  /**
+   * @brief Returns the user's id.
+   * @return string
+   */
+  function getId() {
+    //! @todo: Implement getId() method.
+  }
+
+
+  /**
+   * @brief Returns the username.
+   * @return string
+   */
+  function getUsername() {
+    //! @todo: Implement getUsername() method.
+  }
+
+
+  /**
+   * @brief Returns the user's first name.
+   * @return string
+   */
+  function getFirstName() {
+    //! @todo: Implement getFirstName() method.
+  }
+
+
+  /**
+   * @brief Returns the user's last name.
+   * @return string
+   */
+  function getLastName() {
+    //! @todo: Implement getLastName() method.
+  }
+
+
+  /**
+   * @brief Returns the user's language setting.
+   * @return string
+   */
+  function getLocale() {
+    //! @todo: Implement getLocale() method.
+  }
+
+
+  /**
+   * @brief Returns the user's time offset.
+   * @return integer
+   */
+  function getTimeOffSet() {
+    //! @todo: Implement getTimeOffSet() method.
+  }
+
+
+  /**
+   * @brief Returns `true` in case the user's email has been verified, `false` otherwise.
+   * @return string
+   */
+  function isVerified() {
+    //! @todo: Implement isVerified() method.
+  }
+
+
+  /**
+   * @brief Returns the user's emails.
+   * @return array
+   */
+  function getEmails() {
+    //! @todo: Implement getEmails() method.
+  }
+
+
+  /**
+   * @brief Returns the user friends list.
+   * @return array
+   */
+  function getFriends() {
+    //! @todo: Implement getFriends() method.
+  }
+
+
+  /**
+   * @brief Returns extra data.
+   * @return array
+   */
+  function getExtra() {
+    //! @todo: Implement getExtra() method.
+  }
+
+}
