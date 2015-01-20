c1c7076997c0009042fbb9f258be78ddace4cc9d
Filippo Fadda <filippo.fadda@gmail.com>
2015-01-21 00:19:18 +0100

    added assignTo() method and User dependency

--- src/PitPress/Security/Consumer/IConsumer.php
+++ src/PitPress/Security/Consumer/IConsumer.php
@@ -10,6 +10,9 @@
 namespace PitPress\Security\Consumer;
 
 
+use PitPress\Model\User;
+
+
 /**
  * @brief This interface defines common methods between every class who represent an OAuth Service Provider.
  * @nosubgrouping
@@ -18,85 +21,9 @@ interface IConsumer {
 
 
   /**
-   * @brief Returns the provider's name.
-   * @return string
+   * @brief Assigns the retrieved metadata to the user.
+   * @param[in] User $user The user instance.
    */
-  function getName();
+  function assignTo(User $user);
 
-
-  /**
-   * @brief Returns the user's id.
-   * @return string
-   */
-  function getId();
-
-
-  /**
-   * @brief Returns the primary e-mail.
-   * @return string
-   */
-  function getPrimaryEmail();
-
-
-  /**
-   * @brief Returns `true` in case the user's email has been verified, `false` otherwise.
-   * @return string
-   */
-  function isVerified();
-
-
-  /**
-   * @brief Returns the username.
-   * @return string
-   */
-  function getUsername();
-
-
-  /**
-   * @brief Returns the user's first name.
-   * @return string
-   */
-  function getFirstName();
-
-
-  /**
-   * @brief Returns the user's last name.
-   * @return string
-   */
-  function getLastName();
-
-
-  /**
-   * @brief Returns the user's language setting.
-   * @return string
-   */
-  function getLocale();
-
-
-  /**
-   * @brief Returns the user's time offset.
-   * @return integer
-   */
-  function getTimeOffSet();
-
-
-  /**
-   * @brief Returns the user's secondary e-mails.
-   * @return array
-   */
-  function getSecondaryEmails();
-
-
-  /**
-   * @brief Returns the user friends list.
-   * @return array
-   */
-  function getFriends();
-
-
-  /**
-   * @brief Returns extra data.
-   * @return array
-   */
-  function getExtra();
 }
