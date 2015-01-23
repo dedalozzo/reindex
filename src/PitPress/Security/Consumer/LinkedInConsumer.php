1bec1e7c6e670975fd58cc094087f448f014b56a
Filippo Fadda <filippo.fadda@gmail.com>
2015-01-23 03:10:13 +0100

    renamed consume() to join()

--- src/PitPress/Security/Consumer/LinkedInConsumer.php
+++ src/PitPress/Security/Consumer/LinkedInConsumer.php
@@ -48,10 +48,10 @@ class LinkedInConsumer extends OAuth2Consumer {
   }
 
 
-  public function consume() {
+  public function join() {
     $userData = $this->fetch('/people/~:(id,email-address,first-name,last-name,public-profile-url,headline,summary,date-of-birth)?format=json');
     $this->validate('id', 'emailAddress', $userData);
-    $this->process($userData, $userData['id']);
+    $this->consume($userData['id'], $userData['emailAddress'], $userData);
   }
 
 
