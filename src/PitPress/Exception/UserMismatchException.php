6d0ae473205ad4abe61134feb83ae80dc193b2c9
Filippo Fadda <filippo.fadda@gmail.com>
2015-01-23 16:07:45 +0100

    A new exception used during the "join" procedure of any OAuth2 consumer

---
+++ src/PitPress/Exception/UserMismatchException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file UserMismatchException.php
+ * @brief This file contains the UserMismatchException class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace PitPress\Exception;
+
+
+/**
+ * @brief Exception thrown when the user is not found.
+ */
+class UserMismatchException extends \RuntimeException {}
