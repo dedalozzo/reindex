ad7f04a847ae8e7f226f33bd4d01ad1771792748
Filippo Fadda <filippo.fadda@gmail.com>
2015-01-26 23:34:08 +0100

    added field names constants

--- src/PitPress/Security/Consumer/LinkedInConsumer.php
+++ src/PitPress/Security/Consumer/LinkedInConsumer.php
@@ -19,9 +19,22 @@ use PitPress\Exception\InvalidFieldException;
  */
 class LinkedInConsumer extends OAuth2Consumer {
 
+  /** @name Field Names */
+  //!@{
+  const ID = 'id';
+  const EMAIL = 'emailAddress';
+  const FIRST_NAME = 'firstName';
+  const LAST_NAME = 'lastName';
+  const BIRTHDAY = 'dateOfBirth';
+  const HEADLINE = 'headline';
+  const ABOUT = 'firstName';
+  const PROFILE_URL = 'publicProfileUrl';
+  //!@}
+
 
   // LinkedIn, like Facebook, doesn't provide a username, but PitPress needs one. So we guess the username using the
   // user public profile url. In case the username has already been taken, we add a sequence number to the end.
+  // todo this is not finished
   private function guessUsername($publicProfileUrl) {
     if (preg_match('%.+/in/(?P<username>.+)%i', $publicProfileUrl, $matches))
       return $matches['username'];
@@ -31,16 +44,14 @@ class LinkedInConsumer extends OAuth2Consumer {
 
 
   protected function update(User $user, array $userData) {
-    $user->setMetadata('email', @$userData['emailAddress'], $this->canReplacePrimaryEmail($user), FALSE);
-    $user->setMetadata('username', $this->guessUsername($userData['publicProfileUrl']), FALSE, FALSE);
-    $user->setMetadata('firstName', @$userData['firstName'], FALSE, FALSE);
-    $user->setMetadata('lastName', @$userData['lastName'], FALSE, FALSE);
-    $user->setMetadata('birthday', @$userData['dateOfBirth'], FALSE, FALSE);
-    $user->setMetadata('headline', @$userData['headline'], FALSE, FALSE);
-    $user->setMetadata('about', @$userData['summary'], FALSE, FALSE);
-    $user->setMetadata('profileUrl', @$userData['publicProfileUrl'], FALSE, FALSE);
+    $user->setMetadata('username', $this->guessUsername($userData[static::PROFILE_URL]), FALSE, FALSE);
+    $user->setMetadata('firstName', @$userData[static::FIRST_NAME], FALSE, FALSE);
+    $user->setMetadata('lastName', @$userData[static::LAST_NAME], FALSE, FALSE);
+    $user->setMetadata('birthday', @$userData[static::BIRTHDAY], FALSE, FALSE);
+    $user->setMetadata('headline', @$userData[static::HEADLINE], FALSE, FALSE);
+    $user->setMetadata('about', @$userData[static::ABOUT], FALSE, FALSE);
 
-    $user->addLogin($this->getName(), $userData['id'], $userData['emailAddress'], $userData['publicProfileUrl']);
+    $user->addLogin($this->getName(), $userData[static::ID], $userData[static::EMAIL], $userData[static::PROFILE_URL]);
     $user->internetProtocolAddress = $_SERVER['REMOTE_ADDR'];
     $user->save();
   }
@@ -48,8 +59,8 @@ class LinkedInConsumer extends OAuth2Consumer {
 
   public function join() {
     $userData = $this->fetch('/people/~:(id,email-address,first-name,last-name,public-profile-url,headline,summary,date-of-birth)?format=json');
-    $this->validate('id', 'emailAddress', $userData);
-    $this->consume($userData['id'], $userData['emailAddress'], $userData);
+    $this->validate($userData);
+    $this->consume($userData[static::ID], $userData[static::EMAIL], $userData);
   }
 
 
