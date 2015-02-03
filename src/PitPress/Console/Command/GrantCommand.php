3d2dff6ac12073c8bbe2ef840d7dc2a729bc01ed
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-03 01:29:18 +0100

    GrantCommand allows to grant a privilege to a user

---
+++ src/PitPress/Console/Command/GrantCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file GrantCommand.php
+ * @brief This file contains the GrantCommand class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace PitPress\Console\Command;
+
+
+/**
+ * @brief Grants a privilege to a user.
+ * @nosubgrouping
+ */
+class GrantCommand extends AbstractAssignCommand {
+
+
+  /**
+   * @brief Configures the command.
+   */
+  protected function configure() {
+    $this->setName('grant');
+    parent::configure();
+  }
+
+}
