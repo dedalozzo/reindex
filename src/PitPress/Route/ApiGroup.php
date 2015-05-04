0d6dc053f83c0ee84565ca27d33a73bf8b7722d2
Filippo Fadda <filippo.fadda@gmail.com>
2015-05-04 18:51:33 +0200

    added many APIs routes

--- src/PitPress/Route/ApiGroup.php
+++ src/PitPress/Route/ApiGroup.php
@@ -31,10 +31,21 @@ class ApiGroup extends Group {
 
     $this->setHostName('api.'.DI::getDefault()['config']['application']['domainName']);
 
-    //$this->addPost('/like/', ['action' => 'like']);
+    $this->addPost('/like/', ['action' => 'like']);
     $this->addPost('/star/', ['action' => 'star']);
+    $this->addPost('/submit/', ['action' => 'submit']);
+    $this->addPost('/approve/', ['action' => 'approve']);
+    $this->addPost('/return-for-revision/', ['action' => 'returnForRevision']);
+    $this->addPost('/reject/', ['action' => 'reject']);
+    $this->addPost('/revert/', ['action' => 'revert']);
     $this->addPost('/move-to-trash/', ['action' => 'moveToTrash']);
     $this->addPost('/restore/', ['action' => 'restore']);
+    $this->addPost('/mark-as-draft/', ['action' => 'markAsDraft']);
+    $this->addPost('/close/', ['action' => 'close']);
+    $this->addPost('/lock/', ['action' => 'lock']);
+    $this->addPost('/unprotect/', ['action' => 'unprotect']);
+    $this->addPost('/hide/', ['action' => 'hide']);
+    $this->addPost('/show/', ['action' => 'show']);
   }
 
 }
