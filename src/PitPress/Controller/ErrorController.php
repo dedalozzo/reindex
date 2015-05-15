6fec3e8083a933931cc4946dc312116a8f3288e1
Filippo Fadda <filippo.fadda@gmail.com>
2015-05-15 02:09:43 +0200

    show404Action is now called basicAction and many additions have been made

--- src/PitPress/Controller/ErrorController.php
+++ src/PitPress/Controller/ErrorController.php
@@ -24,13 +24,15 @@ class ErrorController extends BaseController {
   /**
    * @brief Shows the 404 error.
    */
-  public function show404Action() {
+  public function basicAction() {
     $this->response->setHeader('HTTP/1.0 404', 'Not Found');
+    $this->view->setVar('code', '400');
     $this->view->setVar('title', 'Pagina non trovata');
+    $this->view->setVar('message', 'La pagina richiesta non è disponibile. Il link che hai seguito non è funzionante o la pagina è stata rimossa.');
     $this->view->setVar('method', strtolower($_SERVER['REQUEST_METHOD']));
     $this->view->setVar('url', "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
     $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
-    $this->view->pick('views/error/404');
+    $this->view->pick('views/error/basic');
   }
 
 } 
