0c2bb4012f62e51b49672d44403d6a9b48da9fa2
Filippo Fadda <filippo.fadda@gmail.com>
2015-05-16 01:06:41 +0200

    removed log from markdown service

--- services/markdown.php
+++ services/markdown.php
@@ -14,7 +14,7 @@ use Pygmentize\Pygmentize;
 // Creates an instance of Redis client and return it.
 $di->setShared('markdown',
 
-  function() use ($config, $log) {
+  function() use ($config) {
 
     // For a description of the predefined constants see: https://github.com/kjdev/php-ext-hoedown#predefined-constants.
     $hoedown = new Hoedown(
@@ -59,7 +59,7 @@ $di->setShared('markdown',
         ]
     );
 
-    $hoedown->addRender("blockcode", function($code, $language) use ($log) {
+    $hoedown->addRender("blockcode", function($code, $language) {
        return Pygmentize::highlight($code, $language);
       }
     );
