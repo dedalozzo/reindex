0db53fbba2c86bec93836f1c836177305ee76259
Filippo Fadda <filippo.fadda@gmail.com>
2015-05-19 17:05:04 +0200

    added show401 error

--- src/PitPress/Controller/ErrorController.php
+++ src/PitPress/Controller/ErrorController.php
@@ -22,9 +22,9 @@ class ErrorController extends BaseController {
 
 
   /**
-   * @brief Shows the 404 error.
+   * @brief 404 Not Found.
    */
-  public function basicAction() {
+  public function show404Action() {
     $this->response->setHeader('HTTP/1.0 404', 'Not Found');
     $this->view->setVar('code', '400');
     $this->view->setVar('title', 'Pagina non trovata');
@@ -35,4 +35,18 @@ class ErrorController extends BaseController {
     $this->view->pick('views/error/basic');
   }
 
-} 
+
+  /**
+   * @brief 401 Forbidden.
+   */
+  public function show401Action() {
+    $this->response->setHeader('HTTP/1.0 401', 'Forbidden');
+    $this->view->setVar('code', '401');
+    $this->view->setVar('title', 'Operazione vietata');
+    $this->view->setVar('message', "L'operazione richiesta è vietata.");
+    $this->view->setVar('method', strtolower($_SERVER['REQUEST_METHOD']));
+    $this->view->setVar('url', "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+    $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
+    $this->view->pick('views/error/basic');
+  }
+}
