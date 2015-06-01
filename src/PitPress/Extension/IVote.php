50741aa25a57c42bd3a01f395a2c81fb64da65b2
Filippo Fadda <filippo.fadda@gmail.com>
2015-06-01 19:18:30 +0200

    the IVote interface allows to cast a vote for a revision approval

--- src/PitPress/Extension/IVote.php
+++ src/PitPress/Extension/IVote.php
@@ -34,15 +34,19 @@ interface IVote {
   //!@{
 
   /**
-   * @brief Likes a post. Same as voteUp().
+   * @brief Registers, replaces or deletes the vote.
+   * @param[in] int $value The vote.
+   * @param[in] bool $unversion When `true` removes the version from the ID. Use `false` to cast a vote for revision
+   * approval.
    * @retval int The voting status.
    */
-  public function like();
+  public function vote($value, $unversion = TRUE);
 
 
   /**
    * @brief Vote up a post.
    * @retval int The voting status.
+   * @attention This method can't be used to cast a vote for revision approval.
    */
   public function voteUp();
 
@@ -50,35 +54,51 @@ interface IVote {
   /**
    * @brief Vote down a post.
    * @retval int The voting status.
+   * @attention This method can't be used to cast a vote for revision approval.
    */
   public function voteDown();
 
 
   /**
+   * @brief Likes a post. Same as voteUp().
+   * @retval int The voting status.
+   * @attention This method can't be used to cast a vote for revision approval.
+   */
+  public function like();
+
+
+  /**
    * @brief Returns `true` if the user has voted else otherwise.
    * @param[out] string $voteId (optional) The vote ID.
+   * @param[in] bool $unversion When `true` removes the version from the ID. Use `false` to know if the user casted a
+   * vote for revision approval.
    * @retval bool
    */
-  public function didUserVote(&$voteId = NULL);
+  public function didUserVote(&$voteId = NULL, $unversion = TRUE);
 
 
   /**
    * @brief Returns the arithmetic sum of each each vote.
+   * @param[in] bool $unversion When `true` removes the version from the ID. Use `false` to cast a vote for revision
+   * approval.
    * @retval int
    */
-  public function getScore();
+  public function getScore($unversion = TRUE);
 
 
   /**
    * @brief Returns the list of users have voted.
+   * @param[in] bool $unversion When `true` removes the version from the ID. Use `false` to get the users voted for
+   * revision approval.
    * @retval array An associative array.
    */
-  public function getUsersHaveVoted();
+  public function getUsersHaveVoted($unversion = TRUE);
 
 
   /**
    * @brief Returns the timestamp of the last vote casted.
    * @retval int
+   * @attention This method supports only unversion IDs.
    */
   public function getLastVoteTimestamp();
 
