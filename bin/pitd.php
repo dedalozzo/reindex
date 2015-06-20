c3e31595b07f28091f623ebe8dcdb53346f3522d
Filippo Fadda <filippo.fadda@gmail.com>
2015-06-21 00:21:19 +0200

    added the pit press daemon

---
+++ bin/pitd.php
@@ -0,0 +1,53 @@
+#! /usr/bin/php
+<?php
+
+/**
+ * @file pitd.php
+ * @brief The PitPress Daemon.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+use Phalcon\Config\Adapter\Ini as IniReader;
+use Phalcon\DI\FactoryDefault as DependencyInjector;
+
+use Monolog\Logger;
+use Monolog\ErrorHandler;
+use Monolog\Handler\StreamHandler;
+
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+try {
+  $root = realpath(__DIR__."/../");
+
+  // Initializes the Composer autoloading system. (Note: We don't use the Phalcon loader.)
+  require $root."/vendor/autoload.php";
+
+  // Reads the application's configuration.
+  $config = new IniReader($root.'/config.ini');
+
+  $log = new Logger('pit-press');
+
+  // Registers the Monolog error handler to log errors and exceptions.
+  ErrorHandler::register($log);
+
+  // Creates a stream handler to log debugging messages.
+  $log->pushHandler(new StreamHandler($root.'/'.$config->application->logDir."pitpress.log", Logger::DEBUG));
+
+  // The FactoryDefault Dependency Injector automatically registers the right services providing a full stack framework.
+  $di = new DependencyInjector();
+
+  // Initializes the services. The order doesn't matter.
+  require $root . "/services/config.php";
+  require $root . "/services/log.php";
+  require $root . "/services/couchdb.php";
+  require $root . "/services/redis.php";
+  require $root . "/services/markdown.php";
+  require $root . "/services/guardian.php";
+
+}
+catch (Exception $e) {
+  echo $e;
+}
