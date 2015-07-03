349aca769b97f57c661ec665036056f5fd6fecbe
Filippo Fadda <filippo.fadda@gmail.com>
2015-07-03 18:39:13 +0200

    added @details to the IObserver interface documentation

--- src/PitPress/Observer/IObserver.php
+++ src/PitPress/Observer/IObserver.php
@@ -13,6 +13,8 @@ namespace PitPress\Observer;
 
 /**
  * @brief Defines an updating interface for objects that should be notified of changes in a subject.
+ * @details This declaration differs from the one you can find in the GoF book on design patterns. Here we are using a
+ * message queue coupling with a change manager instance.
  * @nosubgrouping
  */
 interface IObserver {
