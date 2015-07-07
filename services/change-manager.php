e29f2362b6453a36f8a2157ffad996d9c80c14ef
Filippo Fadda <filippo.fadda@gmail.com>
2015-07-07 22:48:48 +0200

    change manager service has been added

---
+++ services/change-manager.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file change-manager.php
+ * @brief Loads the change manager.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+use PitPress\Mediator\ChangeManager;
+
+
+// Creates an instance of the change manager and return it.
+$di->setShared('changeManager',
+  function() use ($config, $di) {
+    $changeManager = new ChangeManager($config, $di);
+
+    return $changeManager;
+  }
+);
