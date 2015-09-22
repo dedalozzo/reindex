d64dc5287254234cf940b976bfd285ef44222a96
Filippo Fadda <filippo.fadda@gmail.com>
2015-09-23 00:23:53 +0200

    signin route has been changed to logon

--- src/ReIndex/Route/AuthGroup.php
+++ src/ReIndex/Route/AuthGroup.php
@@ -36,8 +36,8 @@ class AuthGroup extends Group {
     $this->addGet('/activate/', ['action' => 'activate']);
     $this->addGet('/signout/', ['action' => 'signOut']);
 
-    // All the following routes start with /access.
-    $this->setPrefix('/signin');
+    // All the following routes start with /logon.
+    $this->setPrefix('/logon');
     $this->add('/', ['action' => 'logon'], ['GET', 'POST']);
     $this->addGet('/facebook/', ['action' => 'facebook']);
     $this->addGet('/google/', ['action' => 'google']);
