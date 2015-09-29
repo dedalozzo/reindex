d4dbf8849f52bbea7e236f9de2ccf9ab0f7b3845
Filippo Fadda <filippo.fadda@gmail.com>
2015-09-29 18:11:28 +0200

    updated the minor release number

--- src/ReIndex/Version.php
+++ src/ReIndex/Version.php
@@ -20,7 +20,7 @@ namespace ReIndex;
 class Version extends \EoC\Version {
 
   const MAJOR = '0'; //!< Major release number.
-  const MINOR = '10'; //!< Minor release number.
-  const MAINTENANCE = '2'; //!< Maintenance release number (bug fixes only).
+  const MINOR = '11'; //!< Minor release number.
+  const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
 
 } 
