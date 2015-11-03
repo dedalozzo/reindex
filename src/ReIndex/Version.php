b7e1572354cf8df99798c524c4e995038c85ca2e
Filippo Fadda <filippo.fadda@gmail.com>
2015-11-04 00:48:51 +0100

    updated the minor version to 0.12.0

--- src/ReIndex/Version.php
+++ src/ReIndex/Version.php
@@ -20,7 +20,7 @@ namespace ReIndex;
 class Version extends \EoC\Version {
 
   const MAJOR = '0'; //!< Major release number.
-  const MINOR = '11'; //!< Minor release number.
-  const MAINTENANCE = '1'; //!< Maintenance release number (bug fixes only).
+  const MINOR = '12'; //!< Minor release number.
+  const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
 
 } 
