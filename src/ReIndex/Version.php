a805e5f7d8d1e78199c4aa32af9d7fb9a8a616ac
Filippo Fadda <filippo.fadda@gmail.com>
2015-12-24 23:49:22 +0100

    updated maintenance version

--- src/ReIndex/Version.php
+++ src/ReIndex/Version.php
@@ -21,6 +21,6 @@ class Version extends \EoC\Version {
 
   const MAJOR = '0'; //!< Major release number.
   const MINOR = '12'; //!< Minor release number.
-  const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
+  const MAINTENANCE = '1'; //!< Maintenance release number (bug fixes only).
 
 } 
