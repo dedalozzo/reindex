8f29e038c328fcaa518f3ccf3817c7a1afe56a44
Filippo Fadda <filippo.fadda@gmail.com>
2016-01-03 02:17:32 +0100

    added a new Exception raised in case of an error occurred creating the oauth service

---
+++ src/ReIndex/Exception/UnableToCreateService.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file UnableToCreateService.php
+ * @brief This file contains the UnableToCreateService class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Exception;
+
+
+/**
+ * @brief Exception thrown when the OAuth2 Consumer is not able to create the service.
+ */
+class UnableToCreateService extends \RuntimeException {}
