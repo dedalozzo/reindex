aafc6b3f3ad92d7154533d0cf32e74af527dd306
Filippo Fadda <filippo.fadda@gmail.com>
2016-01-06 22:00:46 +0100

    added a new exception used in case a consumer is not supported

---
+++ src/ReIndex/Exception/ConsumerNotSupportedException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file ConsumerNotSupportedException.php
+ * @brief This file contains the ConsumerNotSupportedException class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Exception;
+
+
+/**
+ * @brief Exception thrown when a consumer is not supported by the ReIndex platform.
+ */
+class ConsumerNotSupportedException extends \RuntimeException {}
