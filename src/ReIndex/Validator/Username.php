df54661e1e070e41b28c169f045f37e8cdb23478
Filippo Fadda <filippo.fadda@gmail.com>
2016-01-15 23:35:07 +0100

    Modified the preg_match regex to check the username

--- src/ReIndex/Validator/Username.php
+++ src/ReIndex/Validator/Username.php
@@ -38,6 +38,11 @@ class Username extends Validator implements Validation\ValidatorInterface {
 
   /**
    * @brief Executes the validation.
+   * @details A username should match the following conditions:\n
+   *   1. only one special char `.`, `_`, `-` are allowed and it must not be at the extremes of the string;
+   *   2. the first character cannot be a number;
+   *   3. all the other characters allowed are letters and numbers;
+   *   4. the total length should be between 5 and 24 chars.
    * @param[in] Phalcon\Validation $validator An instance of a Phalcon validation component.
    * @param[in] string $attribute The attribute to be validated.
    * @retval bool
@@ -51,8 +56,10 @@ class Username extends Validator implements Validation\ValidatorInterface {
       $message = sprintf("Il nome utente deve contenere almeno %d caratteri.", self::MIN_LENGTH);
     elseif (mb_strlen($value, "UTF-8") > self::MAX_LENGTH)
       $message = sprintf("Il nome utente può contenere al massimo %d caratteri.", self::MAX_LENGTH);
-    elseif (!preg_match('/^[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+$/iu', $value))
-      $message = "Il nome utente non deve contenere caratteri speciali, numeri o spazi.";
+    elseif (!preg_match('/(?=^.{3,20}$)^[a-zA-Z][a-zA-Z0-9]*[._-]?[a-zA-Z0-9]+$/', $value))
+      $message = "Il nome utente deve cominciare con una lettera (maiuscola o minuscola), può contenere un solo
+        carattere speciale (.-_) e questo carattere non deve essere all'inizio o alla fine della stringa, tutti gli altri
+        caratteri permessi sono numeri e lettere.";
     elseif ($this->guardian->isTaken($value))
       $message = "Il nome utente non è disponibile.";
     else
