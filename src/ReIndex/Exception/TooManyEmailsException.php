489e671aacd48b5d08f1c2313de9602ae3014f65
Filippo Fadda <filippo.fadda@gmail.com>
2016-03-11 03:14:51 +0100

    a nee exception has been added

---
+++ src/ReIndex/Exception/TooManyEmailsException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file TooManyEmailsException.php
+ * @brief This file contains the TooManyEmailsException class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Exception;
+
+
+/**
+ * @brief Exception thrown when an user is trying to add too many e-emails.
+ */
+class TooManyEmailsException extends \RuntimeException {}
