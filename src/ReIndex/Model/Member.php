11a6ce703c724eed66fe3a9cadbade5cd2892a41
Filippo Fadda <filippo.fadda@gmail.com>
2016-03-25 22:13:20 +0100

    added friendship and blacklist methods

--- src/ReIndex/Model/Member.php
+++ src/ReIndex/Model/Member.php
@@ -11,6 +11,7 @@
 namespace ReIndex\Model;
 
 
+use EoC\Couch;
 use EoC\Opt\ViewQueryOpts;
 
 use ReIndex\Extension;
@@ -113,14 +114,6 @@ class Member extends Storable implements IUser, Extension\ICount {
       return $reputation;
     else
       return 1;
-  }
-
-
-  /**
-   * @brief Adds a bunch of potential friends to the list of friends.
-   * @todo Implement the AddFriends() method.
-   */
-  public function addFriends() {
   }
 
 
@@ -529,7 +522,8 @@ class Member extends Storable implements IUser, Extension\ICount {
    * @brief Searches for the user identified by the specified email, if any returns it, otherwise return `false`.
    * @param[in] string $consumerName The consumer name.
    * @param[in] string $userId The user id.
-   * @param[in] string $profileUrl The user's profile URL.
+   * @param[in] string $profileU§+
+   *rl The user's profile URL.
    * @param[in] string $email The user's e-mail.
    * @param[in] string $username The username.
    * @param[in] bool $verified The e-mail is verified or not.
@@ -559,6 +553,133 @@ class Member extends Storable implements IUser, Extension\ICount {
    */
   public function isLoginAlreadyPresent($login) {
     return isset($this->meta['logins'][$login]);
+  }
+
+  //!@}
+
+
+  /** @name Friendship Management Methods */
+  //!@{
+
+  public function isFriend(Member $member, &$friendshipId = NULL, &$approved = FALSE) {
+    $opts = new ViewQueryOpts();
+    $opts->doNotReduce()->setLimit(1)->setKey([$this->id, $member->id]);
+
+    $result = $this->couch->queryView("friendship", "perMember", NULL, $opts);
+
+    if ($result->isEmpty())
+      return FALSE;
+    else {
+      $friendshipId = $result[0]['id'];
+      $approved = $result[0]['approved'];
+      return TRUE;
+    }
+  }
+
+
+  public function addFriend(Member $member) {
+    if ($this->match($member->id))
+      throw new Exception\UserMismatchException("You can't add yourself as a friend. Are you stupid or what?");
+
+    if ($member->isBlacklisted($this))
+      throw new Exception\UserMismatchException("Unfortunately you have been blacklisted from the user you are trying to add as a friend.");
+
+    if ($this->isFriend($member, $friendshipId, $approved)) {
+      if ($approved)
+        throw new Exception\UserMismatchException("You are already friend with the user.");
+      else
+        throw new Exception\UserMismatchException("You have already sent a friend request to this user.");
+    }
+
+    // Creates and stores the friendship.
+    $friendship = Friendship::request($member);
+    $this->couch->saveDoc($friendship);
+  }
+
+
+  public function removeFriend(Member $member) {
+    // We don't care if the friendship has been approved or not, we just remove it.
+    if ($this->isFriend($member, $friendshipId)) {
+      $friendship = $this->couch->getDoc(Couch::STD_DOC_PATH, $friendshipId);
+      $this->couch->deleteDoc(Couch::STD_DOC_PATH, $friendshipId, $friendship->rev);
+    }
+    else
+      throw new Exception\UserMismatchException("You are not friends.");
+  }
+
+
+  public function approveFriend(Member $member) {
+    if ($this->isFriend($member, $friendshipId)) {
+      $friendship = $this->couch->getDoc(Couch::STD_DOC_PATH, $friendshipId);
+
+      if (!$this->match($friendship->receiverId)) throw new Exception\UserMismatchException("You cannot approve someone else's friendship.");
+
+      if (!$friendship->isApproved()) {
+        $friendship->approve();
+        $this->couch->saveDoc($friendship);
+      }
+      else
+        throw new \RuntimeException("The friendship has been approved already.");
+    }
+    else
+      throw new Exception\UserMismatchException("You are not friends.");
+  }
+
+
+  public function rejectFriend(Member $member, $blacklist = FALSE) {
+
+    if ($this->isFriend($member, $friendshipId)) {
+      $friendship = $this->couch->getDoc(Couch::STD_DOC_PATH, $friendshipId);
+
+      if (!$this->match($friendship->receiverId)) throw new Exception\UserMismatchException("It's not up to you approve someone else's friendship.");
+
+      if (!$friendship->isApproved()) {
+        $friendship->approve();
+        $this->couch->saveDoc($friendship);
+      }
+      else
+        throw new \RuntimeException("The friendship has been approved already.");
+    }
+    else
+      throw new Exception\UserMismatchException("You are not friends.");
+  }
+
+
+  public function isBlacklisted(Member $member, &$blackId) {
+    $opts = new ViewQueryOpts();
+    $opts->doNotReduce()->setLimit(1)->setKey([$this->id, $member->id]);
+
+    $result = $this->couch->queryView("blacklist", "perMember", NULL, $opts);
+
+    if ($result->isEmpty())
+      return FALSE;
+    else {
+      $blackId = $result[0]['id'];
+      return TRUE;
+    }
+  }
+
+
+  public function addToBlackList(Member $member) {
+
+  }
+
+
+  public function removeFromBlacklist(Member $member) {
+
+  }
+
+
+  public function getBlacklist() {
+
+  }
+
+
+  /**
+   * @brief Adds a bunch of potential friends to the list of friends, using a list of e-mails.
+   * @todo Implement the inviteFriends() method.
+   */
+  public function inviteFriends() {
   }
 
   //!@}
