863aae551e00f1078c539d618fb3ccf4525a5f19
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-02 01:56:31 +0200

    added the developer role implementation

---
+++ src/ReIndex/Security/Role/DeveloperRole.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file DeveloperRole.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class DeveloperRole extends AbstractRole {
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
