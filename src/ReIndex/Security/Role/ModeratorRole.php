b43663609417bd4e2007a816c8c6781589723dc1
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-02 01:56:01 +0200

    added the ModeratorRole implementation

---
+++ src/ReIndex/Security/Role/ModeratorRole.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file ModeratorRole.php
+ * @brief This file contains the ModeratorRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class ModeratorRole extends AbstractRole {
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
