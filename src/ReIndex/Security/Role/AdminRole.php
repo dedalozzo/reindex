df795f38c73b12b6dc9815090c8266b85d190c17
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-02 01:56:49 +0200

    added the admin role implementation

---
+++ src/ReIndex/Security/Role/AdminRole.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file AdminRole.php
+ * @brief This file contains the AdminRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class AdminRole extends AbstractRole {
+
+  
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+  
+}
