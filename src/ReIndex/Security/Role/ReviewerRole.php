40864ee720d97c91bec6c3d5b78bfa387dacd461
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-02 01:56:18 +0200

    added the ReviewerRole implementation

---
+++ src/ReIndex/Security/Role/ReviewerRole.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file ReviewerRole.php
+ * @brief This file contains the ReviewerRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class ReviewerRole extends AbstractRole {
+
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
