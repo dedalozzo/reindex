b9f14d2e84c8a1f05bce0e0e0fc9b12494ed00c0
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-02 01:55:50 +0200

    added the EditorRole implementation

---
+++ src/ReIndex/Security/Role/EditorRole.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file EditorRole.php
+ * @brief This file contains the EditorRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class EditorRole extends AbstractRole {
+
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
