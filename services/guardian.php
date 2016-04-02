6c56c936a99dd7b794b8e437ff172c6082bdd10c
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-02 02:12:41 +0200

    loading the roles

--- services/guardian.php
+++ services/guardian.php
@@ -9,12 +9,19 @@
 
 
 use ReIndex\Security;
+use ReIndex\Security\Role;
 
 
 // Creates an instance of Guardian and return it.
 $di->setShared('guardian',
   function() use ($config, $di) {
     $guardian = new Security\Guardian($config, $di);
+    
+    $guardian->loadRole(new Role\SupervisorRole());
+    $guardian->loadRole(new Role\AdminRole());
+    $guardian->loadRole(new Role\ModeratorRole());
+    $guardian->loadRole(new Role\ReviewerRole());
+    $guardian->loadRole(new Role\EditorRole());
 
     return $guardian;
   }
