9cd474a3c5c9db00dda6ce723c90a74e81887327
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-09 01:17:09 +0200

    removed DocStatus class

---
+++ src/ReIndex/Security/Role/Permission/Reviewer/ApproveVersionPermission.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file ApproveVersionPermission.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Reviewer;
+
+
+class ApproveVersionPermission {
+
+  /**
+   * @brief Returns `true` if the document can be approved, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+    if ($this->user->isModerator() && ($this->isCreated() or $this->isDraft() or $this->isSubmittedForPeerReview()))
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+}
---
+++ src/ReIndex/Security/Role/Permission/Reviewer/RejectVersionPermission.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file RejectVersionPermission.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Reviewer;
+
+
+class RejectVersionPermission {
+
+  /**
+   * @brief Returns `true` if the document revision can be rejected, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+    if ($this->user->isModerator() && $this->isSubmittedForPeerReview())
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+}
