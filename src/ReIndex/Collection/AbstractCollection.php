8a80d8e1c621d7b972b7aa1a8ca6cc6434bbc767
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-08 22:31:46 +0200

    an abstract superclass with common methods to all the collections

---
+++ src/ReIndex/Collection/AbstractCollection.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @file AbstractCollection.php
+ * @brief This file contains the AbstractCollection class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Collection;
+
+
+/**
+ * @brief This class is used to represent a generic collection.
+ * @details This class implements `IteratorAggregate`, `Countable`, and `ArrayAccess`.
+ * @nosubgrouping
+ */
+abstract class AbstractCollection implements \IteratorAggregate, \Countable, \ArrayAccess {
+
+  protected $meta;
+
+  const NAME = "collection";
+
+
+  /**
+   * @brief Creates a new collection of e-mails.
+   * @param[in] array $meta Member's array of metadata.
+   */
+  public function __construct(array &$meta) {
+    $this->meta = &$meta;
+  }
+
+
+  /**
+   * @brief Removes the specified value from the collection.
+   * @param[in] mixed $value A value.
+   */
+  public function remove($value) {
+    unset($this->meta[static::NAME][$value]);
+  }
+
+
+  /**
+   * @brief Returns `true` if the value is already present, `false` otherwise.
+   * @param[in] mixed $value A value.
+   * @retval bool
+   */
+  public function exists($value) {
+    return isset($this->meta[static::NAME][$value]);
+  }
+
+  
+  /**
+   * @brief Returns the collection as a real array.
+   * @retval array An associative array using as keys the e-mail addresses, and as values if the address are verified or
+   * not.
+   */
+  public function asArray() {
+    return $this->meta[static::NAME];
+  }
+
+
+  /**
+   * @brief Returns an external iterator.
+   * @retval [ArrayIterator](http://php.net/manual/en/class.arrayiterator.php).
+   */
+  public function getIterator() {
+    return new \ArrayIterator($this->meta[static::NAME]);
+  }
+
+
+  /**
+   * @brief Returns the number of documents found.
+   * @retval integer Number of documents.
+   */
+  public function count() {
+    return count($this->meta[static::NAME]);
+  }
+
+
+  /**
+   * @brief Whether or not an offset exists.
+   * @details This method is executed when using `isset()` or `empty()` on objects implementing ArrayAccess.
+   * @param[in] integer $offset An offset to check for.
+   * @retval bool Returns `true` on success or `false` on failure.
+   */
+  public function offsetExists($offset) {
+    return isset($this->meta[static::NAME][$offset]);
+  }
+
+
+  /**
+   * @brief Returns the value at specified offset.
+   * @details This method is executed when checking if offset is `empty()`.
+   * @param[in] integer $offset The offset to retrieve.
+   * @retval mixed Can return all value types.
+   */
+  public function offsetGet($offset)  {
+    return $this->meta[static::NAME][$offset];
+  }
+
+
+  //! @cond HIDDEN_SYMBOLS
+
+  public function offsetSet($offset, $value) {
+    throw new \BadMethodCallException("Result is immutable and cannot be changed.");
+  }
+
+
+  public function offsetUnset($offset) {
+    throw new \BadMethodCallException("Result is immutable and cannot be changed.");
+  }
+
+  //! @endcond
+
+}
