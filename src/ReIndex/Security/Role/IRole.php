eaabb6762b4f8ba08f17fb0e34a5373041fd55f9
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-10 23:28:49 +0200

    modified IRole brief

--- src/ReIndex/Security/Role/IRole.php
+++ src/ReIndex/Security/Role/IRole.php
@@ -14,7 +14,7 @@ use ReIndex\Security\Role\Operation\IOperation;
 
 
 /**
- * @brief You may implement this interface to create a new role.
+ * @brief You may implement this interface to create a new role class.
  * @details Roles are hereditary, that means Admin is a subclass of Moderator. When you grant a role to a member, the
  * less important role is replaced with the most important. In the previous example, since Admin is a subclass of
  * Moderator, the Admin role will replace the Moderator role. There are cases when you have different roles, since each
