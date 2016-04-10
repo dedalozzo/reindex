b7767d3e237aef5ac0d4fcf65574b515142c8c2e
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-11 00:25:04 +0200

    removed all the permission stuff, because it doens't belong here

--- src/ReIndex/Security/Role/IRole.php
+++ src/ReIndex/Security/Role/IRole.php
@@ -10,9 +10,6 @@
 namespace ReIndex\Security\Role;
 
 
-use ReIndex\Security\Role\Operation\IOperation;
-
-
 /**
  * @brief You may implement this interface to create a new role class.
  * @details Roles are hereditary, that means Admin is a subclass of Moderator. When you grant a role to a member, the
@@ -36,36 +33,6 @@ interface IRole  {
    * @return string
    */
   function getDescription();
-
-
-  /**
-   * @brief Grants the permission to the current role.
-   * @details If another permission with the same name exists, replaces it.
-   * @param[in] string $name The permission name.
-   * @param[in] string $class The class name of the permission, included its namespace.
-   */
-  function grantPermission($name, $class);
-
-
-  /**
-   * @brief Revokes the permission associated to the current role.
-   * @param[in] string The operation name.
-   */
-  function revokePermission($name);
-
-
-  /**
-   * @brief Returns `true` if the permission is granted for this role, `false` otherwise.
-   * @param[in] string The permission name.
-   */
-  function permissionExists($name);
-
-
-  /**
-   * @brief Given the permission name returns the associated permission class..
-   * @param[in] string The permission name.
-   */
-  function obtainPermissionClass($name);
 
 
   /**
