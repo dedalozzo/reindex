e4ed9041e70e4a23aa030c91f4cfe9036e0e2fc9
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-12 01:40:59 +0200

    added BanMemberPermission for the Admin

---
+++ src/ReIndex/Security/Role/Permission/Admin/BanMemberPermission.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file BanMemberPermission.php
+ * @brief This file contains the BanMemberPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Admin;
+
+
+use ReIndex\Security\Role\Permission\Moderator\BanMemberPermission as Superclass;
+
+
+class BanMemberPermission extends Superclass {
+
+
+  /**
+   * @brief Returns `true` if the user logged in is allowed to ban the current user, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+    if ($this->user->isAdmin() && !$this->member->isAdmin() && !$this->user->match($this->member->id))
+      return TRUE;
+    elseif ($this->user->isModerator() && !$this->isModerator() && !$this->user->match($this->member->id))
+      return TRUE;
+    else
+      return FALSE;
+  }
+  
+}
