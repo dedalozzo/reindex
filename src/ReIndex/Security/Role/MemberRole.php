d9994b4d06707cc626a8bce84b3a69adf629b699
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-11 02:07:46 +0200

    every member must have at least this role associated

---
+++ src/ReIndex/Security/Role/MemberRole.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file MemberRole.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class MemberRole extends AbstractRole {
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
