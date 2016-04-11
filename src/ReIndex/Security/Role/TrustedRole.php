7f0d88cac8e0261ad932d0ccaf123e41b75ee793
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-11 02:07:20 +0200

    TrustedRole inherits from MemberRole

---
+++ src/ReIndex/Security/Role/TrustedRole.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file TrustedRole.php
+ * @brief This file contains the TrustedRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class TrustedRole extends MemberRole {
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
