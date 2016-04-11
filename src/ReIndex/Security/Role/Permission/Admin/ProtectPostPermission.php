28ecf9c940569bcc884f0d1b215b62d6fbce2e09
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-12 01:41:09 +0200

    added ProtectPostPermission

---
+++ src/ReIndex/Security/Role/Permission/Admin/ProtectPostPermission.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file ProtectPostPermission.php
+ * @brief This file contains the ProtectPostPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Admin;
+
+
+use ReIndex\Security\Role\Permission\Moderator\ProtectPostPermission as Superclass;
+
+
+class ProtectPostPermission extends Superclass {
+
+
+  /**
+   * @brief Returns `true` if the protection can be removed from the post, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+    if (!$this->post->isProtected()) return FALSE;
+
+    if ($this->post->isCurrent() or $this->post->isDraft())
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+
+}
