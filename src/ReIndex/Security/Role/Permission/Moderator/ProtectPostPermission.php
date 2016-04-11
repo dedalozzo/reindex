2712d8788fc0c51d761936f2e97607f290466aa7
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-12 01:40:32 +0200

    added ProtectPostPermission

---
+++ src/ReIndex/Security/Role/Permission/Moderator/ProtectPostPermission.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file ProtectPostPermission.php
+ * @brief This file contains the ProtectPostPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Moderator;
+
+
+use ReIndex\Security\Role\Permission\AbstractPostPermission;
+
+
+class ProtectPostPermission extends AbstractPostPermission {
+
+
+  /**
+   * @brief Returns `true` if the protection can be removed from the post, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+    if (!$this->post->isProtected()) return FALSE;
+
+    if ($this->user->match($this->post->protectorId) &&
+      ($this->post->isCurrent() or $this->post->isDraft()))
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+}
