9c59e17a4767ca974d4d183ff33a1c7dd44efde0
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-12 01:33:36 +0200

    added the new DeveloperRole

---
+++ src/ReIndex/Security/Role/DeveloperRole.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file DeveloperRole.php
+ * @brief This file contains the DeveloperRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class DeveloperRole extends AdminRole {
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
