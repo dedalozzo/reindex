55275ccfbbabfcc4681b5a017a1e7259cc255074
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-12 01:38:49 +0200

    added RestoreVersionPermission

---
+++ src/ReIndex/Security/Role/Permission/Moderator/RestoreVersionPermission.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file RestoreVersionPermission.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Moderator;
+
+
+class RestoreVersionPermission {
+
+  /**
+   * @brief Returns `true` if the post can be moved to trash, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+    if ($this->isMovedToTrash() and
+      ($this->user->isModerator() && ($this->dustmanId == $this->user->id)) or
+      $this->user->isAdmin())
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+
+}
