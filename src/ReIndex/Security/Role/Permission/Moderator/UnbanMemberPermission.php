e093cbcea463f725543799b914c631e527749a7a
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-12 01:41:25 +0200

    added UnbanMemberPermission

---
+++ src/ReIndex/Security/Role/Permission/Moderator/UnbanMemberPermission.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file UnbanMemberPermission.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Moderator;
+
+
+class UnbanMemberPermission {
+
+}
