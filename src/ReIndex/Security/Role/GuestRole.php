e3a88e0130d4006701f1c4a502dc9d8a994a6bb4
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-16 19:20:18 +0200

    added a new role for the anonymous users

---
+++ src/ReIndex/Security/Role/GuestRole.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file GuestRole.php
+ * @brief This file contains the AbstractRole class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role;
+
+
+class GuestRole extends AbstractRole {
+
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+}
