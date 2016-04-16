296f2fb6e2b2c7d6ab38fa7df4490f9d6b35b9d4
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-16 19:19:29 +0200

    a ViewPostPermission for the member

---
+++ src/ReIndex/Security/Role/Permission/Member/ViewPostPermission.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file ViewPostPermission.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Member;
+
+
+use ReIndex\Security\Role\Permission\Guest\ViewPostPermission as Superclass;
+
+
+class ViewPostPermission extends Superclass {
+
+  public function check() {
+    return $this->user->match($this->creatorId) ? TRUE : FALSE;
+  }
+
+}
