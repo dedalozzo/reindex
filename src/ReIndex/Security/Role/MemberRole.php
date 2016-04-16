eee3b12faa0b24e9511e66c98c16863dfe31279d
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-16 19:20:02 +0200

    MemberRole inherits from GuestRole

--- src/ReIndex/Security/Role/MemberRole.php
+++ src/ReIndex/Security/Role/MemberRole.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file MemberRole.php
- * @brief This file contains the ${CLASS_NAME} class.
+ * @brief This file contains the MemberRole class.
  * @details
  * @author Filippo F. Fadda
  */
@@ -10,7 +10,7 @@
 namespace ReIndex\Security\Role;
 
 
-class MemberRole extends AbstractRole {
+class MemberRole extends GuestRole {
 
   public function getDescription() {
     //! @todo: Implement getDescription() method.
