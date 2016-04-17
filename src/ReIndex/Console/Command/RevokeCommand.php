fc2eaf985399596979274067dd77784085958a23
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-18 00:31:00 +0200

    implemented the protected method RevokeCommand.perform()

--- src/ReIndex/Console/Command/RevokeCommand.php
+++ src/ReIndex/Console/Command/RevokeCommand.php
@@ -11,11 +11,27 @@
 namespace ReIndex\Console\Command;
 
 
+use Symfony\Component\Console\Output\OutputInterface;
+
+use ReIndex\Model\Member;
+use ReIndex\Security\Guardian;
+
+
 /**
  * @brief Revokes a privilege to a user.
  * @nosubgrouping
  */
-class RevokeCommand extends AbstractPrivilegeCommand {
+class RevokeCommand extends AbstractRoleCommand {
+
+
+  protected function perform($roleName, Member $member, Guardian $guardian, OutputInterface $output) {
+    if ($member->roles->exists($roleName)) {
+      $member->roles->revoke($roleName);
+      $member->save();
+    }
+    else
+      $output->writeln("The role `$roleName` doesn't exist for the member `$member->username`.");
+  }
 
 
   /**
