195ab6f4a54e48ba57a7025884fd6191ce6682e1
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-17 21:48:59 +0200

    implemented getDescription and improved the documentation

--- src/ReIndex/Security/Role/MemberRole.php
+++ src/ReIndex/Security/Role/MemberRole.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file MemberRole.php
  * @brief This file contains the MemberRole class.
@@ -10,10 +11,17 @@
 namespace ReIndex\Security\Role;
 
 
+/**
+ * @brief A classic community's member.
+ * @attention This role is granted to every community's member. You cannot revoke this role unless a superior role has
+ * been granted to the member.
+ * @nosubgrouping
+ */
 class MemberRole extends GuestRole {
 
+
   public function getDescription() {
-    //! @todo: Implement getDescription() method.
+    return "This role is granted to every community's member.";
   }
 
 }
