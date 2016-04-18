b673b9e2f9e91489357b2ef3dbab03be44e1a539
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-18 20:41:05 +0200

    modified the perform() method of the GrantCommand

--- src/ReIndex/Console/Command/GrantCommand.php
+++ src/ReIndex/Console/Command/GrantCommand.php
@@ -11,10 +11,8 @@
 namespace ReIndex\Console\Command;
 
 
-use Symfony\Component\Console\Output\OutputInterface;
-
 use ReIndex\Model\Member;
-use ReIndex\Security\Guardian;
+use ReIndex\Security\Role\IRole;
 
 
 /**
@@ -24,13 +22,8 @@ use ReIndex\Security\Guardian;
 class GrantCommand extends AbstractRoleCommand {
 
 
-  protected function perform($roleName, Member $member, Guardian $guardian, OutputInterface $output) {
-    if ($guardian->roleExists($roleName)) {
-      $member->roles->grant($guardian->getRole($roleName));
-      $member->save();
-    }
-    else
-      $output->writeln("The role `$roleName` doesn't exist.");
+  protected function perform(IRole $role, Member $member) {
+    $member->roles->grant($role);
   }
 
 
