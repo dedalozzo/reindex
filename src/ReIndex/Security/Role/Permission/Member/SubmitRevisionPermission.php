6e037bd8d9c457a08d1fb30a9e1c20769a6b7a26
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-21 16:54:16 +0200

    added SubmitRevisionPermission

---
+++ src/ReIndex/Security/Role/Permission/Member/SubmitRevisionPermission.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file SubmitRevisionPermission.php
+ * @brief This file contains the ${CLASS_NAME} class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Member;
+
+
+use ReIndex\Security\Role\Permission\AbstractPermission;
+use ReIndex\Model\Versionable;
+
+
+class SubmitRevisionPermission extends AbstractPermission {
+
+  public $versionable;
+
+
+  public function __construct(Versionable $versionable) {
+    parent::__construct();
+    $this->versionable = $versionable;
+  }
+
+
+  public function getDescription() {
+    //! @todo: Implement getDescription() method.
+  }
+
+
+  /**
+   * @brief Returns `true` if the document can be moved to trash, `false` otherwise.
+   * @retval bool
+   */
+  public function check() {
+  }
+
+}
