495ccf8fc97d81f62df6bfb69575d75e50906453
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-22 20:01:27 +0200

    implemented UnprotectPostPermission

--- src/ReIndex/Security/Role/ModeratorRole/UnprotectPostPermission.php
+++ src/ReIndex/Security/Role/ModeratorRole/UnprotectPostPermission.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @file UnprotectPostPermission.php
+ * @file ModeratorRole/UnprotectPostPermission.php
  * @brief This file contains the UnprotectPostPermission class.
  * @details
  * @author Filippo F. Fadda
@@ -12,10 +12,14 @@ namespace ReIndex\Security\Role\ModeratorRole;
 
 
 use ReIndex\Security\Role\AbstractPermission;
+use EoC\Couch;
 
 
 /**
  * @brief Permission to unprotect a post.
+ * @details A moderator (or a member with a superior role) can unprotect only the current revision of a post, just in
+ * case it has an active protection. A moderator can unprotect only a post protected by himself, but an admin is able to
+ * unprotect a post protected by a moderator; so a superuser is able to unprotect a post protected by an admin.
  */
 class UnprotectPostPermission extends AbstractPermission {
 
@@ -35,13 +39,14 @@ class UnprotectPostPermission extends AbstractPermission {
 
 
   public function check() {
-    if (!$this->context->isProtected()) return FALSE;
-
-    if ($this->user->match($this->context->protectorId) &&
-      ($this->post->context->isCurrent() or $this->context->state->isDraft()))
-      return TRUE;
-    else
+    if (!$this->context->isProtected())
       return FALSE;
+    elseif ($this->context->protectorId === $this->user->id)
+      return TRUE;
+    else {
+      $protector = $this->di['couchdb']->getDoc(Couch::STD_DOC_PATH, $this->context->bannerId);
+      return !$protector->roles->isSuperior($this->getRole()) ? TRUE : FALSE;
+    }
   }
 
 }
