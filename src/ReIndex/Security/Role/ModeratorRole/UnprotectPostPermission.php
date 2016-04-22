0d8781d06f1384e4c3c556019f188b643605b5d0
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-22 15:14:41 +0200

    added UnprotectPostPermission class

---
+++ src/ReIndex/Security/Role/ModeratorRole/UnprotectPostPermission.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file UnprotectPostPermission.php
+ * @brief This file contains the UnprotectPostPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\ModeratorRole;
+
+
+use ReIndex\Security\Role\AbstractPermission;
+
+
+/**
+ * @brief Permission to unprotect a post.
+ */
+class UnprotectPostPermission extends AbstractPermission {
+
+
+  /**
+   * @brief Constructor.
+   * @param[in] Model::Post $context
+   */
+  public function __construct($context = NULL) {
+    parent::__construct($context);
+  }
+
+
+  public function getDescription() {
+    return "Permission to unprotect a content.";
+  }
+
+
+  public function check() {
+    if (!$this->context->isProtected()) return FALSE;
+
+    if ($this->user->match($this->context->protectorId) &&
+      ($this->post->context->isCurrent() or $this->context->state->isDraft()))
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+}
