b82db1234bf0f7bf7f380b0c58c030546530f53c
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-28 18:48:17 +0200

    a lot of changes and bug fix

--- src/ReIndex/Model/Friendship.php
+++ src/ReIndex/Model/Friendship.php
@@ -25,36 +25,36 @@ class Friendship extends Doc {
 
   /**
    * @brief Creates an instance of Friendship class.
+   * @param[in] Member $sender The sender of the friendship request.
+   * @param[in] Member $receiver The receiver of the friendship request.
    */
-  public static function request(Member $member, $approved = FALSE) {
+  public static function request(Member $sender, Member $receiver) {
     $instance = new self();
 
-    $instance->meta["senderId"] = $instance->receiver->id;
-    $instance->meta["receiverId"] = $member->id;
-    $instance->meta["approved"] = $approved;
-
+    $instance->meta["senderId"] = $sender->id;
+    $instance->meta["receiverId"] = $receiver->id;
+    $instance->meta["approved"] = FALSE;
     $instance->meta["requestedAt"] = time();
-
-    if ($approved)
-      $instance->meta["approvedAt"] = $instance->meta["requestedAt"];
-  }
-
-
-  public function approve() {
-    $this->meta["approvedAt"] = time();
-  }
-
-
-  public function isApproved() {
-    return $this->meta["approved"];
   }
 
 
   /**
-   * @brief Saves the friendship to the database.
+   * @brief Approves the friendship.
+   * @attention Instead of reject a friendship, you have to simply delete it.
    */
-  public function save() {
-    $this->couch->saveDoc($this);
+  public function approve() {
+    $this->meta["approve"] = TRUE;
+    $this->meta["approvedAt"] = time();
+  }
+
+
+  /**
+   * @brief Returns `true` if a friendship has been approved, false otherwise.
+   * @attention Due to the internal `friendships/perMember` implementation, this method is pretty useless.
+   * @retval bool
+   */
+  public function isApproved() {
+    return $this->meta["approved"];
   }
 
 
@@ -65,103 +65,18 @@ class Friendship extends Doc {
   }
 
 
-  public function issetSenderId() {
-    return isset($this->meta['senderId']);
-  }
-
-
-  public function setSenderId($value) {
-    $this->meta['senderId'] = $value;
-  }
-
-
-  public function unsetSenderId() {
-    if ($this->isMetadataPresent('senderId'))
-      unset($this->meta['senderId']);
-  }
-
-
   public function getReceiverId() {
     return $this->meta['receiverId'];
   }
 
-
-  public function issetReceiverId() {
-    return isset($this->meta['receiverId']);
-  }
-
-
-  public function setReceiverId($value) {
-    $this->meta['receiverId'] = $value;
-  }
-
-
-  public function unsetReceiverId() {
-    if ($this->isMetadataPresent('receiverId'))
-      unset($this->meta['receiverId']);
-  }
-
-  
-  public function getApproved() {
-    return $this->meta['approved'];
-  }
-
-
-  public function issetApproved() {
-    return isset($this->meta['approved']);
-  }
-
-
-  public function setApproved($value) {
-    $this->meta['approved'] = $value;
-  }
-
-
-  public function unsetApproved() {
-    if ($this->isMetadataPresent('approved'))
-      unset($this->meta['approved']);
-  }
-  
 
   public function getRequestedAt() {
     return $this->meta['requestedAt'];
   }
 
 
-  public function issetRequestedAt() {
-    return isset($this->meta['requestedAt']);
-  }
-
-
-  public function setRequestedAt($value) {
-    $this->meta['requestedAt'] = $value;
-  }
-
-
-  public function unsetRequestedAt() {
-    if ($this->isMetadataPresent('requestedAt'))
-      unset($this->meta['requestedAt']);
-  }
-
-
   public function getApprovedAt() {
     return $this->meta['approvedAt'];
-  }
-
-
-  public function issetApprovedAt() {
-    return isset($this->meta['approvedAt']);
-  }
-
-
-  public function setApprovedAt($value) {
-    $this->meta['approvedAt'] = $value;
-  }
-
-
-  public function unsetApprovedAt() {
-    if ($this->isMetadataPresent('approvedAt'))
-      unset($this->meta['approvedAt']);
   }
 
   //! @endcond
