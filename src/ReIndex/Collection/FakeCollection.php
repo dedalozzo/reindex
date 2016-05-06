80115ba09d4a87315508c4a53a098268f68cd3dc
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-06 18:27:16 +0200

    added a FakeCollection since the collection doesn't store any data

---
+++ src/ReIndex/Collection/FakeCollection.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file FakeCollection.php
+ * @brief This file contains the FakeCollection class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Collection;
+
+
+use ReIndex\Model\Member;
+
+use Phalcon\Di;
+
+
+abstract class FakeCollection implements \Countable {
+
+  protected $di;    // Stores the default Dependency Injector.
+  protected $user;  // Stores the current user.
+  protected $couch; // Stores the CouchDB instance.
+
+
+  /**
+   * @brief Creates a new collection of items.
+   */
+  public function __construct(Member $user) {
+    $this->user = $user;
+    $this->di = Di::getDefault();
+    $this->couch = $this->di['couchdb'];
+  }
+
+
+  /**
+   * @brief Using the lady loading pattern, this method returns the collection count.
+   * @details Since the data resides on a database, the system prevent from loading them, unless they are strictly
+   * needed.
+   */
+  abstract protected function getCount();
+
+
+  /**
+   * @brief Returns the number of collection's items.
+   * @retval integer
+   */
+  public function count() {
+    return count($this->getCount());
+  }
+
+
+  /**
+   * @brief Returns `true` in case there aren't items inside the collection, `false` otherwise.
+   * @attention This method must be used in place of `empty()`.
+   * @retval bool
+   */
+  public function isEmpty() {
+    return empty($this->getCount()) ? TRUE : FALSE;
+  }
+
+}
