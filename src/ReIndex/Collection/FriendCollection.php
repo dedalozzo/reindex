eb27f684612d92dcf45248e41f449eb3329ff2a5
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-06 18:27:39 +0200

    refactored FriendCollection to inherit from FakeCollection

--- src/ReIndex/Collection/FriendCollection.php
+++ src/ReIndex/Collection/FriendCollection.php
@@ -26,40 +26,21 @@ use Phalcon\Di;
  * @details This class implements `IteratorAggregate`, `Countable`, and `ArrayAccess`.
  * This class uses the Lazy loading pattern.
  */
-class FriendCollection implements \IteratorAggregate, \Countable, \ArrayAccess {
+class FriendCollection extends FakeCollection {
 
-  protected $di;    // Stores the default Dependency Injector.
-  protected $user;  // Stores the current user.
-  protected $couch; // Stores the CouchDB instance.
-
-  protected $friendships = NULL; // Stores the member's friendships.
+  protected $friendsCount = NULL; // Stores the number of friends in the collection.
 
 
-  /**
-   * @brief Creates a new collection of friends.
-   */
-  public function __construct(Member $user) {
-    $this->user = $user;
-    $this->di = Di::getDefault();
-    $this->couch = $this->di['couchdb'];
-  }
-
-
-  /**
-   * @brief Using the lady loading pattern, this method returns the member's friendships.
-   * @details Since the friends data resides on a database, the system prevent from loading them, unless they are
-   * strictly needed.
-   */
-  protected function getFriendships() {
+  protected function getCount() {
     // Test is made using `is_null()` instead of `empty()` because a member may have no friends at all.
-    if (is_null($this->friendships)) {
+    if (is_null($this->friendsCount)) {
       $opts = new ViewQueryOpts();
       $opts->doNotReduce()->setKey([$this->user->id]);
 
-      $this->data = $this->couch->queryView("friendship", "perMember", NULL, $opts)->asArray();
+      $this->friendsCount = $this->couch->queryView("friendship", "perMember", NULL, $opts)->getReducedValu();
     }
 
-    return $this->friendships;
+    return $this->friendsCount;
   }
 
 
@@ -71,7 +52,7 @@ class FriendCollection implements \IteratorAggregate, \Countable, \ArrayAccess {
   public function add(Member $member) {
     // Of course, you can't add yourself to the collection.
     if ($member->match($this->user->id))
-      return;
+      throw new Exception\UserMismatchException("You are nut.");
 
     if ($member->blacklist->exists($this->user))
       throw new Exception\UserMismatchException("Unfortunately you have been blacklisted from the user you are trying to add as a friend.");
@@ -181,82 +162,5 @@ class FriendCollection implements \IteratorAggregate, \Countable, \ArrayAccess {
    */
   public function invite() {
   }
-
-
-  /**
-   * @brief Returns the collection as a real array.
-   * @retval array An associative array using as keys the e-mail addresses, and as values if the address are verified or
-   * not.
-   */
-  public function asArray() {
-    return $this->getFriendships();
-  }
-
-
-  /**
-   * @brief Returns an external iterator.
-   * @retval [ArrayIterator](http://php.net/manual/en/class.arrayiterator.php).
-   */
-  public function getIterator() {
-    return new \ArrayIterator($this->getFriendships());
-  }
-
-
-  /**
-   * @brief Returns the number of member's friendships.
-   * @retval integer
-   */
-  public function count() {
-    return count($this->getFriendships());
-  }
-
-
-  /**
-   * @brief Returns `true` in case there aren't items inside the collection, `false` otherwise.
-   * @details Since the PHP core developers are noobs, `empty()` cannot be used on any class that implements ArrayAccess.
-   * @attention This method must be used in place of `empty()`.
-   * @retval bool
-   */
-  public function isEmpty() {
-    return empty($this->getFriendships()) ? TRUE : FALSE;
-  }
-
-
-  /**
-   * @brief Whether or not an offset exists.
-   * @details This method is executed when using `isset()` or `empty()` on objects implementing ArrayAccess.
-   * @param[in] integer $offset An offset to check for.
-   * @retval bool Returns `true` on success or `false` on failure.
-   */
-  public function offsetExists($offset) {
-    return isset($this->getFriendships()[$offset]);
-  }
-
-
-  /**
-   * @brief Returns the value at specified offset.
-   * @details This method is executed when checking if offset is `empty()`.
-   * @param[in] integer $offset The offset to retrieve.
-   * @retval mixed Can return all value types.
-   */
-  public function offsetGet($offset)  {
-    return $this->getFriendships()[$offset];
-  }
-
-
-  //! @cond HIDDEN_SYMBOLS
-
-  public function offsetSet($offset, $value) {
-    throw new \BadMethodCallException("Collection is immutable and cannot be changed.");
-  }
-
-
-  public function offsetUnset($offset) {
-    throw new \BadMethodCallException("Collection is immutable and cannot be changed.");
-  }
-
-  //! @endcond
-
-  
   
 }
