4e2591e1cb9fe4dce18d0932dbed59b9398d0927
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-16 23:51:56 +0200

    now inherites from GuestRole\GrantRolePermission

--- src/ReIndex/Security/Role/AdminRole/GrantRolePermission.php
+++ src/ReIndex/Security/Role/AdminRole/GrantRolePermission.php
@@ -12,28 +12,13 @@
 namespace ReIndex\Security\Role\AdminRole;
 
 
-use ReIndex\Security\Role\AbstractPermission;
-use ReIndex\Security\Role\IRole;
+use ReIndex\Security\Role\GuestRole\GrantRolePermission as Superclass;
 
 
 /**
  * @brief Permission to grant or revoke a role.
  */
-class GrantRolePermission extends AbstractPermission {
-
-
-  /**
-   * @brief Constructor.
-   * @param[in] IRole $context
-   */
-  public function __construct(IRole $context = NULL) {
-    parent::__construct($context);
-  }
-
-
-  public function getDescription() {
-    return "Permission to grant a role to a member or revoke it.";
-  }
+class GrantRolePermission extends Superclass {
 
 
   public function check() {
