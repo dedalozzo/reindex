b50d6ea9e2847774be86f89f2873e0caf78aada6
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-18 00:30:11 +0200

    removed a commented line from couchdb service

--- services/couchdb.php
+++ services/couchdb.php
@@ -17,7 +17,6 @@ $di->setShared('couchdb',
   function() use ($config) {
     //$couch = new Couch(new Adapter\NativeAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
     $couch = new Couch(new Adapter\CurlAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
-    //$couch = new Couch(new Adapter\CurlAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, 'godzilla'));
 
     $couch->selectDb($config->couchdb->database);
 
