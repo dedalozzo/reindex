26743584afe61995b93007c775cac48635919811
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-20 19:54:25 +0200

    added a new collection to store the tags

---
+++ src/ReIndex/Collection/TagCollection.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * @file TagCollection.php
+ * @brief This file contains the TagCollection class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Collection;
+
+
+use ReIndex\Model\Tag;
+use ReIndex\Helper;
+
+use EoC\Opt\ViewQueryOpts;
+
+
+/**
+ * @brief This class is used to represent a collection of tags.
+ * @nosubgrouping
+ */
+class TagCollection extends MetaCollection {
+
+  const NAME = "tags";
+
+  protected $couch; // Stores the CouchDB instance.
+  protected $redis; // Stores the Redis instance.
+
+
+  /**
+   * @brief Creates a new collection of tags.
+   * @param[in] array $meta Post's array of metadata.
+   */
+  public function __construct(array &$meta) {
+    parent::__construct($meta);
+
+    $this->couch = $this->di['couchdb'];
+    $this->redis = $this->di['redis'];
+  }
+
+
+  /**
+   * @brief Removes all associated tags.
+   */
+  public function reset() {
+    unset($this->meta[static::NAME]);
+    $this->meta[static::NAME] = [];
+  }
+
+
+  /**
+   * @brief Adds the specified tag to the list of tags.
+   * @attention Don't use this method even if it's public, unless you know what are you doing.
+   * @param[in] string $tagId The tag uuid.
+   */
+  public function add($tagId) {
+    $this->meta['tags'][] = Helper\Text::unversion($tagId);
+  }
+
+
+  /**
+   * @brief Adds many tags at once to the list of tags.
+   * @param[in] array $names An array of strings, the tag names.
+   */
+  public function addMultipleAtOnce(array $names) {
+    $names = array_unique($names);
+
+    $opts = new ViewQueryOpts();
+    $opts->includeMissingKeys();
+    $rows = $this->couch->queryView("tags", "byNameSpecial", $names, $opts)->asArray();
+
+    foreach ($rows as $row) {
+
+      if (is_null($row['id'])) {
+        // A tag hasn't been found, so creates it.
+        $tag = Tag::create();
+        $tag->name = $row['key'];
+        $tag->creatorId = $this->meta['creatorId'];
+        $tag->approve();
+        $tag->save();
+
+        $this->add($tag->id);
+      }
+      else
+        $this->add($row['id']);
+
+    }
+  }
+
+
+  /**
+   * @brief Resolve the synonyms and returns only unique master tags.
+   * @retval array
+   */
+  public function uniqueMasters() {
+    $opts = new ViewQueryOpts();
+    $opts->doNotReduce();
+    $masters = $this->couch->queryView("tags", "synonyms", $this->meta['tags'], $opts);
+    return array_unique(array_column($masters->asArray(), 'value'));
+  }
+
+
+  /**
+   * @brief Gets the associated list of tags.
+   */
+  public function names() {
+    if (!$this->isEmpty()) {
+      $ids = $this->uniqueMasters();
+
+      $opts = new ViewQueryOpts();
+      $opts->doNotReduce();
+      return $this->couch->queryView("tags", "allNames", $ids, $opts);
+    }
+    else
+      return [];
+  }
+
+}
