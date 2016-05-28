c22995a36679c0d0a14934320799485c6a7a62e0
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-28 16:02:15 +0200

    added a stub for the ITask interface

---
+++ src/ReIndex/Task/ITask.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file ITask.php
+ * @brief This file contains the ITask interface.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Task;
+
+
+interface ITask {
+
+}
