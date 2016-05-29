1ececf1868ac3e4b4aebc1781b72cff519f02840
Filippo Fadda <filippo.fadda@gmail.com>
2016-05-29 02:07:32 +0200

    fixed the documentation of the create() method

--- src/ReIndex/Model/Follower.php
+++ src/ReIndex/Model/Follower.php
@@ -25,8 +25,8 @@ class Follower extends Doc {
 
   /**
    * @brief Creates an instance of Follower class.
-   * @param[in] string $senderId The sender ID of the member requested the friendship.
-   * @param[in] string $receiverId The receiver ID of the member requested the friendship.
+   * @param[in] string $memberId The sender ID of the member requested the friendship.
+   * @param[in] string $followerId The receiver ID of the member requested the friendship.
    */
   public static function create($memberId, $followerId) {
     $instance = new self();
