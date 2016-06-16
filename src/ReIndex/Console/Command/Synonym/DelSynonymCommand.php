0b5e68f271f063cc8e36da565d15b51f6cc6e6d3
Filippo Fadda <filippo.fadda@gmail.com>
2016-06-17 00:53:22 +0200

    implemented delsynonym command

--- src/ReIndex/Console/Command/Synonym/DelSynonymCommand.php
+++ src/ReIndex/Console/Command/Synonym/DelSynonymCommand.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * @file AddSynonymCommand.php
- * @brief This file contains the AddSynonymCommand class.
+ * @file DelSynonymCommand.php
+ * @brief This file contains the DelSynonymCommand class.
  * @details
  * @author Filippo F. Fadda
  */
 
 
-namespace ReIndex\Console\Command;
+namespace ReIndex\Console\Command\Synonym;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,33 +16,23 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
+use ReIndex\Console\Command\AbstractCommand;
 use ReIndex\Thesaurus;
-
-use Monolog\Logger;
 
 
 /**
  * @brief Displays the synonyms of a tag or add a tag as a synonym of another one.
  * @nosubgrouping
  */
-class AddSynonymCommand extends AbstractCommand {
-
-  /**
-   * @var Logger $log
-   */
-  protected $log;
+class DelSynonymCommand extends AbstractCommand {
 
 
   /**
    * @brief Configures the command.
    */
   protected function configure() {
-    $this->setName("addsynonym");
-    $this->setDescription("Adds a synonym to a tag.");
-
-    $this->addArgument("tag",
-      InputArgument::REQUIRED,
-      "The tag's name.");
+    $this->setName("delsynonym");
+    $this->setDescription("Deletes a synonym.");
 
     $this->addArgument("synonym",
       InputArgument::REQUIRED,
@@ -59,11 +49,10 @@ class AddSynonymCommand extends AbstractCommand {
    * @brief Executes the command.
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $tagName = $input->getArgument('tag');
-    $synonymName = $input->getArgument('synonym');
+    $synonym = $input->getArgument('synonym');
 
     if (!$input->getOption('force')) {
-      $question = new ConfirmationQuestion('Are you sure you want add the synonym? [Y/n]', FALSE);
+      $question = new ConfirmationQuestion('Are you sure you want delete the synonym? [Y/n]', FALSE);
       $helper = $this->getHelper('question');
       $proceed = $helper->ask($input, $output, $question);
     }
@@ -72,7 +61,7 @@ class AddSynonymCommand extends AbstractCommand {
 
     if ($proceed) {
       $thesaurus = new Thesaurus();
-      $thesaurus->addSynonym($tagName, $synonymName);
+      $thesaurus->delSynonym($synonym);
     }
 
     parent::execute($input, $output);
