39acf8255cb8ec983d7aaa88e0579dedbd161621
Filippo Fadda <filippo.fadda@gmail.com>
2016-06-16 20:27:47 +0200

    implemented add synonym command

--- src/ReIndex/Console/Command/AddSynonymCommand.php
+++ src/ReIndex/Console/Command/AddSynonymCommand.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @file SynonymizeCommand.php
- * @brief This file contains the SynonymizeCommand class.
+ * @file AddSynonymCommand.php
+ * @brief This file contains the AddSynonymCommand class.
  * @details
  * @author Filippo F. Fadda
  */
@@ -10,17 +10,13 @@
 
 namespace ReIndex\Console\Command;
 
-use ReIndex\Model\Tag;
-use ReIndex\Model\Synonym;
-
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
-use EoC\Couch;
-use EoC\Opt\ViewQueryOpts;
+use ReIndex\Thesaurus;
 
 use Monolog\Logger;
 
@@ -30,12 +26,7 @@ use Monolog\Logger;
  * @details This class implement the IChunkHook interface.
  * @nosubgrouping
  */
-class SynonymizeCommand extends AbstractCommand {
-
-  /**
-   * @var Couch $couch
-   */
-  protected $couch;
+class AddSynonymCommand extends AbstractCommand {
 
   /**
    * @var Logger $log
@@ -47,7 +38,7 @@ class SynonymizeCommand extends AbstractCommand {
    * @brief Configures the command.
    */
   protected function configure() {
-    $this->setName("refresh");
+    $this->setName("addsynonym");
     $this->setDescription("Displays the synonyms of a tag or add a tag as a synonym of another one or remove it.");
 
     $this->addArgument("tag",
@@ -70,50 +61,19 @@ class SynonymizeCommand extends AbstractCommand {
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
     $tagName = $input->getArgument('tag');
+    $synonymName = $input->getArgument('synonym');
 
-    $opts = new ViewQueryOpts();
-    $opts->setLimit(1);
-    $opts->setKey($tagName);
-    $rows = $this->couch->queryView("tags", "byName", NULL, $opts);
+    if (!$input->getOption('force')) {
+      $question = new ConfirmationQuestion('Are you sure you want add the synonym? [Y/n]', FALSE);
+      $helper = $this->getHelper('question');
+      $proceed = $helper->ask($input, $output, $question);
+    }
+    else
+      $proceed = TRUE;
 
-    if ($rows->isEmpty())
-      throw new \RuntimeException('Tag not found.');
-
-    $master = Tag::find(current($rows->getIterator())['id']);
-
-    // Adds a synonym.
-    if ($name = $input->getOption('add')) {
-
-      if (!$input->getOption('force')) {
-        $question = new ConfirmationQuestion('Are you sure you want add the synonym? [Y/n]', FALSE);
-        $helper = $this->getHelper('question');
-        $proceed = $helper->ask($input, $output, $question);
-      }
-      else
-        $proceed = TRUE;
-
-      if ($proceed) {
-        $opts = new ViewQueryOpts();
-        $opts->setLimit(1);
-        $opts->setKey($name);
-        $rows = $this->couch->queryView("tags", "andSynonymsByName", NULL, $opts);
-
-        if ($rows->isEmpty()) {
-          $synonym = Synonym::create();
-          $synonym->name = $name;
-          $synonym->save();
-          $master->synonyms->add($synonym);
-          $master->save();
-        } else {
-          $tag = Tag::find(current($rows->getIterator())['id']);
-
-          if ($tag instanceof Tag)
-            $tag->markAsSynonymOf($tag);
-          else
-            throw new \RuntimeException('The synonym already exists');
-        }
-      }
-
+    if ($proceed) {
+      $thesaurus = new Thesaurus();
+      $thesaurus->addSynonym($tagName, $synonymName);
     }
 
     parent::execute($input, $output);
