de28ded0e8dbe25662f21b92eb94d190b5e22614
Filippo Fadda <filippo.fadda@gmail.com>
2016-06-16 21:04:45 +0200

    many changes to make it work

--- src/ReIndex/Collection/SynonymCollection.php
+++ src/ReIndex/Collection/SynonymCollection.php
@@ -11,8 +11,7 @@
 namespace ReIndex\Collection;
 
 
-use ReIndex\Task\ITask;
-use ReIndex\Queue\TaskQueue;
+use ReIndex\Model\Synonym;
 use ReIndex\Helper;
 
 
@@ -24,87 +23,44 @@ class SynonymCollection extends MetaCollection {
 
   const NAME = "synonyms";
 
-  /**
-   * @var TaskQueue $queue
-   */
-  protected $queue;
-
 
   /**
-   * @brief Creates a new collection of synonyms.
-   * @param[in] array $meta Tag's array of metadata.
+   * @brief Adds many synonyms at once to the collection.
+   * @param[in] array $synonyms An array of IDs.
    */
-  public function __construct(array &$meta) {
-    parent::__construct($meta);
-
-    $this->queue = $this->di['taskqueue'];
-  }
-
-
-  private function addMultipleAtOnce(array $synonyms) {
-    $this->meta['synonyms'] = Helper\ArrayHelper::merge($this->meta['synonyms'], $synonyms);
+  protected function addMultipleAtOnce(array $synonyms) {
+    $this->meta[static::NAME] = Helper\ArrayHelper::merge($this->meta[static::NAME], $synonyms);
   }
 
 
   /**
-   * @brief Returns the ids of its synonyms.
-   * @retval array An array of strings.
+   * @brief Adds a synonym to the collection.
+   * @param[in] Synonym $synonym A synonym object.
    */
-  public function getSynonyms() {
-    return (!$this->isSynonym()) ? $this->meta['synonyms'] : [];
+  public function add(Synonym $synonym) {
+    array_push($this->meta[static::NAME], $synonym->id);
+    $this->addMultipleAtOnce($synonym->getRelatedIds());
   }
 
 
   /**
-   * @brief Removes all the synonyms from the collection.
+   * @brief Removes the synonym from the collection.
+   * @param[in] Synonym $synonym A synonym object.
    */
-  public function reset() {
-    $this->meta['synonyms'] = [];
+  public function remove(Synonym $synonym) {
+    $key = array_search($synonym->name, $this->meta[static::NAME]);
+    if ($key !== FALSE)
+      unset($this->meta[static::NAME][$key]);
   }
 
 
   /**
-   * @brief Marks the provided tag as synonym of the current tag.
-   * @param[in] Tag $tag The tag you want add as synonym to the current tag.
-   */
-  public function add(Tag $tag) {
-    // You can't add a synonym to a synonym, neither you can add a master to a synonym.
-    if ($this->isSynonym() or !$this->state->isCurrent() or $tag->isSynonym() or !$tag->state->isCurrent())
-      throw new \RuntimeException("You can't add a synonym to a synonym, neither you can add a master to a synonym.");
-
-    array_push($this->meta['synonyms'], $tag->unversionId);
-    $this->addMultipleAtOnce($tag->getSynonyms());
-
-    $tag->transIntoSynonym();
-    $tag->save();
-
-    // In order to execute a command have have it not hang your php script while it runs, the program you run must not
-    // output back to php. To do this, redirect both stdout and stderr to /dev/null, then background it.
-    // @see http://stackoverflow.com/a/3819422/1889828
-    $cmd = 'nohup rei synonym --add='. $tag->id . ' -f > /dev/null 2>&1 &';
-
-    exec($cmd);
-  }
-
-
-  /**
-   * @brief Removes the specified task from the collection.
-   * @param[in] ITask $task The task object.
-   */
-  public function remove(Tag $tag) {
-    if ($this->exists($task))
-      unset($this->meta[static::NAME][get_class($task)]);
-  }
-
-
-  /**
-   * @brief Returns `true` if the task is already present, `false` otherwise.
-   * @param[in] ITask $task A task object.
+   * @brief Returns `true` if the synonym is already present, `false` otherwise.
+   * @param[in] Synonym $synonym A synonym object.
    * @retval bool
    */
-  public function exists(Tag $tag) {
-    return isset($this->meta[static::NAME][get_class($task)]);
+  public function exists(Synonym $synonym) {
+    return in_array($synonym->name, $this->meta[static::NAME]);
   }
-
 
 }
