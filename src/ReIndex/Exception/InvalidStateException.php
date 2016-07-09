82070e6e3238658fee1563b84b54cc63633e4baf
Filippo Fadda <filippo.fadda@gmail.com>
2016-07-09 18:01:37 +0200

    added a new exception raised when the state is invalid

---
+++ src/ReIndex/Exception/InvalidStateException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file InvalidStateException.php
+ * @brief This file contains the InvalidStateException class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Exception;
+
+
+/**
+ * @brief Exception thrown when an incomplete field or an invalid value for the field is provided.
+ */
+class InvalidStateException extends \RuntimeException {}
