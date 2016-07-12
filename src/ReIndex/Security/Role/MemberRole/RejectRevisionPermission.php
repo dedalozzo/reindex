9e8597906a929213733146d881c6261601b8d67b
Filippo Fadda <filippo.fadda@gmail.com>
2016-07-13 01:34:07 +0200

    even the member can reject a revision

---
+++ src/ReIndex/Security/Role/MemberRole/RejectRevisionPermission.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file MemberRole/RejectRevisionPermission.php
+ * @brief This file contains the RejectRevisionPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\MemberRole;
+
+
+use ReIndex\Security\Role\AbstractPermission;
+use ReIndex\Doc\Versionable;
+use ReIndex\Enum\State;
+
+
+/**
+ * @brief Permission to vote for the rejection of a document's revision.
+ */
+class RejectRevisionPermission extends AbstractPermission {
+
+
+  /**
+   * @brief Constructor.
+   * @param[in] Doc::Versionable $context
+   */
+  public function __construct(Versionable $context) {
+    parent::__construct($context);
+  }
+
+
+  public function getDescription() {
+    return "Permission to vote for the rejection of a document's revision.";
+  }
+
+
+  /**
+   * @brief Returns the value for the vote if the document revision can be rejected, `false` otherwise.
+   * @retval mixed
+   */
+  public function check() {
+    if ($this->user->match($this->context->creatorId) && $this->context->state->is(State::SUBMITTED))
+      return -$this->di['config']->review->creatorVoteValue;
+    else
+      return FALSE;
+  }
+
+}
