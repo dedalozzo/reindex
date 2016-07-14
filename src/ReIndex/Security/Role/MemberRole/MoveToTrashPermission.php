d7655ce8ebc3f146a1cb720ad5125346c2f7187a
Filippo Fadda <filippo.fadda@gmail.com>
2016-07-14 22:17:25 +0200

    implemented MoveToTrashPermission

---
+++ src/ReIndex/Security/Role/MemberRole/MoveToTrashPermission.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file MemberRole/MoveToTrashPermission.php
+ * @brief This file contains the MoveToTrashPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\MemberRole;
+
+
+use ReIndex\Doc\Comment;
+use ReIndex\Security\Role\AbstractPermission;
+use ReIndex\Doc\Versionable;
+use ReIndex\Enum\State;
+use ReIndex\Doc\Update;
+
+
+/**
+ * @brief Permission to delete the content.
+ */
+class MoveToTrashPermission extends AbstractPermission {
+
+
+  /**
+   * @brief Constructor.
+   * param[in] Doc::Versionable $context
+   */
+  public function __construct(Versionable $context = NULL) {
+    parent::__construct($context);
+  }
+
+
+  public function getDescription() {
+    return "Permission to delete the content.";
+  }
+
+
+  public function check() {
+    if (!$this->user->match($this->context->creatorId))
+      return FALSE;
+
+    if ($this->context->state->is(State::DRAFT))
+      return TRUE;
+
+    // Special case for updates and comments.
+    if ($this->context->state->is(State::CURRENT) && ($this->context instanceof Update or $this->context instanceof Comment))
+      return TRUE;
+
+    return FALSE;
+  }
+
+}
