5460e28c5d57c0a8690770c366c3f6284907773f
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-09 18:40:32 +0200

    selectDb() has been removed, we don't call it anymore

--- services/couchdb.php
+++ services/couchdb.php
@@ -15,10 +15,8 @@ use EoC\Adapter;
 // Creates an instance of EoC client and return it.
 $di->setShared('couchdb',
   function() use ($config) {
-    //$couch = new Couch(new Adapter\NativeAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
-    $couch = new Couch(new Adapter\CurlAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
-
-    $couch->selectDb($config->couchdb->database);
+    $couch = new Couch(new Adapter\NativeAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
+    //$couch = new Couch(new Adapter\CurlAdapter($config->couchdb->host.":".$config->couchdb->port, $config->couchdb->user, $config->couchdb->password));
 
     return $couch;
   }
