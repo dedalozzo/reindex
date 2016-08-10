6955ec1bc6a3c8db610abba4f537935a40897d59
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-10 16:38:48 +0200

    route names refer now to a specific document type

--- src/ReIndex/Route/ApiGroup.php
+++ src/ReIndex/Route/ApiGroup.php
@@ -33,22 +33,21 @@ class ApiGroup extends Group {
 
     $this->setPrefix('/api');
 
-    $this->addPost('/like/', ['action' => 'like']);
-    $this->addPost('/star/', ['action' => 'starTag']);
-    $this->addPost('/submit/', ['action' => 'submit']);
-    $this->addPost('/approve/', ['action' => 'approve']);
-    $this->addPost('/reject/', ['action' => 'reject']);
-    $this->addPost('/revert/', ['action' => 'revert']);
-    $this->addPost('/move-to-trash/', ['action' => 'moveToTrash']);
-    $this->addPost('/restore/', ['action' => 'restore']);
-    $this->addPost('/mark-as-draft/', ['action' => 'markAsDraft']);
-    $this->addPost('/close/', ['action' => 'close']);
-    $this->addPost('/lock/', ['action' => 'lock']);
-    $this->addPost('/unprotect/', ['action' => 'unprotect']);
-    $this->addPost('/hide/', ['action' => 'hide']);
-    $this->addPost('/show/', ['action' => 'show']);
-    $this->addPost('/addfriend/', ['action' => 'addFriend']);
-    $this->addPost('/removefriend/', ['action' => 'removeFriend']);
+    $this->addPost('/friend/add/', ['action' => 'addFriend']);
+    $this->addPost('/friend/remove/', ['action' => 'removeFriend']);
+
+    $this->addPost('/post/like/', ['action' => 'likePost']);
+    $this->addPost('/post/close/', ['action' => 'closePost']);
+    $this->addPost('/post/lock/', ['action' => 'lockPost']);
+    $this->addPost('/post/unprotect/', ['action' => 'unprotectPost']);
+    $this->addPost('/post/hide/', ['action' => 'hidePost']);
+    $this->addPost('/post/show/', ['action' => 'showPost']);
+    $this->addPost('/post/move-to-trash/', ['action' => 'movePostToTrash']);
+    $this->addPost('/post/restore/', ['action' => 'restorePost']);
+
+    $this->addPost('/tag/star/', ['action' => 'starTag']);
+
+    $this->addPost('/reply/like/', ['action' => 'likeReply']);
   }
 
 }
