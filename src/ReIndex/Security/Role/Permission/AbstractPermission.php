b581b65655e829d9557584fcfdb5cf9b4a06bcbb
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-30 19:10:25 +0200

    added __call and __set to dynamic add methods using closures

--- src/ReIndex/Security/Role/Permission/AbstractPermission.php
+++ src/ReIndex/Security/Role/Permission/AbstractPermission.php
@@ -56,6 +56,48 @@ abstract class AbstractPermission implements IPermission {
   }
 
 
+  /**
+   * @brief Calls is triggered when invoking inaccessible methods in an object context.
+   * @param[in] string $name The name of the method being called.
+   * @param[in] array $arguments An enumerated array containing the parameters passed to the method.
+   */
+  public function __call($name, array $arguments) {
+    if (is_callable($this->$name))
+      return call_user_func($this->$name, $arguments);
+    else
+      throw new \RuntimeException("Method {$name} does not exist.");
+  }
+
+
+  /**
+   * @brief Sometime a programmer needs to define a new special role, and eventually a set of permissions to check the
+   * access, for this particular role, to any existent resource. Through this tecnique the programmer may define dynamic
+   * methods to check the permissions for a particular role.
+   * @param[in] string $name The name of the method being called.
+   * @param[in] callable $value A closure.
+   * @details In the following example we define the method `checkForGodRole`, to extend the `ImpersonatePermission`
+   * class such as God will be able to impersonate anyone.
+     @code
+       // We assume `$member` is an instance of the `Member` class.
+       $permission = new ImpersonatePermission($member);
+
+       $permission->checkForGodRole = function() {
+         return TRUE;
+       };
+
+       // Prints `true`.
+       print $permission->checkForGodRole();
+     @endcode
+   */
+  public function __set($name, $value) {
+    $this->$name = is_callable($value) ? $value->bindTo($this, $this) : $value;
+  }
+
+
+  /**
+   * @brief Overrides the magic method __toString() to return the permission's name.
+   * @return string
+   */
   public function __toString() {
     return $this->getName();
   }
