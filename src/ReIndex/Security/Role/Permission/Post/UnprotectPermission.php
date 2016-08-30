09c8655454de97937b94b637d7168e4c62df39c7
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-30 18:17:07 +0200

    refactored

--- src/ReIndex/Security/Role/Permission/Post/UnprotectPermission.php
+++ src/ReIndex/Security/Role/Permission/Post/UnprotectPermission.php
@@ -1,17 +1,18 @@
 <?php
 
 /**
- * @file ModeratorRole/UnprotectPostPermission.php
- * @brief This file contains the UnprotectPostPermission class.
+ * @file Post/UnprotectPermission.php
+ * @brief This file contains the UnprotectPermission class.
  * @details
  * @author Filippo F. Fadda
  */
 
 
-namespace ReIndex\Security\Role\ModeratorRole;
+namespace ReIndex\Security\Role\Permission\Post;
 
 
-use ReIndex\Security\Role\AbstractPermission;
+use ReIndex\Doc\Post;
+
 use EoC\Couch;
 
 
@@ -21,15 +22,21 @@ use EoC\Couch;
  * case it has an active protection. A moderator can unprotect only a post protected by himself, but an admin is able to
  * unprotect a post protected by a moderator; so a superuser is able to unprotect a post protected by an admin.
  */
-class UnprotectPostPermission extends AbstractPermission {
+class UnprotectPermission extends ProtectPermission  {
+
+  /**
+   * @var Couch $couch
+   */
+  protected $couch;
 
 
   /**
    * @brief Constructor.
    * @param[in] Doc::Post $context
    */
-  public function __construct($context = NULL) {
-    parent::__construct($context);
+  public function __construct(Post $post) {
+    parent::__construct($post);
+    $this->couch = $this->di['couchdb'];
   }
 
 
@@ -38,13 +45,17 @@ class UnprotectPostPermission extends AbstractPermission {
   }
 
 
-  public function check() {
-    if (!$this->context->isProtected())
+  /**
+   * @brief Returns `true` if the post can be unprotected, `false` otherwise.
+   * @retval bool
+   */
+  public function checkForModeratorRole() {
+    if (!$this->post->isProtected())
       return FALSE;
-    elseif ($this->context->protectorId === $this->user->id)
+    elseif ($this->post->protectorId === $this->user->id)
       return TRUE;
     else {
-      $protector = $this->di['couchdb']->getDoc('members', Couch::STD_DOC_PATH, $this->context->bannerId);
+      $protector = $this->couch->getDoc('members', Couch::STD_DOC_PATH, $this->post->bannerId);
       return !$protector->roles->areSuperiorThan($this->getRole()) ? TRUE : FALSE;
     }
   }
