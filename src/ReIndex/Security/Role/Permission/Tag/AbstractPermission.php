177d6341ad6ae02a6f878f3874ff727d40e8ce51
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-31 17:18:48 +0200

    added an Abstract class for tag permissions

---
+++ src/ReIndex/Security/Role/Permission/Tag/AbstractPermission.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file Tag/AbstractPermission.php
+ * @brief This file contains the AbstractPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Tag;
+
+
+use ReIndex\Security\Role\Permission\AbstractPermission as Superclass;
+use ReIndex\Doc\Tag;
+
+
+//! Permissions related to the tags
+abstract class AbstractPermission extends Superclass {
+
+  protected $tag;
+
+
+  /**
+   * @brief Constructor.
+   * @param[in] Tag $context.
+   */
+  public function __construct(Tag $tag) {
+    parent::__construct();
+  }
+
+}
