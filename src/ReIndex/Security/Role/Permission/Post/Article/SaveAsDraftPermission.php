45ffe778459ada600a31beb8591042583e0b8a19
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-31 17:22:16 +0200

    added a new permission to save as draft

---
+++ src/ReIndex/Security/Role/Permission/Post/Article/SaveAsDraftPermission.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file Article/SaveAsDraftPermission.php
+ * @brief This file contains the SaveAsDraftPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Post\Article;
+
+use ReIndex\Enum\State;
+
+
+/**
+ * @brief Permission to save an article as draft.
+ */
+class SaveAsDraftPermission extends AbstractPermission  {
+
+
+  public function getDescription() {
+    return "Permission to save an article as draft.";
+  }
+
+
+  public function checkForMemberRole() {
+    return $this->user->match($this->article->creatorId) &&
+           ($this->article->state->is(State::CREATED) or $this->article->state->is(State::DRAFT))
+           ? TRUE : FALSE;
+  }
+
+}
