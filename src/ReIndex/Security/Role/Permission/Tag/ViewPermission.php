1d848ee5ad02b1c5c50fe0bb20da2a18c8da7e26
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-31 17:23:06 +0200

    view permission on article added

---
+++ src/ReIndex/Security/Role/Permission/Tag/ViewPermission.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file Tag/ViewPermission.php
+ * @brief This file contains the ViewPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Tag;
+
+
+use ReIndex\Enum\State;
+
+
+/**
+ * @brief Permission to display information about a tag.
+ */
+class ViewPermission extends AbstractPermission  {
+
+
+  public function getDescription() {
+    return "Permission to display information about tag.";
+  }
+
+
+  /**
+   * @brief Returns `true` if tag is current, `false` otherwise.
+   * @retval bool
+   */
+  public function checkForGuest() {
+    return $this->tag->state->is(State::CURRENT) ? TRUE : FALSE;
+  }
+
+}
