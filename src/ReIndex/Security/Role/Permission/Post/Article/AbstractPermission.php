82ae0973396f08fadfa0ea8db969869c4802d493
Filippo Fadda <filippo.fadda@gmail.com>
2016-08-31 17:19:11 +0200

    added an abstract class for articles' permissions

---
+++ src/ReIndex/Security/Role/Permission/Post/Article/AbstractPermission.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file Article/AbstractPermission.php
+ * @brief This file contains the AbstractPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Role\Permission\Post\Article;
+
+
+use ReIndex\Security\Role\Permission\AbstractPermission as Superclass;
+use ReIndex\Doc\Article;
+
+
+abstract class AbstractPermission extends Superclass {
+
+  protected $article;
+
+
+  /**
+   * @brief Constructor.
+   * @param[in] Doc::Article $article
+   */
+  public function __construct(Article $article) {
+    $this->article = $article;
+    parent::__construct();
+  }
+
+}
