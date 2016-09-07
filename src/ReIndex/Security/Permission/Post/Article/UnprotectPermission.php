ba065bf25e426b6d3070a0faf1c9dc775653a1c4
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-07 18:28:18 +0200

    fixed bugs

--- src/ReIndex/Security/Permission/Post/Article/UnprotectPermission.php
+++ src/ReIndex/Security/Permission/Post/Article/UnprotectPermission.php
@@ -11,53 +11,10 @@
 namespace ReIndex\Security\Permission\Post\Article;
 
 
-use ReIndex\Doc\Article;
-
-use EoC\Couch;
+use ReIndex\Security\Permission\Post\UnprotectPermission as Superclass;
 
 
 /**
  * @brief Permission to unprotect an article.
- * @details A moderator (or a member with a superior role) can unprotect only the current revision of a post, just in
- * case it has an active protection. A moderator can unprotect only a post protected by himself, but an admin is able to
- * unprotect a post protected by a moderator; so a superuser is able to unprotect a post protected by an admin.
  */
-class UnprotectPermission extends ProtectPermission  {
-
-  /**
-   * @var Couch $couch
-   */
-  protected $couch;
-
-
-  /**
-   * @brief Constructor.
-   * @param[in] Doc::Article $article
-   */
-  public function __construct(Article $article) {
-    parent::__construct($article);
-    $this->couch = $this->di['couchdb'];
-  }
-
-
-  public function getDescription() {
-    return "Permission to unprotect a content.";
-  }
-
-
-  /**
-   * @brief Returns `true` if the post can be unprotected, `false` otherwise.
-   * @retval bool
-   */
-  public function checkForModeratorRole() {
-    if (!$this->article->isProtected())
-      return FALSE;
-    elseif ($this->article->protectorId === $this->user->id)
-      return TRUE;
-    else {
-      $protector = $this->couch->getDoc('members', Couch::STD_DOC_PATH, $this->article->bannerId);
-      return !$protector->roles->areSuperiorThan($this->getRole()) ? TRUE : FALSE;
-    }
-  }
-
-}
+class UnprotectPermission extends Superclass {}
