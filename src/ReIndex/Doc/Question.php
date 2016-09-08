a70f8704dccb80117ee30b5e333d44f22ea91fcf
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-08 20:29:33 +0200

    updated namespace

--- src/ReIndex/Doc/Question.php
+++ src/ReIndex/Doc/Question.php
@@ -11,7 +11,7 @@
 namespace ReIndex\Doc;
 
 
-use ReIndex\Security\Permission\Post\Question as Permission;
+use ReIndex\Security\Permission\Versionable\Post\Question as Permission;
 use ReIndex\Enum\State;
 use Reindex\Exception;
 use ReIndex\Controller\BaseController;
