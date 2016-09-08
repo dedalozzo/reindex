eb2a2a0105251b320f7b9f6906460c42bb510f18
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-08 21:38:44 +0200

    no longer abstract

--- src/ReIndex/Security/Permission/Versionable/Post/UnprotectPermission.php
+++ src/ReIndex/Security/Permission/Versionable/Post/UnprotectPermission.php
@@ -17,12 +17,9 @@ use EoC\Couch;
 
 
 /**
- * @brief Permission to unprotect an post.
- * @details A moderator (or a member with a superior role) can unprotect only the current revision of a post, just in
- * case it has an active protection. A moderator can unprotect only a post protected by himself, but an admin is able to
- * unprotect a post protected by a moderator; so a superuser is able to unprotect a post protected by an admin.
+ * @brief Permission to unprotect a post.
  */
-class UnprotectPermission extends ProtectPermission  {
+abstract class UnprotectPermission extends ProtectPermission {
 
   /**
    * @var Couch $couch
@@ -40,13 +37,11 @@ class UnprotectPermission extends ProtectPermission  {
   }
 
 
-  public function getDescription() {
-    return "Permission to unprotect a content.";
-  }
-
-
   /**
-   * @brief Returns `true` if the post can be unprotected, `false` otherwise.
+   * @brief A moderator (or a member with a superior role) can unprotect only the current revision of a post, just in
+   * case it has an active protection.
+   * @details A moderator can unprotect only a post protected by himself, but an admin is able to unprotect a post
+   * protected by a moderator; so a superuser is able to unprotect a post protected by an admin.
    * @retval bool
    */
   public function checkForModeratorRole() {
