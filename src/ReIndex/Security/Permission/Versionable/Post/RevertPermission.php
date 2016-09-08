660a7f54b16f92052f139d15c2433e059aefe42b
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-08 21:33:28 +0200

    removed getDescription()

--- src/ReIndex/Security/Permission/Versionable/Post/RevertPermission.php
+++ src/ReIndex/Security/Permission/Versionable/Post/RevertPermission.php
@@ -1,25 +1,20 @@
 <?php
 
 /**
- * @file Article/RevertPermission.php
+ * @file Post/RevertPermission.php
  * @brief This file contains the RevertPermission class.
  * @details
  * @author Filippo F. Fadda
  */
 
 
-namespace ReIndex\Security\Permission\Versionable\Post\Article;
+namespace ReIndex\Security\Permission\Versionable\Post;
 
 
 /**
  * @brief Permission to revert the content to a specific revision.
  */
 class RevertPermission extends AbstractPermission {
-
-
-  public function getDescription() {
-    return "Permission to revert the article content to a specific revision.";
-  }
 
 
   public function checkForModeratorRole() {
