2cfee94d7902f46b0b1e0c931a8852cc7adf5722
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-08 19:10:54 +0200

    many changes all over the code

--- src/ReIndex/Doc/Update.php
+++ src/ReIndex/Doc/Update.php
@@ -11,6 +11,15 @@
 namespace ReIndex\Doc;
 
 
+use ReIndex\Controller\BaseController;
+use ReIndex\Security\Permission\Post\Update as Permission;
+use ReIndex\Enum\State;
+use Reindex\Exception;
+
+
+use Phalcon\Mvc\Controller;
+
+
 /*
  * @brief This class represents an user's update.
  * @nosubgrouping
@@ -18,8 +27,130 @@ namespace ReIndex\Doc;
 class Update extends Post {
 
 
-  protected function needForApproval() {
-    return FALSE;
+  /**
+   * @copydoc Post::close()
+   */
+  public function close() {
+    if (!$this->user->has(new Permission\ProtectPermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::close();
+  }
+
+
+  /**
+   * @copydoc Post::lock()
+   */
+  public function lock() {
+    if (!$this->user->has(new Permission\ProtectPermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::lock();
+  }
+
+
+  /**
+   * @copydoc Post::unprotect()
+   */
+  public function unprotect() {
+    if (!$this->user->has(new Permission\UnprotectPermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::unprotect();
+  }
+
+
+  /**
+   * @copydoc Versionable::submit()
+   */
+  public function submit() {
+    if (!$this->user->has(new Permission\EditPermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::submit();
+  }
+
+
+  /**
+   * @copydoc Versionable::approve()
+   */
+  public function approve() {
+    $permission = new Permission\ApprovePermission($this);
+
+    if (!$this->user->has($permission))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    $this->castVoteByRole($permission->getRole());
+
+    parent::approve();
+  }
+
+
+  /**
+   * @copydoc Versionable::reject()
+   */
+  public function reject($reason) {
+    $permission = new Permission\RejectPermission($this);
+
+    if (!$this->user->has($permission))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    $this->castVoteByRole($permission->getRole());
+
+    parent::reject($reason);
+  }
+
+
+  /**
+   * @copydoc Versionable::revert()
+   */
+  public function revert($versionNumber = NULL) {
+    if (!$this->user->has(new Permission\RevertPermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::revert($versionNumber);
+  }
+
+
+  /**
+   * @copydoc Versionable::delete()
+   */
+  public function moveToTrash() {
+    if (!$this->user->has(new Permission\MoveToTrashPermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::moveToTrash();
+  }
+
+
+  /**
+   * @copydoc Versionable::restore()
+   */
+  public function restore() {
+    if (!$this->user->has(new Permission\RestorePermission($this)))
+      throw new Exception\AccessDeniedException("Privilegi insufficienti o stato incompatibile.");
+
+    parent::restore();
+  }
+
+
+  /**
+   * @copydoc Post::newAction()
+   */
+  public function editAction(BaseController $controller) {
+    if (!$this->user->has(new Permission\ViewPermission($this)))
+      return $controller->dispatcher->forward(['controller' => 'error', 'action' => 'show401']);
+
+    $controller->view->setVar('canEdit', $this->user->has(new Permission\EditPermission($this)));
+  }
+
+
+  /**
+   * @copydoc Post::viewAction()
+   */
+  public function viewAction(BaseController $controller) {
+    if (!$this->user->has(new Permission\ViewPermission($this)))
+      return $controller->dispatcher->forward(['controller' => 'error', 'action' => 'show401']);
   }
 
 
