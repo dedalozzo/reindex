7d768de85bf02eaa49845bd4215c0fa2c0692c90
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-08 19:08:08 +0200

    edit permission needed a buff

--- src/ReIndex/Security/Permission/Post/Article/EditPermission.php
+++ src/ReIndex/Security/Permission/Post/Article/EditPermission.php
@@ -11,54 +11,17 @@
 namespace ReIndex\Security\Permission\Post\Article;
 
 
-use ReIndex\Enum\State;
+use ReIndex\Security\Permission\Post\EditPermission as Superclass;
 
 
 /**
  * @brief Permission to edit an article.
  */
-class EditPermission extends AbstractPermission {
+class EditPermission extends Superclass {
   
 
   public function getDescription() {
     return "Permission to edit an article.";
-  }
-
-
-  /**
-   * @brief Returns `true` if the user is the creator of the post and the post is unlocked, `false` otherwise.
-   * @retval bool
-   */
-  public function checkForMemberRole() {
-    if (!$this->article->isLocked() &&
-        ($this->article->state->is(State::CURRENT) or 
-         ($this->article->state->is(State::DRAFT) && $this->user->match($this->article->creatorId))))
-      return TRUE;
-    else
-      return FALSE;
-  }
-
-
-  public function checkForEditorRole() {
-    if ($this->checkForMemberRole())
-      return TRUE;
-    else
-     return (!$this->article->isLocked() && $this->article->state->is(State::CURRENT)) ? TRUE : FALSE;
-  }
-
-
-  public function checkForReviewerRole() {
-    if ($this->checkForEditorRole())
-      return TRUE;
-    else
-      return !$this->article->isLocked() &&
-      ($this->article->state->is(State::CURRENT) || $this->article->state->is(State::SUBMITTED))
-        ? TRUE : FALSE;
-  }
-
-
-  public function checkForModeratorRole() {
-    return $this->article->state->is(State::CURRENT) or $this->article->state->is(State::SUBMITTED);
   }
 
 }
---
+++ src/ReIndex/Security/Permission/Post/EditPermission.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file Post/EditPermission.php
+ * @brief This file contains the EditPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Permission\Post;
+
+
+use ReIndex\Enum\State;
+
+
+/**
+ * @brief Permission to edit a post.
+ */
+abstract class EditPermission extends AbstractPermission {
+
+
+  /**
+   * @brief Returns `true` if the user is the creator of the post and the post is unlocked, `false` otherwise.
+   * @retval bool
+   */
+  public function checkForMemberRole() {
+    if (!$this->post->isLocked() &&
+        ($this->post->state->is(State::CURRENT) || ($this->post->state->is(State::DRAFT) && $this->user->match($this->post->creatorId))))
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+
+  public function checkForEditorRole() {
+    if ($this->checkForMemberRole())
+      return TRUE;
+    else
+      return (!$this->post->isLocked() && $this->post->state->is(State::CURRENT)) ? TRUE : FALSE;
+  }
+
+
+  public function checkForReviewerRole() {
+    if ($this->checkForEditorRole())
+      return TRUE;
+    else
+      return !$this->post->isLocked() &&
+      ($this->post->state->is(State::CURRENT) || $this->post->state->is(State::SUBMITTED))
+        ? TRUE : FALSE;
+  }
+
+
+  public function checkForModeratorRole() {
+    return $this->post->state->is(State::CURRENT) or $this->post->state->is(State::SUBMITTED);
+  }
+
+}
