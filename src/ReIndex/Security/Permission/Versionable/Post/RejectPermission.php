c2b008fe01fbd2c8f28eea97f1f7988d5d75e6bc
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-09 18:20:35 +0200

    extends ApprovePermission

--- src/ReIndex/Security/Permission/Versionable/Post/RejectPermission.php
+++ src/ReIndex/Security/Permission/Versionable/Post/RejectPermission.php
@@ -11,51 +11,7 @@
 namespace ReIndex\Security\Permission\Versionable\Post;
 
 
-use ReIndex\Enum\State;
-
-
 /**
  * @brief Permission to vote for the rejection of a post's revision.
  */
-class RejectPermission extends AbstractPermission {
-
-
-  /**
-   * @brief Returns the value for the vote if the document revision can be rejected, `false` otherwise.
-   * @retval mixed
-   */
-  public function checkForMemberRole() {
-    if ($this->post->state->is(State::SUBMITTED) &&
-      $this->user->match($this->post->creatorId) &&
-      !$this->user->match($this->post->editorId))
-      return -$this->di['config']->review->creatorVoteValue;
-    else
-      return FALSE;
-  }
-
-
-  public function checkForReviewerRole() {
-    if ($this->post->state->is(State::SUBMITTED) &&
-      !$this->user->match($this->post->editorId))
-      return -$this->di['config']->review->reviewerVoteValue;
-    else
-      return FALSE;
-  }
-
-
-  public function checkForModeratorRole() {
-    if ($this->post->state->is(State::SUBMITTED))
-      return -$this->di['config']->review->moderatorVoteValue;
-    else
-      return FALSE;
-  }
-
-
-  public function checkForAdminRole() {
-    if ($this->post->state->is(State::SUBMITTED))
-      return $this->di['config']->review->scoreToRejectRevision;
-    else
-      return FALSE;
-  }
-
-}
+class RejectPermission extends ApprovePermission {}
