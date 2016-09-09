abea0b2de52091763e77464e17e940606f9d8bb5
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-09 18:21:04 +0200

    extends ApprovePermission of the Article namespace

---
+++ src/ReIndex/Security/Permission/Versionable/Post/Article/RejectPermission.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file Article/RejectPermission.php
+ * @brief This file contains the RejectPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Permission\Versionable\Post\Article;
+
+
+/**
+ * @brief Permission to vote for the rejection of an article's revision.
+ */
+class RejectPermission extends ApprovePermission {}
