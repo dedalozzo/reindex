8d1e161d0dd479575632bf724ae5bf300b666875
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-10 15:36:47 +0200

    the Reply class is now Answer

--- src/ReIndex/Doc/Answer.php
+++ src/ReIndex/Doc/Answer.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @file Answer.php
- * @brief This file contains the Answer class.
+ * @file Reply.php
+ * @brief This file contains the Reply class.
  * @details
  * @author Filippo F. Fadda
  */
@@ -11,18 +11,69 @@
 namespace ReIndex\Doc;
 
 
+use ReIndex\Property\TBody;
+use ReIndex\Collection;
+
+
 /**
- * @brief A user's answer to a question.
+ * @brief A generic reply. It can be an answer, a comment to a question (or to an answer) or a reply to a post (an
+ * article, a book review, etc.).
  * @nosubgrouping
+ *
+ * @cond HIDDEN_SYMBOLS
+ *
+ * @property Collection\VoteCollection $votes // Casted votes.
+ *
+ * @endcond
  */
-class Answer extends Reply {
+class Reply extends Versionable {
+  use TBody;
+
+
+  public function __construct() {
+    parent::__construct();
+
+    // Since we can't use reflection inside EoC Server, we need a way to recognize every subclass of the `Reply` class.
+    // This is done testing `isset($doc->supertype) && $doc->supertype == 'reply'`.
+    $this->meta['supertype'] = 'reply';
+  }
 
 
   /**
-   * @brief Marks the item as duplicate of another item.
+   * @copydoc ActiveDoc::getDbName()
    */
-  public function markAsDuplicate() {
-
+  protected function getDbName() {
+    return 'replies';
   }
+
+
+  protected function approve() {
+    //! @todo: Implement approve() method.
+  }
+
+
+  //! @cond HIDDEN_SYMBOLS
+
+  public function getPostId() {
+    return $this->meta['postId'];
+  }
+
+
+  public function issetPostId() {
+    return isset($this->meta['postId']);
+  }
+
+
+  public function setPostId($value) {
+    $this->meta['postId'] = $value;
+  }
+
+
+  public function unsetPostId() {
+    if ($this->isMetadataPresent('postId'))
+      unset($this->meta['postId']);
+  }
+
+  //! @endcond
 
 }
