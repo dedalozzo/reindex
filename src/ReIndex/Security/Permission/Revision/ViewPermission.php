a899f6395a92e7249f5513643ae5754c9850fad1
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-10 16:44:57 +0200

    replaced Versionable with Revision

--- src/ReIndex/Security/Permission/Revision/ViewPermission.php
+++ src/ReIndex/Security/Permission/Revision/ViewPermission.php
@@ -1,22 +1,22 @@
 <?php
 
 /**
- * @file Post/ViewPermission.php
+ * @file Revision/ViewPermission.php
  * @brief This file contains the ViewPermission class.
  * @details
  * @author Filippo F. Fadda
  */
 
 
-namespace ReIndex\Security\Permission\Versionable\Post;
+namespace ReIndex\Security\Permission\Revision;
 
 
 use ReIndex\Enum\State;
 
 
 /**
- * @brief Permission to display a post.
- * @details A member can only see his own posts, even if they are invisible to the other members.
+ * @brief Permission to display a revision.
+ * @details A member can only see his own revisions, even if they are invisible to the other members.
  */
 class ViewPermission extends AbstractPermission  {
 
@@ -26,43 +26,43 @@ class ViewPermission extends AbstractPermission  {
    * @retval bool
    */
   public function checkForGuestRole() {
-    return $this->post->state->is(State::CURRENT) ? TRUE : FALSE;
+    return $this->revision->state->is(State::CURRENT) ? TRUE : FALSE;
   }
 
 
   /**
-   * @brief In addition to every current post, a member can display his own posts.
+   * @brief In addition to every current revision, a member can display his own revisions.
    * @retval bool
    */
   public function checkForMemberRole() {
     if ($this->checkForGuestRole())
       return TRUE;
     else
-      return $this->user->match($this->post->creatorId) ? TRUE : FALSE;
+      return $this->user->match($this->revision->creatorId) ? TRUE : FALSE;
   }
 
 
   /**
-   * @brief A reviewer can see any submitted posts.
+   * @brief A reviewer can see any submitted revisions.
    * @retval bool
    */
   public function checkForReviewerRole() {
     if ($this->checkForMemberRole())
       return TRUE;
     else
-      return $this->post->state->is(State::SUBMITTED) ? TRUE : FALSE;
+      return $this->revision->state->is(State::SUBMITTED) ? TRUE : FALSE;
   }
 
 
   /**
-   * @brief A moderator can display current, submitted, rejected and even deleted posts.
+   * @brief A moderator can display current, submitted, rejected and even deleted revisions.
    * @retval bool
    */
   public function checkForModeratorRole() {
-    return ($this->post->state->is(State::CURRENT) or
-      $this->post->state->is(State::SUBMITTED) or
-      $this->post->state->is(State::REJECTED) or
-      $this->post->state->is(State::DELETED)) ? TRUE : FALSE;
+    return ($this->revision->state->is(State::CURRENT) or
+      $this->revision->state->is(State::SUBMITTED) or
+      $this->revision->state->is(State::REJECTED) or
+      $this->revision->state->is(State::DELETED)) ? TRUE : FALSE;
   }
 
 }
