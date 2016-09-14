85b7068927be9f0f1ec2a1fd744a4e8064e4760c
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-14 19:02:59 +0200

    modified to use bitwise operators

--- src/ReIndex/Enum/State.php
+++ src/ReIndex/Enum/State.php
@@ -10,6 +10,7 @@
 
 //! Enumerators
 namespace ReIndex\Enum;
+use ReIndex\Exception\InvalidStateException;
 
 
 /**
@@ -19,15 +20,16 @@ class State {
 
   protected $meta;
 
-  const CREATED = "created";      //!< A document's revision has been created.
-  const CURRENT = "current";      //!< Indicates this is the current document's revision.
-  const DRAFT = "draft";          //!< The document's revision can eventually be saved as draft.
-  const APPROVED = "approved";    //!< The document's revision has been approved by peers.
-  const SUBMITTED = "submitted";  //!< The document's revision has been submitted for peer review.
-  const REJECTED = "rejected";    //!< The document's revision has been rejected.
-  const DELETED = "deleted";      //!< The document's revision has been deleted.
-  const INDEXING = "indexing";    //!< The document's revision needs to be indexed.
-  const DELETING = "deleting";    //!< The document's revision needs to be deindexed.
+  // DO NOT CHANGE THE CONSTANTS' VALUES!
+  const CURRENT     = 1;   //!< Indicates this is the current document's revision.
+  const CREATED     = 2;   //!< A document's revision has been created.
+  const DRAFT       = 4;   //!< The document's revision can eventually be saved as draft.
+  const APPROVED    = 8;   //!< The document's revision has been approved by peers.
+  const SUBMITTED   = 16;  //!< The document's revision has been submitted for peer review.
+  const REJECTED    = 32;  //!< The document's revision has been rejected.
+  const DELETED     = 64;  //!< The document's revision has been deleted.
+
+  const INDEXING    = 128; //!<
 
 
   /**
@@ -40,12 +42,12 @@ class State {
 
 
   /**
-   * @brief Returns `true` in case the provided value matches the current version's state.
+   * @brief Returns `true` in case the provided value matches the state, `false` otherwise.
    * @param[in] string $value The value to check.
    * @retval bool
    */
   public function is($value) {
-    return ($this->meta["state"] === $value) ? TRUE : FALSE;
+    return ($this->meta["state"] & $value);
   }
 
 
@@ -67,11 +69,24 @@ class State {
 
 
   /**
-   * @brief Returns `true` in case the provided state matches the current version state.
+   * @brief Sets the state to the provided value.
    * @param[in] string $value The value to set.
    */
   public function set($value) {
-    $this->meta["state"] = $value;
+
+    switch ($value) {
+      case self::CURRENT: $this->meta["state"] = $value; break;
+      case self::CREATED: $this->meta["state"] = $value; break;
+      case self::DRAFT: $this->meta["state"] = $value; break;
+      case self::APPROVED: $this->meta["state"] = $value; break;
+      case self::SUBMITTED: $this->meta["state"] = $value; break;
+      case self::REJECTED: $this->meta["state"] = $value; break;
+      case self::DELETED: $this->meta["state"] = $value; break;
+      case (self::CURRENT & self::INDEXING): $this->meta["state"] = $value; break;
+      case (self::DELETED & self::INDEXING): $this->meta["state"] = $value; break;
+      default: throw new InvalidStateException('Invalid state.');
+    }
+
   }
 
 }
