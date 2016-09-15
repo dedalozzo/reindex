aa9d4e6ee45371d5a2c5fde8d4fb50950b1190c9
Filippo Fadda <filippo.fadda@gmail.com>
2016-09-15 22:08:34 +0200

    we added an ImportPermission for the articles

---
+++ src/ReIndex/Security/Permission/Revision/Post/Article/ImportPermission.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file Article/ImportPermission.php
+ * @brief This file contains the ImportPermission class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ReIndex\Security\Permission\Revision\Post\Article;
+
+
+use ReIndex\Security\Permission\Revision\Post\AbstractPermission;
+
+
+/**
+ * @brief Permission to revert the content to a specific revision.
+ */
+class ImportPermission extends AbstractPermission {}
